20fe5386475bf461d9b20dd673f5d758d73d97c4
Daniel Jönsson <daniel@copax.se>
2015-01-29 09:34:06 +0100

    Core: Color conversion commenting

--- include/inviwo/core/util/colorconversion.h
+++ include/inviwo/core/util/colorconversion.h
@@ -34,6 +34,16 @@
 #include <inviwo/core/common/inviwocoredefine.h>
 #include <inviwo/core/common/inviwo.h>
 
+/** 
+ * \brief Convert from HSV to RGB color.
+ *
+ * See http://en.wikipedia.org/wiki/HSL_and_HSV and 
+ * http://en.wikipedia.org/wiki/RGB_color_model 
+ * for a detailed explanation of the color spaces.
+ *
+ * @param hsv Color in the [0 1]^3 range.
+ * @return RGB color in [0 1]^3 range.
+ */
 IVW_CORE_API inline vec3 hsv2rgb(const vec3& hsv)
 {
     float hue = hsv.x;
@@ -93,27 +103,37 @@ IVW_CORE_API inline vec3 hsv2rgb(const vec3& hsv)
     return vec3(x,y,z);
 }
 
+/** 
+ * \brief Convert from RGB to HSV color.
+ *
+ * See http://en.wikipedia.org/wiki/HSL_and_HSV and 
+ * http://en.wikipedia.org/wiki/RGB_color_model 
+ * for a detailed explanation of the color spaces.
+ *
+ * @param rgb Color in the [0 1]^3 range.
+ * @return HSV color in the [0 1]^3 range.
+ */
 IVW_CORE_API inline vec3 rgb2hsv(const vec3& rgb)
 {
     const float& x = rgb.r;
     const float& y = rgb.g;
     const float& z = rgb.b;
-    float max    = (x > y) ? ((x > z) ? x : z) : ((y > z) ? y : z);
-    float min    = (x < y) ? ((x < z) ? x : z) : ((y < z) ? y : z);
-    float range = max - min;
-    float val    = max;
+    float maximum = (x > y) ? ((x > z) ? x : z) : ((y > z) ? y : z);
+    float minimum = (x < y) ? ((x < z) ? x : z) : ((y < z) ? y : z);
+    float range = maximum - minimum;
+    float val    = maximum;
     float sat   = 0.f;
     float hue   = 0.f;
 
-    if (max != 0.f)
-        sat = range/max;
+    if (maximum != 0.f)
+        sat = range/maximum;
 
     if (sat != 0.f) {
         float h;
 
-        if (x == max)
+        if (x == maximum)
             h = (y - z) / range;
-        else if (y == max)
+        else if (y == maximum)
             h = 2.f + (z - x) / range;
         else
             h = 4.f + (x - y) / range;
