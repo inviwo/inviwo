b5bf02dd1c5be69e178aa370aff82981c6fa4186
Rickard Englund <rickard@r-englund.com>
2015-02-09 11:45:19 +0100

    Python3: Removed code that sometimes generate error, fixes inviwo/inviwo-dev#660

--- modules/python3/scripts/outputredirector.py
+++ modules/python3/scripts/outputredirector.py
@@ -1,38 +1,14 @@
 import sys
 import inviwo_internal
 
-class OutputRedirectStdout(object):
-    def __init__(self,out):
-        self.old = out
-        self = out
-        
+class OutputRedirectStdout:
     def write(self, string):
         inviwo_internal.ivwPrint(string, 0)
-        self.old.write(string+'\n')
 
-    def start(self):
-        sys.stdout = self
-
-    def stop(self):
-        sys.stdout = self.old
-        return self
-
-class OutputRedirectStderr(object):
-    def __init__(self,err):
-        self.old = err
-        self = err
-        
+class OutputRedirectStderr:
     def write(self, string):
         inviwo_internal.ivwPrint(string, 1)
-        self.old.write(string+'\n')
 
-    def start(self):
-        sys.stderr = self
+sys.stdout = OutputRedirectStdout()  
+sys.stderr = OutputRedirectStderr() 
 
-    def stop(self):
-        sys.stderr = self.old
-        return self
-
-        
-sys.stdout = OutputRedirectStdout(sys.stdout)
-sys.stderr = OutputRedirectStderr(sys.stderr)
