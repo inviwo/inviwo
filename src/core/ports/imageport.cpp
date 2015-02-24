bd31b4d704737f7c8142786fafb8d56a26b24531
eriksunden <eriksunden85@gmail.com>
2015-02-24 16:47:16 +0100

    Minor Imageport potential crash fix.

--- src/core/ports/imageport.cpp
+++ src/core/ports/imageport.cpp
@@ -185,7 +185,7 @@ ImageOutport::ImageOutport(std::string identifier, const DataFormatBase* format,
 
 ImageOutport::~ImageOutport() {
     for (auto& elem : imageDataMap_) {
-        if (isDataOwner() || elem.second != data_) {
+        if (isDataOwner() && elem.second != data_) {
             delete elem.second;
         }
     }
