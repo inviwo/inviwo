26cdf904ad0dfa2a9c58da64af7e53326aa35f14
Rickard Englund <rickard@r-englund.com>
2015-03-02 08:13:42 +0100

    CMake/C++11: Added cmake tag to force v120_CTP_Nov2012 compiler if MSVC 2012 is used. (Closes inviwo/inviwo-dev#781)

--- cmake/globalconfig.cmake
+++ cmake/globalconfig.cmake
@@ -310,5 +310,12 @@ IF(NOT MSVC)
   else()
     message(FATAL_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler.")
   endif()
+else()
+  if( MSVC_VERSION LESS 1700 )       # VC10-/VS2010- 
+    message(FATAL_ERROR "Inviwo requires C++11 features. " 
+                        "You need at least Visual Studio 11 (Microsoft Visual Studio 2012), " 
+                        "with Microsoft Visual C++ Compiler Nov 2012 CTP (v120_CTP_Nov2012).") 
+  elseif( MSVC_VERSION EQUAL 1700 )  # VC11/VS2012 
+    set(CMAKE_GENERATOR_TOOLSET "v120_CTP_Nov2012" CACHE STRING "Platform Toolset" FORCE) 
+  endif() 
 endif()
-
