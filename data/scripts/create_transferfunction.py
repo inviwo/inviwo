976c97f878aa5f843a1093e8b932035f68dd71bb
Rickard Englund <rickard@r-englund.com>
2015-05-27 12:52:51 +0200

    PythonScripts: Fix for create_transferfunction.py example script

--- data/scripts/create_transferfunction.py
+++ data/scripts/create_transferfunction.py
@@ -6,15 +6,15 @@ def color(x):
    g = 1-x
    return (r,g,0)
 
-inviwo.clearTransferfunction("SimpleRaycaster.transferFunction")
-inviwo.addPointToTransferFunction("SimpleRaycaster.transferFunction",(0.0,0.0),(0,0,0)) 
+inviwo.clearTransferfunction("VolumeRaycaster.transferFunction")
+inviwo.addPointToTransferFunction("VolumeRaycaster.transferFunction",(0.0,0.0),(0,0,0)) 
 
 for i in range(1,256,9):
    x = i / 256.0
    a = 0
    if i%2==1:
       a = 0.1
-   inviwo.addPointToTransferFunction("SimpleRaycaster.transferFunction",(x,a),color(x)) 
+   inviwo.addPointToTransferFunction("VolumeRaycaster.transferFunction",(x,a),color(x)) 
 
 
-inviwo.addPointToTransferFunction("SimpleRaycaster.transferFunction",(1.0,0.0),(0,0,0)) 
+inviwo.addPointToTransferFunction("VolumeRaycaster.transferFunction",(1.0,0.0),(0,0,0)) 
