c0a8e0131db5e84b5537a943d0846a86e44df808
Sathish Kottravel <sathishkottravel@gmail.com>
2015-08-24 11:48:39 +0200

    Reverted: Copy-Paste Composite Property modification.

--- src/core/properties/compositeproperty.cpp
+++ src/core/properties/compositeproperty.cpp
@@ -91,24 +91,15 @@ void CompositeProperty::set(const Property* srcProperty) {
 
 void CompositeProperty::set(const CompositeProperty* src) {
     NetworkLock lock;
-    auto subProperties = src->getPropertiesRecursive();
-    auto properties = this->getPropertiesRecursive();
-    if (subProperties.size() == properties.size()) {
+    auto subProperties = src->getProperties();
+    if (subProperties.size() == this->properties_.size()) {
         for (size_t i = 0; i < subProperties.size(); i++) {
-            properties[i]->set(subProperties[i]);
+            this->properties_[i]->set(subProperties[i]);
         }
         propertyModified();
-    } else {
-        auto subProperties = src->getProperties();
-        if (subProperties.size() == this->properties_.size()) {
-            for (size_t i = 0; i < subProperties.size(); i++) {
-                this->properties_[i]->set(subProperties[i]);
-            }
-            propertyModified();
-        }
-        else {
-            LogWarn("CompositeProperty mismatch. Unable to link");
-        }
+    }
+    else {
+        LogWarn("CompositeProperty mismatch. Unable to link");
     }
 }
 
