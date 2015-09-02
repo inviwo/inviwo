8d384f9b2bcff8b0946fd7f8de62193cd12b633c
Rickard Englund <rickard@r-englund.com>
2015-09-02 09:34:13 +0200

    Core: Added a IndexCalculator to help calculate indices for volumes (and images)

---
+++ src/core/util/indexcalculator.cpp
@@ -0,0 +1,60 @@
+/*********************************************************************************
+ *
+ * Inviwo - Interactive Visualization Workshop
+ *
+ * Copyright (c) 2015 Inviwo Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************************/
+
+#include <inviwo/core/util/indexcalculator.h>
+
+namespace inviwo {
+
+
+    IndexCalculator::IndexCalculator(const size3_t& dim) : dims_(dim)
+    {
+
+    }
+
+    size_t IndexCalculator::index(const size_t& x, const size_t& y, const size_t& z)
+    {
+        return index(size3_t(x, y, z));
+    }
+
+    vec3 IndexCalculator::posFromIndex(size_t i)
+    {
+        vec3 pos;
+
+        pos.z = i / (dims_.y * dims_.x);
+        i -= pos.z * (dims_.y * dims_.x);
+
+        pos.y = i / dims_.x;
+        i -= pos.y * dims_.x;
+
+        pos.x = i;
+        return pos / (vec3)(dims_ - size3_t(1, 1, 1));
+    }
+
+} // namespace
+
