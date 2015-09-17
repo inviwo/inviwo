7b5ed62dacd5458de06fe72921599416fb88f259
eriksunden <eriksunden85@gmail.com>
2015-09-17 17:57:48 +0200

    Update to version 0.9.4

--- cmake/globalconfig.cmake
+++ cmake/globalconfig.cmake
@@ -27,10 +27,10 @@
  # 
  #################################################################################
  
-set(IVW_VERSION 0.9.3dev)
+set(IVW_VERSION 0.9.4)
 set(IVW_MAJOR_VERSION 0)
 set(IVW_MINOR_VERSION 9)
-set(IVW_PATCH_VERSION 3dev)
+set(IVW_PATCH_VERSION 4)
  
 set_property(GLOBAL PROPERTY USE_FOLDERS On)
 set_property(GLOBAL PROPERTY PREDEFINED_TARGETS_FOLDER cmake)
