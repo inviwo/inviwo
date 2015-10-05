016b6d65ffebeacde611aa1305b54c58230bd9b4
Peter Steneteg <peter@steneteg.se>
2015-10-05 16:42:38 +0200

    GLFW: minor cleanup

--- apps/minimals/glfw/glfwminimum.cpp
+++ apps/minimals/glfw/glfwminimum.cpp
@@ -24,7 +24,7 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  *********************************************************************************/
 
 #ifdef _MSC_VER
@@ -56,68 +56,60 @@ int main(int argc, char** argv) {
     LogCentral::init();
     LogCentral::getPtr()->registerLogger(new ConsoleLogger());
 
-    InviwoApplication inviwoApp(argc, argv, "Inviwo v"+IVW_VERSION + " - GLFWApp", inviwo::filesystem::findBasePath());
-    inviwoApp.setPostEnqueueFront([](){glfwPostEmptyEvent();});
+    InviwoApplication inviwoApp(argc, argv, "Inviwo v" + IVW_VERSION + " - GLFWApp",
+                                inviwo::filesystem::findBasePath());
+    inviwoApp.setPostEnqueueFront([]() { glfwPostEmptyEvent(); });
 
     CanvasGLFW::setAlwaysOnTopByDefault(false);
 
-    //Initialize all modules
+    // Initialize all modules
     inviwoApp.initialize(&inviwo::registerAllModules);
 
-    //Continue initialization of default context
-    CanvasGLFW* sharedCanvas = static_cast<CanvasGLFW*>(RenderContext::getPtr()->getDefaultRenderContext());
+    // Continue initialization of default context
+    CanvasGLFW* sharedCanvas =
+        static_cast<CanvasGLFW*>(RenderContext::getPtr()->getDefaultRenderContext());
     sharedCanvas->initialize();
     sharedCanvas->activate();
 
     // Load simple scene
     inviwoApp.getProcessorNetwork()->lock();
     const CommandLineParser* cmdparser = inviwoApp.getCommandLineParser();
-    std::string workspace;
+    const std::string workspace =
+        cmdparser->getLoadWorkspaceFromArg()
+            ? cmdparser->getWorkspacePath()
+            : inviwoApp.getPath(InviwoApplication::PATH_WORKSPACES, "/boron.inv");
 
-    if (cmdparser->getLoadWorkspaceFromArg())
-        workspace = cmdparser->getWorkspacePath();
-    else
-        workspace = inviwoApp.getPath(InviwoApplication::PATH_WORKSPACES, "/boron.inv");
-
-    auto widgetDeleter = [](ProcessorWidget* w) {delete w; };
-    std::vector<std::unique_ptr<ProcessorWidget, decltype(widgetDeleter)>> widgets;
-
-    try
-    {
-        if (!workspace.empty()){
+    std::vector<std::unique_ptr<ProcessorWidget>> widgets;
+    try {
+        if (!workspace.empty()) {
             IvwDeserializer xmlDeserializer(workspace);
             inviwoApp.getProcessorNetwork()->deserialize(xmlDeserializer);
             std::vector<Processor*> processors = inviwoApp.getProcessorNetwork()->getProcessors();
 
-            for (std::vector<Processor*>::iterator it = processors.begin(); it != processors.end(); ++it) {
-                Processor* processor = *it;
+            for (auto processor : processors) {
                 processor->invalidate(INVALID_RESOURCES);
-
-                ProcessorWidget* processorWidget = ProcessorWidgetFactory::getPtr()->create(processor).release();
-                if (processorWidget) {
-                    widgets.emplace_back(processorWidget, widgetDeleter);
+                if (auto processorWidget = ProcessorWidgetFactory::getPtr()->create(processor)) {
                     processorWidget->setProcessor(processor);
                     processorWidget->initialize();
                     processorWidget->setVisible(processorWidget->ProcessorWidget::isVisible());
-                    processor->setProcessorWidget(processorWidget);
+                    processor->setProcessorWidget(processorWidget.get());
+                    widgets.push_back(std::move(processorWidget));
                 }
             }
         }
-    }
-    catch (const AbortException& exception) {
+    } catch (const AbortException& exception) {
         util::log(exception.getContext(),
-            "Unable to load network " + workspace + " due to " + exception.getMessage(),
-            LogLevel::Error);
+                  "Unable to load network " + workspace + " due to " + exception.getMessage(),
+                  LogLevel::Error);
         return 1;
-    }
-    catch (const IgnoreException& exception) {
+    } catch (const IgnoreException& exception) {
         util::log(exception.getContext(),
-            "Incomplete network loading " + workspace + " due to " + exception.getMessage(),
-            LogLevel::Error);
+                  "Incomplete network loading " + workspace + " due to " + exception.getMessage(),
+                  LogLevel::Error);
         return 1;
-    }
-    catch (const ticpp::Exception& exception) {
-        LogErrorCustom("glfwminimum", "Unable to load network " + workspace + " due to deserialization error: " + exception.what());
+    } catch (const ticpp::Exception& exception) {
+        LogErrorCustom("glfwminimum", "Unable to load network " + workspace +
+                                          " due to deserialization error: " + exception.what());
         return 1;
     }
 
@@ -126,22 +118,18 @@ int main(int argc, char** argv) {
 
     if (cmdparser->getCaptureAfterStartup()) {
         std::string path = cmdparser->getOutputPath();
-
-        if (path.empty())
-            path = inviwoApp.getPath(InviwoApplication::PATH_IMAGES);
+        if (path.empty()) path = inviwoApp.getPath(InviwoApplication::PATH_IMAGES);
 
         util::saveAllCanvases(inviwoApp.getProcessorNetwork(), path, cmdparser->getSnapshotName());
     }
 
-    if (cmdparser->getQuitApplicationAfterStartup()){
+    if (cmdparser->getQuitApplicationAfterStartup()) {
         glfwTerminate();
         return 0;
     }
 
-
-    while (CanvasGLFW::getWindowCount()>0) {
+    while (CanvasGLFW::getWindowCount() > 0) {
         glfwWaitEvents();
-        //glfwPollEvents();
         inviwoApp.processFront();
     }
 
