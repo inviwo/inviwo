63164688ec5d6186d57c76d63a030c6fbc0961b0
Daniel Jönsson <daniel@copax.se>
2015-11-05 18:29:02 +0100

    OpenGL: Fixed introduced error in gradient computation for multichannel data inviwo/inviwo-dev#1003

--- modules/opengl/glsl/utils/gradients.glsl
+++ modules/opengl/glsl/utils/gradients.glsl
@@ -117,10 +117,12 @@ mat4x3 gradientAllForwardDiff(vec4 intensity, sampler3D volume, VolumeParameters
     fDs[2] = getNormalizedVoxel(volume, volumeParams, samplePos + volumeParams.textureSpaceGradientSpacing[2]);
     // f' = ( f(x+h)-f(x) ) / h
     fDs -= mat3x4(intensity, intensity, intensity);
-    fDs[0] /= volumeParams.worldSpaceGradientSpacing;
-    fDs[1] /= volumeParams.worldSpaceGradientSpacing;
-    fDs[2] /= volumeParams.worldSpaceGradientSpacing;
-    return transpose(fDs);
+    mat4x3 gradients = transpose(fDs);
+    gradients[0] /= volumeParams.worldSpaceGradientSpacing;
+    gradients[1] /= volumeParams.worldSpaceGradientSpacing;
+    gradients[2] /= volumeParams.worldSpaceGradientSpacing;
+    gradients[3] /= volumeParams.worldSpaceGradientSpacing;
+    return gradients;
 }
 
 // Compute world space gradient using central difference: f' = ( f(x+h)-f(x-h) ) / 2*h
@@ -136,10 +138,12 @@ mat4x3 gradientAllCentralDiff(vec4 intensity, sampler3D volume, VolumeParameters
     cDs[1] -= getNormalizedVoxel(volume, volumeParams, samplePos - volumeParams.textureSpaceGradientSpacing[1]);
     cDs[2] -= getNormalizedVoxel(volume, volumeParams, samplePos - volumeParams.textureSpaceGradientSpacing[2]);
     // f' = ( f(x+h)-f(x-h) ) / 2*h
-    cDs[0] /= 2.0*volumeParams.worldSpaceGradientSpacing;
-    cDs[1] /= 2.0*volumeParams.worldSpaceGradientSpacing;
-    cDs[2] /= 2.0*volumeParams.worldSpaceGradientSpacing;
-    return transpose(cDs);
+    mat4x3 gradients = transpose(cDs);
+    gradients[0] /= 2.0*volumeParams.worldSpaceGradientSpacing;
+    gradients[1] /= 2.0*volumeParams.worldSpaceGradientSpacing;
+    gradients[2] /= 2.0*volumeParams.worldSpaceGradientSpacing;
+    gradients[3] /= 2.0*volumeParams.worldSpaceGradientSpacing;
+    return gradients;
 }
 
 // Compute world space gradient using backward difference: f' = ( f(x)-f(x-h) ) / h
@@ -152,10 +156,12 @@ mat4x3 gradientAllBackwardDiff(vec4 intensity, sampler3D volume, VolumeParameter
     fDs[2] = getNormalizedVoxel(volume, volumeParams, samplePos - volumeParams.textureSpaceGradientSpacing[2]);
     // f' = ( f(x)-f(x-h) ) / h
     fDs = mat3x4(intensity, intensity, intensity) - fDs;
-    fDs[0] /= volumeParams.worldSpaceGradientSpacing;
-    fDs[1] /= volumeParams.worldSpaceGradientSpacing;
-    fDs[2] /= volumeParams.worldSpaceGradientSpacing;
-    return transpose(fDs);
+    mat4x3 gradients = transpose(fDs);
+    gradients[0] /= volumeParams.worldSpaceGradientSpacing;
+    gradients[1] /= volumeParams.worldSpaceGradientSpacing;
+    gradients[2] /= volumeParams.worldSpaceGradientSpacing;
+    gradients[3] /= volumeParams.worldSpaceGradientSpacing;
+    return gradients;
 }
 
 // Compute world space gradient using higher order central difference: f' = ( -f(x+2h)+8.f(x+h)-8.f(x-h)+f(x-2h) ) / 12*h
@@ -179,11 +185,12 @@ mat4x3 gradientAllCentralDiffH(vec4 intensity, sampler3D volume, VolumeParameter
     cDs[1] += getNormalizedVoxel(volume, volumeParams, samplePos - 2.0*volumeParams.textureSpaceGradientSpacing[1]);
     cDs[2] += getNormalizedVoxel(volume, volumeParams, samplePos - 2.0*volumeParams.textureSpaceGradientSpacing[2]);
     // f' = ( -f(x+2h)+8.f(x+h)-8.f(x-h)+f(x-2h) ) / 12*h
-
-    cDs[0] /= 12.0*volumeParams.worldSpaceGradientSpacing;
-    cDs[1] /= 12.0*volumeParams.worldSpaceGradientSpacing;
-    cDs[2] /= 12.0*volumeParams.worldSpaceGradientSpacing;
-    return transpose(cDs);
+    mat4x3 gradients = transpose(cDs);
+    gradients[0] /= 12.0*volumeParams.worldSpaceGradientSpacing;
+    gradients[1] /= 12.0*volumeParams.worldSpaceGradientSpacing;
+    gradients[2] /= 12.0*volumeParams.worldSpaceGradientSpacing;
+    gradients[3] /= 12.0*volumeParams.worldSpaceGradientSpacing;
+    return gradients;
 }
 
 // Use pre-computed gradients stored in xyz of the current sample. The gradient will be transformed from texture space
