776b37071c205c2322855aeece0cf924f1e59fc4
eriksunden <eriksunden85@gmail.com>
2015-12-17 20:15:54 +0100

    Include fix

--- modules/basegl/processors/imagemixer.h
+++ modules/basegl/processors/imagemixer.h
@@ -34,6 +34,7 @@
 #include <inviwo/core/common/inviwo.h>
 #include <inviwo/core/processors/processor.h>
 #include <inviwo/core/ports/imageport.h>
+#include <inviwo/core/properties/boolproperty.h>
 #include <inviwo/core/properties/optionproperty.h>
 #include <inviwo/core/properties/ordinalproperty.h>
 #include <modules/opengl/shader/shader.h>
