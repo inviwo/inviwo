1f58375160be5353a9fee62d5101f6301ca2ec5f
eriksunden <eriksunden85@gmail.com>
2016-01-29 09:53:48 +0100

    Commited dev tag version

--- cmake/globalconfig.cmake
+++ cmake/globalconfig.cmake
@@ -30,7 +30,7 @@
 set(IVW_VERSION 0.9.5)
 set(IVW_MAJOR_VERSION 0)
 set(IVW_MINOR_VERSION 9)
-set(IVW_PATCH_VERSION 5)
+set(IVW_PATCH_VERSION 5dev)
 
 #--------------------------------------------------------------------
 # Requirement checks
