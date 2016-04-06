5fe9e6b9bbb82ef5cf5496eddde9215e1fc3cf0a
Rickard Englund <rickard@r-englund.com>
2016-04-06 19:54:38 +0200

    Base: Added a processor to Create a VolumeSampler and put it on a SpatialSamplerPort

---
+++ modules/base/processors/volumetospatialsampler.cpp
@@ -0,0 +1,62 @@
+/*********************************************************************************
+ *
+ * Inviwo - Interactive Visualization Workshop
+ *
+ * Copyright (c) 2015 Inviwo Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************************/
+
+#include "volumetospatialsampler.h"
+#include <inviwo/core/util/volumesampler.h>
+
+namespace inviwo {
+
+// The Class Identifier has to be globally unique. Use a reverse DNS naming scheme
+const ProcessorInfo VolumeToSpatialSampler::processorInfo_{
+    "org.inviwo.VolumeToSpatialSampler",      // Class identifier
+    "Volume To Spatial Sampler",                // Display name
+    "Undefined",              // Category
+    CodeState::Experimental,  // Code state
+    Tags::None,               // Tags
+};
+const ProcessorInfo VolumeToSpatialSampler::getProcessorInfo() const {
+    return processorInfo_;
+}
+
+VolumeToSpatialSampler::VolumeToSpatialSampler()
+    : Processor()
+    , volume_("volume")
+    , sampler_("sampler")
+{    
+    addPort(volume_);
+    addPort(sampler_);
+}
+    
+void VolumeToSpatialSampler::process() {
+    auto sampler = std::make_shared< VolumeDoubleSampler<3>>(volume_.getData());
+    sampler_.setData(sampler);
+}
+
+} // namespace
+
