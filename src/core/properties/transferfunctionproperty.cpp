3371785dd077b0b4ff4c69fd7059daad5d1a9493
martinfalk <martin.falk@liu.se>
2016-04-27 17:30:22 +0200

    Core: minor fix in TF property

--- src/core/properties/transferfunctionproperty.cpp
+++ src/core/properties/transferfunctionproperty.cpp
@@ -166,7 +166,6 @@ void TransferFunctionProperty::set(const TransferFunction& value) {
 
 void TransferFunctionProperty::set(const Property *property) {
     if (auto tfp = dynamic_cast<const TransferFunctionProperty*>(property)) {
-        set(tfp->get());
         TemplateProperty<TransferFunction>::set(tfp);
     }
 }
