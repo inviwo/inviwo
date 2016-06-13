a66570539e5f96a6febcc9f61a3ae071b2847da8
martinfalk <martin.falk@liu.se>
2016-06-13 14:46:16 +0200

    FontRendering: minor fix and comments

--- modules/fontrendering/processors/textoverlaygl.cpp
+++ modules/fontrendering/processors/textoverlaygl.cpp
@@ -170,11 +170,14 @@ std::string TextOverlayGL::getString() const {
         std::string numStr = str.substr(offset + 1, 3);
         if (std::isdigit(numStr[0])) {
             std::size_t numDigits = 0;
-            std::size_t argNum = std::stoul(numStr, &numDigits);
+            // extract number and reduce it by one since the %args start with 1
+            // std::stoul will not throw an invalid argument exception since we made sure, it is a
+            // number (std::isdigit above)
+            std::size_t argNum = std::stoul(numStr, &numDigits) - 1;
             if (argNum <= numArgs_) {
                 // make textual replacement ("%" and number of digits)
-                str.replace(offset, numDigits + 1, args[argNum - 1]->get());
-                offset += args[argNum - 1]->get().size();
+                str.replace(offset, numDigits + 1, args[argNum]->get());
+                offset += args[argNum]->get().size();
             }
             else {
                 if (numDigits > 2) {
