35e4236a05bc3e39797ff9e6e6ed7227629b84a4
martinfalk <martin.falk@liu.se>
2016-06-23 09:20:14 +0200

    Core: comments in colorconversion.h

--- include/inviwo/core/util/colorconversion.h
+++ include/inviwo/core/util/colorconversion.h
@@ -38,7 +38,7 @@ namespace inviwo {
 namespace color {
 
 /**
-* \brief reference white point D65 in XYZ color space
+* \brief reference white point D65 in CIE XYZ color space
 *
 * The values are obtained by converting sRGB(1, 1, 1) to XYZ using rgb2XYZ() since sRGB assumes D65
 * as reference white.
@@ -55,8 +55,8 @@ IVW_CORE_API vec3 getD65WhitePoint();
  * http://en.wikipedia.org/wiki/RGB_color_model
  * for a detailed explanation of the color spaces.
  *
- * @param hsv Color in the [0 1]^3 range.
- * @return RGB color in [0 1]^3 range.
+ * @param hsv Color in the [0 1]^3 range
+ * @return RGB color in [0 1]^3 range
  */
 IVW_CORE_API vec3 hsv2rgb(vec3 hsv);
 
@@ -67,8 +67,8 @@ IVW_CORE_API vec3 hsv2rgb(vec3 hsv);
  * http://en.wikipedia.org/wiki/RGB_color_model
  * for a detailed explanation of the color spaces.
  *
- * @param rgb Color in the [0 1]^3 range.
- * @return HSV color in the [0 1]^3 range.
+ * @param rgb Color in the [0 1]^3 range
+ * @return HSV color in the [0 1]^3 range
  */
 IVW_CORE_API vec3 rgb2hsv(vec3 rgb);
 
@@ -76,19 +76,19 @@ IVW_CORE_API vec3 rgb2hsv(vec3 rgb);
  * \brief Convert from XYZ to Lab color space
  * See http://en.wikipedia.org/wiki/CIE_1931_color_space
  * and http://www.brucelindbloom.com/
- * @param xyz Color in the [0 1]^3 range
+ * @param xyz color in the CIE XYZ color space (roughly in the [0 1]^3 range)
  * @param whitePoint Normalized white point. Default white point is D65.
- * @return vec3 Color in Lab space
+ * @return Lab color
  */
 IVW_CORE_API vec3 XYZ2lab(vec3 xyz, vec3 whitePoint = getD65WhitePoint());
 
 /**
- * \brief Convert from Lab to XYZ color space
+ * \brief Convert from Lab to CIE XYZ color space
  * See http://en.wikipedia.org/wiki/Lab_color_space
  * and http://www.brucelindbloom.com/
  * @param lab Color
  * @param whitePoint Normalized white point. Default white point is D65.
- * @return Lab color
+ * @return CIE XYZ color (roughly in the [0 1]^3 range)
  */
 IVW_CORE_API vec3 lab2XYZ(vec3 lab, const vec3 whitePoint = getD65WhitePoint());
 
@@ -98,40 +98,36 @@ IVW_CORE_API vec3 lab2XYZ(vec3 lab, const vec3 whitePoint = getD65WhitePoint());
  * http://www.brucelindbloom.com/
  * and http://en.wikipedia.org/wiki/RGB_color_model
  * @param rgb rgb color in the [0 1]^3 range
- * @return XYZ color in the [0 1]^3 range.
+ * @return CIE XYZ color (roughly in the [0 1]^3 range)
  */
 IVW_CORE_API vec3 rgb2XYZ(const vec3 rgb);
 
 /**
- * \brief Convert from XYZ color to sRGB using D65 white point.
+ * \brief Convert from CIE XYZ color to sRGB using D65 white point.
  *
  * See http://en.wikipedia.org/wiki/CIE_1931_color_space,
  * http://www.brucelindbloom.com/
  * and http://en.wikipedia.org/wiki/RGB_color_model
- * @param xyz  XYZ color in the [0 1]^3 range.
- * @return sRGB color in the [0 1]^3 range.
+ * @param xyz  CIE XYZ color (roughly in the [0 1]^3 range)
+ * @return sRGB color in the [0 1]^3 range
  */
 IVW_CORE_API vec3 XYZ2rgb(vec3 xyz);
 
 /**
-* \brief Convert from XYZ color to xyY, which is for example used in the chromaticity diagram.
+* \brief Convert from CIE XYZ color space to normalized xyY, which is for example used in the chromaticity diagram.
 *
-* See http://en.wikipedia.org/wiki/CIE_1931_color_space,
-* http://www.brucelindbloom.com/
-* and http://en.wikipedia.org/wiki/RGB_color_model
-* @param xyz  XYZ color in the [0 1]^3 range.
-* @return sRGB color in the [0 1]^3 range.
+* See http://en.wikipedia.org/wiki/CIE_1931_color_space and http://www.brucelindbloom.com/
+* @param xyz  CIE XYZ color (roughly in the [0 1]^3 range)
+* @return xyY color, (x, y chromaticity, and Y lightness) in the [0 1]^3 range
 */
 IVW_CORE_API vec3 XYZ2xyY(vec3 xyz);
 
 /**
-* \brief Convert from XYZ color to sRGB using D65 white point.
+* \brief Convert from xyY color space to XYZ
 *
-* See http://en.wikipedia.org/wiki/CIE_1931_color_space,
-* http://www.brucelindbloom.com/
-* and http://en.wikipedia.org/wiki/RGB_color_model
-* @param xyz XYZ color in the [0 1]^3 range.
-* @return sRGB color in the [0 1]^3 range.
+* See http://en.wikipedia.org/wiki/CIE_1931_color_space and http://www.brucelindbloom.com/
+* @param xyY  xyY color
+* @return CIE XYZ color (roughly in the [0 1]^3 range)
 */
 IVW_CORE_API vec3 xyY2XYZ(vec3 xyY);
 
@@ -142,7 +138,7 @@ IVW_CORE_API vec3 xyY2XYZ(vec3 xyY);
  * http://www.brucelindbloom.com/
  * and http://en.wikipedia.org/wiki/RGB_color_model
  *
- * @param rgb rgb color in the [0 1]^3 range.
+ * @param rgb rgb color in the [0 1]^3 range
  * @return Lab color
  */
 IVW_CORE_API vec3 rgb2lab(vec3 rgb);
@@ -154,8 +150,8 @@ IVW_CORE_API vec3 rgb2lab(vec3 rgb);
  * http://www.brucelindbloom.com/
  * and http://en.wikipedia.org/wiki/RGB_color_model
  *
- * @param lab Lab color
- * @return Color in the [0 1]^3 range.
+ * @param lab color in Lab color space
+ * @return rgb color in the [0 1]^3 range
  */
 IVW_CORE_API vec3 lab2rgb(vec3 lab);
 
@@ -168,7 +164,7 @@ IVW_CORE_API vec3 lab2rgb(vec3 lab);
  *
  * See http://en.wikipedia.org/wiki/YCbCr
  *
- * @param rgb RGB color, [0, 1]^3
+ * @param rgb rgb color, [0, 1]^3
  * @return YCbCr color, Y in [0, 1], Cb in [-0.5, 0.5], Cr in [-0.5, 0.5]
  */
 IVW_CORE_API vec3 rgb2ycbcr(const vec3 &rgb);
@@ -181,40 +177,69 @@ IVW_CORE_API vec3 rgb2ycbcr(const vec3 &rgb);
 *
 * See http://en.wikipedia.org/wiki/YCbCr
 *
-* @param YCbCr color, Y in [0, 1], Cb in [-0.5, 0.5], Cr in [-0.5, 0.5]
-* @return rgb RGB color, [0, 1]^3
+* @param ycbcr YCbCr color, Y in [0, 1], Cb in [-0.5, 0.5], Cr in [-0.5, 0.5]
+* @return rgb color, [0, 1]^3
 */
 IVW_CORE_API vec3 ycbcr2rgb(const vec3 &ycbcr);
 
 /**
 * \brief Convert from normalized chromaticity of CIE Luv, i.e. u' and v', to sRGB
 *
-* The chromaticity of CIE Luv (luminance, u', v') is converted to sRGB via the XYZ color space.
+* The chromaticity of CIE Luv (luminance, u', v') is converted to sRGB via the CIE XYZ color space.
 *
 * See http://en.wikipedia.org/wiki/CIELUV, http://www.brucelindbloom.com/
 *
-* @param LuvChroma chromaticity, L in [0, 1], u' in [0.0, 1.0], v' in [0.0, 1.0]
+* @param LuvChroma chromaticity, L in [0, 100], u' in [0.0, 1.0], v' in [0.0, 1.0]
 * @param clamp   clamp resulting rgb values to [0,1]^3
 * @param whitePointXYZ  Normalized white point. Default white point is D65.
-* @return rgb RGB color, [0, 1]^3 if clamping is enabled
+* @return rgb color, [0, 1]^3 if clamping is enabled
 */
 IVW_CORE_API vec3 LuvChromaticity2rgb(const vec3 &LuvChroma, bool clamp=false, vec3 whitePointXYZ = getD65WhitePoint());
 
+/**
+* \brief Convert from sRGB to normalized chromaticity of CIE Luv, i.e. u' and v'
+*
+* The sRGB color is converted to chromaticity of CIE Luv (luminance, u', v') via the CIE XYZ color space.
+*
+* See http://en.wikipedia.org/wiki/CIELUV, http://www.brucelindbloom.com/
+*
+* @param rgb rgb color, [0, 1]^3
+* @param whitePointXYZ  Normalized white point. Default white point is D65.
+* @return LuvChroma chromaticity, L in [0, 100], u' in [0.0, 1.0], v' in [0.0, 1.0]
+*/
 IVW_CORE_API vec3 rgb2LuvChromaticity(const vec3 &rgb, vec3 whitePointXYZ = getD65WhitePoint());
 
+/**
+* \brief Convert from normalized chromaticity of CIE Luv, i.e. u' and v', to XYZ
+*
+* See http://en.wikipedia.org/wiki/CIELUV, http://www.brucelindbloom.com/
+*
+* @param LuvChroma chromaticity, L in [0, 100], u' in [0.0, 1.0], v' in [0.0, 1.0]
+* @param whitePointXYZ  Normalized white point. Default white point is D65.
+* @return CIE XYZ color (roughly in the [0 1]^3 range)
+*/
 IVW_CORE_API vec3 LuvChromaticity2XYZ(const vec3 &LuvChroma, vec3 whitePointXYZ = getD65WhitePoint());
 
+/**
+* \brief Convert from CIE XYZ color space to normalized chromaticity of CIE Luv, i.e. u' and v'
+*
+* See http://en.wikipedia.org/wiki/CIELUV, http://www.brucelindbloom.com/
+*
+* @param XYZ CIE XYZ color (roughly in the [0 1]^3 range)
+* @param whitePointXYZ  Normalized white point. Default white point is D65.
+* @return CIE XYZ color (roughly in the [0 1]^3 range)
+*/
 IVW_CORE_API vec3 XYZ2LuvChromaticity(const vec3 &XYZ, vec3 whitePointXYZ = getD65WhitePoint());
 
 /**
 * \brief Convert from CIE XYZ to CIE Luv
 *
-* The CIE Luv color value is converted to XYZ color space using the reference white point.
+* The CIE Luv color value is converted to CIE XYZ color space using the reference white point.
 *
 * See http://en.wikipedia.org/wiki/CIELUV, http://en.wikipedia.org/wiki/CIE_1931_color_space, and
 * http://www.brucelindbloom.com/
 *
-* @param XYZ   CIE XYZ color [0,1]^3
+* @param XYZ  CIE XYZ color (roughly in the [0 1]^3 range)
 * @param whitePointXYZ  Normalized white point. Default white point is D65.
 * @return CIE Luv color value, L in [0, 100], u and v in [-100, +100] (for typical images)
 */
@@ -223,15 +248,15 @@ IVW_CORE_API vec3 XYZ2Luv(const vec3 &XYZ, vec3 whitePointXYZ = getD65WhitePoint
 /**
 * \brief Convert from CIE Luv to CIE XYZ
 *
-* The CIE Luv color value is converted to XYZ color space using the reference white point.
+* The CIE Luv color value is converted to CIE XYZ color space using the reference white point.
 *
 * See http://en.wikipedia.org/wiki/CIELUV, http://en.wikipedia.org/wiki/CIE_1931_color_space, and
 * http://www.brucelindbloom.com/
 *
-* @param Luv   CIE Luv color, L in [0, 100], u and v in [-100, +100] but values might exceed this
+* @param Luv CIE Luv color, L in [0, 100], u and v in [-100, +100] but values might exceed this
 * range
 * @param whitePointXYZ  Normalized white point. Default white point is D65.
-* @return CIE XYZ color value
+* @return CIE XYZ color (roughly in the [0 1]^3 range)
 */
 IVW_CORE_API vec3 Luv2XYZ(const vec3 &Luv, vec3 whitePointXYZ = getD65WhitePoint());
 
