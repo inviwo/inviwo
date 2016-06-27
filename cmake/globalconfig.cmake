9b40c39788fb7c555c7add1f01b770d95a8fae4e
Peter Steneteg <peter@steneteg.se>
2016-06-27 16:21:26 +0200

    Version update to 0.9.7

--- cmake/globalconfig.cmake
+++ cmake/globalconfig.cmake
@@ -30,7 +30,7 @@
 set(IVW_VERSION 0.9.6)
 set(IVW_MAJOR_VERSION 0)
 set(IVW_MINOR_VERSION 9)
-set(IVW_PATCH_VERSION 6)
+set(IVW_PATCH_VERSION 7)
 
 #--------------------------------------------------------------------
 # Requirement checks
