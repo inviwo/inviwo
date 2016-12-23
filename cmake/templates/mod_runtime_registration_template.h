5f92dd7938550531c7c121dcb50d2e5870da79a5
Daniel Jönsson <daniel@copax.se>
2016-12-23 10:49:52 +0100

    Core: inviwo/inviwo-dev#1364 Added module library search directories on unix. DT_RUNPATH and DT_RPATH in ELF and environment variable LD_RUN_PATH

--- cmake/templates/mod_runtime_registration_template.h
+++ cmake/templates/mod_runtime_registration_template.h
@@ -3,6 +3,9 @@
 #include <inviwo/core/util/sharedlibrary.h>
 #include <inviwo/core/util/filesystem.h>
 #include <inviwo/core/util/stringconversion.h>
+#if defined(__unix__)
+#include <elf.h> // To retrieve rpath
+#endif
 
 namespace inviwo {
 
@@ -18,18 +21,47 @@ std::vector<std::string> registerAllModules() {
         inviwo::filesystem::getPath(inviwo::PathType::Modules)};
     
     // http://unix.stackexchange.com/questions/22926/where-do-executables-look-for-shared-objects-at-runtime
-    char* dyldPaths = nullptr;
 #if defined(__APPLE__)
     // Xcode/OSX store library output path in DYLD_LIBRARY_PATH
-    dyldPaths = std::getenv("DYLD_LIBRARY_PATH");
-#elif defined(__unix__)
-    // Unix uses LD_LIBRARY_PATH instead
-    dyldPaths = std::getenv("LD_LIBRARY_PATH");
-#endif
-    if (dyldPaths) {
-        auto dyPaths = splitString(dyldPaths, ':');
-        paths.insert(std::end(paths), std::begin(dyPaths), std::end(dyPaths));
+    if (char* envPaths = std::getenv("DYLD_LIBRARY_PATH")) {
+        auto libPaths = splitString(envPaths, ':');
+        paths.insert(std::end(paths), std::begin(libPaths), std::end(libPaths));
     }
+#elif defined(__unix__)
+    // Unix uses LD_LIBRARY_PATH or LD_RUN_PATH
+    if (char* envPaths = std::getenv("LD_LIBRARY_PATH")) {
+        auto libPaths = splitString(envPaths, ':');
+        paths.insert(std::end(paths), std::begin(libPaths), std::end(libPaths));
+    }
+    if (char* envPaths = std::getenv("LD_RUN_PATH")) {
+        auto libPaths = splitString(envPaths, ':');
+        paths.insert(std::end(paths), std::begin(libPaths), std::end(libPaths));
+    }
+    // Additional paths can be specified in
+    // ELF header: RUN_PATH or RPATH
+    const ElfW(Dyn) *rPath = nullptr;
+    const ElfW(Dyn) *runPath = nullptr;
+    const char *offset = nullptr;
+    for (const ElfW(Dyn) *dyn = _DYNAMIC; dyn->d_tag != DT_NULL; ++dyn) {
+        if (dyn->d_tag == DT_RUNPATH) {
+            *runPath = dyn->d_tag
+        } else if (dyn->d_tag == DT_RPATH) {
+            rPath = dyn;
+        } else if (dyn->d_tag == DT_STRTAB) {
+            offset = (const char *)dyn->d_un.d_val;
+        }
+    }
+    if (offset) {
+        // Prioritize DT_RUNPATH, DT_RPATH is deprecated
+        if (runPath) {
+            auto rPaths = splitString(offset + runPath->d_un.d_val, ':');
+            paths.insert(std::end(paths), std::begin(rPaths), std::end(rPaths));
+        } else if (rPath) {
+            auto rPaths = splitString(offset + rPath->d_un.d_val, ':');
+            paths.insert(std::end(paths), std::begin(rPaths), std::end(rPaths));
+        }
+    }
+#endif
     return paths;
 }
 
