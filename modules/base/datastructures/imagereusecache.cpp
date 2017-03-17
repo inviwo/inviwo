7e56676fb3211c9af7f9999dc443a8fbfa73a6dd
Peter Steneteg <peter@steneteg.se>
2017-03-17 14:53:11 +0100

    Base: Added a data structure to reuse output image in processors so one does not have to overwrite the output data.

---
+++ modules/base/datastructures/imagereusecache.cpp
@@ -0,0 +1,51 @@
+/*********************************************************************************
+ *
+ * Inviwo - Interactive Visualization Workshop
+ *
+ * Copyright (c) 2017 Inviwo Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************************/
+
+#include <modules/base/datastructures/imagereusecache.h>
+
+namespace inviwo {
+
+std::shared_ptr<Image> ImageReuseCache::getUnused() {
+    auto it = std::find_if(imageCache_.begin(), imageCache_.end(),
+                           [](const auto& elem) { return elem.use_count() == 1; });
+    if (it != imageCache_.end()) {
+        auto res = *it;
+        imageCache_.erase(it);
+        return res;
+    } else {
+        return nullptr;
+    }
+}
+
+void ImageReuseCache::add(std::shared_ptr<Image> image) {
+    imageCache_.push_back(image);
+}
+
+} // namespace
+
