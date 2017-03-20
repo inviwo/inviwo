37b425db61203081e4600f789609cd8a8ee6f004
martinfalk <martin.falk@liu.se>
2017-03-20 13:31:14 +0100

    BaseGL: added GL point renderer
    * input meshes will be rendered with 2D points
    
    * supports border and antialising

---
+++ modules/basegl/processors/pointrenderer.h
@@ -0,0 +1,100 @@
+/*********************************************************************************
+ *
+ * Inviwo - Interactive Visualization Workshop
+ *
+ * Copyright (c) 2017 Inviwo Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************************/
+
+#ifndef IVW_POINTRENDERER_H
+#define IVW_POINTRENDERER_H
+
+#include <modules/basegl/baseglmoduledefine.h>
+#include <inviwo/core/common/inviwo.h>
+#include <inviwo/core/processors/processor.h>
+#include <inviwo/core/interaction/cameratrackball.h>
+#include <inviwo/core/ports/meshport.h>
+#include <inviwo/core/ports/imageport.h>
+#include <inviwo/core/properties/cameraproperty.h>
+#include <inviwo/core/properties/ordinalproperty.h>
+#include <inviwo/core/properties/compositeproperty.h>
+#include <modules/opengl/shader/shader.h>
+#include <vector>
+
+namespace inviwo {
+
+/** \docpage{org.inviwo.PointRenderer, Point Renderer}
+ * ![](org.inviwo.PointRenderer.png?classIdentifier=org.inviwo.PointRenderer)
+ * This processor renders a set of meshes as points using OpenGL.
+ *
+ * ### Inports
+ *   * __geometry__ Input meshes
+ *   * __imageInport__ Optional background image
+ *
+ * ### Outports
+ *   * __image__ output image containing the rendered mesh and the optional input image
+ *
+ * ### Properties
+ *   * __Point Size__  size of the rendered points (in pixel)
+ *   * __Border Width__  width of the border
+ *   * __Border Color__  color of the border
+ *   * __Antialising__ width of the antialised point edge (in pixel), this determines the
+ *                     softness along the outer edge of the point
+ */
+
+/**
+ * \class PointRenderer
+ * \brief Renders input geometry with 2D points
+ */
+class IVW_MODULE_BASEGL_API PointRenderer : public Processor {
+public:
+    PointRenderer();
+    virtual ~PointRenderer() = default;
+
+    virtual void process() override;
+
+    virtual const ProcessorInfo getProcessorInfo() const override;
+    static const ProcessorInfo processorInfo_;
+
+private:
+    void drawMeshes();
+
+    MeshFlatMultiInport inport_;
+    ImageInport imageInport_;
+    ImageOutport outport_;
+
+    FloatProperty pointSize_;
+    FloatProperty borderWidth_;
+    FloatVec4Property borderColor_;
+    FloatProperty antialising_;
+
+    CameraProperty camera_;
+    CameraTrackball trackball_;
+
+    Shader shader_;
+};
+
+}  // namespace inviwo
+
+#endif  // IVW_POINTRENDERER_H
