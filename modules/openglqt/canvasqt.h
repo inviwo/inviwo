bb041c64e797d0a2888fcd390531072dea3045b1
Peter Steneteg <peter@steneteg.se>
2017-04-07 10:18:23 +0200

    OpenGLQt: fix for context menu always showing in canvas even when dragging to zoom

--- modules/openglqt/canvasqt.h
+++ modules/openglqt/canvasqt.h
@@ -87,12 +87,11 @@ public:
     virtual bool isFullScreen() const override;
     virtual void setFullScreen(bool fullscreen) override;
 
-    void contextMenuEvent(QContextMenuEvent* event) override;
-
 protected:
     virtual bool event(QEvent *e) override;
 
 private:
+    void doContextMenu(QMouseEvent* event);
     dvec2 normalPos(dvec2 pos) const;
 
     bool mapMousePressEvent(QMouseEvent* e);
@@ -123,6 +122,7 @@ CanvasQtBase<T>::CanvasQtBase(size2_t dim, const std::string& name)
     QtBase::makeCurrent();
     RenderContext::getPtr()->registerContext(this, name);
     utilgl::handleOpenGLDebugMode(this->activeContext());
+    this->setContextMenuPolicy(Qt::PreventContextMenu);
 }
 
 template <typename T>
@@ -169,16 +169,10 @@ bool inviwo::CanvasQtBase<T>::isFullScreen() const {
 }
 
 template <typename T>
-void CanvasQtBase<T>::contextMenuEvent(QContextMenuEvent* event) {
+void CanvasQtBase<T>::doContextMenu(QMouseEvent* event) {
     if (auto canvasProcessor = dynamic_cast<CanvasProcessor*>(this->ownerWidget_->getProcessor())) {
 
-        if (!canvasProcessor->isContextMenuAllowed()) {
-            return;
-        }
-
-        if (event->reason() == QContextMenuEvent::Mouse && blockContextMenu_) {
-            return;
-        }
+        if (!canvasProcessor->isContextMenuAllowed()) return;
 
         QMenu menu(this);
 
@@ -253,8 +247,6 @@ bool CanvasQtBase<T>::mapMousePressEvent(QMouseEvent* e) {
     e->accept();
     Canvas::propagateEvent(&mouseEvent);
 
-    blockContextMenu_ = e->button() == Qt::RightButton && mouseEvent.hasBeenUsed();
-
     return true;
 }
 
@@ -270,7 +262,7 @@ bool CanvasQtBase<T>::mapMouseDoubleClickEvent(QMouseEvent* e) {
 
     e->accept();
     Canvas::propagateEvent(&mouseEvent);
-    blockContextMenu_ |= e->button() == Qt::RightButton && mouseEvent.hasBeenUsed();
+    blockContextMenu_ = true;
     return true;
 }
 
@@ -286,7 +278,13 @@ bool CanvasQtBase<T>::mapMouseReleaseEvent(QMouseEvent* e) {
                           this->getDepthValueAtNormalizedCoord(pos));
     e->accept();
     Canvas::propagateEvent(&mouseEvent);
-    blockContextMenu_ |= e->button() == Qt::RightButton && mouseEvent.hasBeenUsed();
+    
+    // Only show context menu when we have not used the event and the mouse have not been dragged.
+    if (e->button() == Qt::RightButton && !mouseEvent.hasBeenUsed() && !blockContextMenu_) {
+        doContextMenu(e);
+    }
+    blockContextMenu_ = false;
+    
     return true;
 }
 
