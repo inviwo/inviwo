7e4c223e9493cd7070cad68a2b61072154f278ea
Rickard Englund <r-englund@users.noreply.github.com>
2017-04-21 14:19:37 +0200

    BaseGL: Background: Additional fixes for multi line macros

--- modules/basegl/processors/background.cpp
+++ modules/basegl/processors/background.cpp
@@ -199,6 +199,7 @@ void Background::updateShaderInputs() {
         // location 0 is reserved for FragData0, location 1 for PickingData
         size_t outputLocation = 2;
         std::stringstream ssUniform;
+        ssUniform << "\n";
         for (size_t i = 1; i < numColorLayers; ++i) {
             ssUniform << "layout(location = " << outputLocation++ << ") out vec4 FragData" << i
                       << ";\n";
@@ -211,7 +212,11 @@ void Background::updateShaderInputs() {
 
         std::stringstream ssWrite;
         for (size_t i = 1; i < numColorLayers; ++i) {
-            ssWrite << "FragData" << i << " = texture(color" << i << ", texCoord_.xy);\n";
+            ssWrite << "FragData" << i << " = texture(color" << i << ", texCoord.xy);";
+            if(i<numColorLayers-1){
+                ssWrite << " \\";
+            }
+            ssWrite << "\n";
         }
         shader_.getFragmentShaderObject()->addShaderDefine("ADDITIONAL_COLOR_LAYER_WRITE",
                                                            ssWrite.str());
