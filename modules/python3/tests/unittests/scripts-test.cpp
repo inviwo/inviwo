00f26a4e1569462036ad7b3f057ddfc19ea4e644
Rickard Englund <rickard@r-englund.com>
2017-05-20 10:22:58 +0200

    Python3: Unittests: rewrote the array type tests to use parameterized tests (TEST_P)

--- modules/python3/tests/unittests/scripts-test.cpp
+++ modules/python3/tests/unittests/scripts-test.cpp
@@ -69,7 +69,7 @@ std::string getPath() {
         ModulePath::UnitTests);
     return path + "/scripts/";
 }
-}
+}  // namespace
 
 TEST(Python3Scripts, GrabReturnValues) {
     PythonScriptDisk script(getPath() + "grabreturnvalue.py");
@@ -150,105 +150,10 @@ TEST(Python3Scripts, SimpleBufferTest) {
 
                    auto vec = intBuffer.getEditableRAMRepresentation()->getDataContainer();
                    for (size_t i = 0; i < bufferSize; i++) {
-                       EXPECT_EQ(static_cast<int>(i * i), vec[i]) <<
-                                 "Simple buffer test: write value";
+                       EXPECT_EQ(static_cast<int>(i * i), vec[i])
+                           << "Simple buffer test: write value";
                    }
                });
-
-    EXPECT_TRUE(status);
-}
-
-TEST(Python3Scripts, BufferTypesTest) {
-    PythonScriptDisk script(getPath() + "buffer_types.py");
-
-    bool status = false;
-    script.run([&](pybind11::dict dict) {
-        auto listOfArrays = pybind11::cast<pybind11::list>(dict["arrs"]);
-        size_t testnum = 0;
-        try
-        {
-            for (auto &arrObj : listOfArrays) {
-                auto arr = pybind11::cast<pybind11::array>(arrObj);
-                auto buffer = pyutil::createBuffer(arr);
-
-                EXPECT_EQ(3, buffer->getSize());
-
-                buffer->getEditableRepresentation<BufferRAM>()->dispatch<void>([&](auto pBuffer) {
-                    auto vec = pBuffer->getDataContainer();
-                    int expected = 1;
-                    for (auto v : vec) {
-                        for (size_t i = 0; i < pBuffer->getDataFormat()->getComponents(); i++) {
-                            EXPECT_EQ(expected++, (int)util::glmcomp(v, i));
-                        }
-                    }
-
-                });
-                testnum++;
-            }
-
-            status = true;
-        }
-        catch (...)
-        {
-            ASSERT_TRUE(false) << "Test number " << testnum << " failed";
-        }
-        
-    });
-    EXPECT_TRUE(status);
-}
-
-TEST(Python3Scripts, LayerTypesTest) {
-    PythonScriptDisk script(getPath() + "layer_types.py");
-
-    bool status = false;
-    script.run([&](pybind11::dict dict) {
-        auto listOfArrays = pybind11::cast<pybind11::list>(dict["arrs"]);
-        for (auto &arrObj : listOfArrays) {
-            auto arr = pybind11::cast<pybind11::array>(arrObj);
-            auto layer = pyutil::createLayer(arr);
-            layer->getEditableRepresentation<LayerRAM>()->dispatch<void>([&](auto pLayer) {
-                auto dims = pLayer->getDimensions();
-                EXPECT_EQ(size2_t(2, 2), dims);
-                auto data = pLayer->getDataTyped();
-                int expected = 1;
-                for (int j = 0; j < 4; j++) {
-                    auto v = data[j];
-                    for (size_t i = 0; i < pLayer->getDataFormat()->getComponents(); i++) {
-                        EXPECT_EQ(expected++, (int)util::glmcomp(v, i));
-                    }
-                }
-
-            });
-        }
-        status = true;
-    });
-    EXPECT_TRUE(status);
-}
-
-TEST(Python3Scripts, VolumeTypesTest) {
-    PythonScriptDisk script(getPath() + "volume_types.py");
-
-    bool status = false;
-    script.run([&](pybind11::dict dict) {
-        auto listOfArrays = pybind11::cast<pybind11::list>(dict["arrs"]);
-        for (auto &arrObj : listOfArrays) {
-            auto arr = pybind11::cast<pybind11::array>(arrObj);
-            auto volume = pyutil::createVolume(arr);
-            volume->getEditableRepresentation<VolumeRAM>()->dispatch<void>([&](auto pVolume) {
-                auto dims = pVolume->getDimensions();
-                EXPECT_EQ(size3_t(2, 2, 2), dims);
-                auto data = pVolume->getDataTyped();
-                int expected = 1;
-                for (int j = 0; j < 8; j++) {
-                    auto v = data[j];
-                    for (size_t i = 0; i < pVolume->getDataFormat()->getComponents(); i++) {
-                        EXPECT_EQ(expected++, (int)util::glmcomp(v, i));
-                    }
-                }
-            });
-        }
-        status = true;
-    });
 
     EXPECT_TRUE(status);
 }
@@ -296,11 +201,8 @@ TEST(Python3Scripts, GLMTest) {
         EXPECT_TRUE(
             glm::all(glm::epsilonEqual(vec3(-2, 1, 6), v3, std::numeric_limits<float>::epsilon())));
 
-
-        EXPECT_TRUE(
-            glm::all(glm::epsilonEqual(vec4(1,2,3,4), v4, std::numeric_limits<float>::epsilon())));
-
-
+        EXPECT_TRUE(glm::all(
+            glm::epsilonEqual(vec4(1, 2, 3, 4), v4, std::numeric_limits<float>::epsilon())));
 
         EXPECT_FLOAT_EQ(1.f, dict["x"].cast<float>());
         EXPECT_FLOAT_EQ(1.f, dict["r"].cast<float>());
@@ -318,7 +220,6 @@ TEST(Python3Scripts, GLMTest) {
         EXPECT_FLOAT_EQ(4.f, dict["a"].cast<float>());
         EXPECT_FLOAT_EQ(4.f, dict["q"].cast<float>());
 
-
         status = true;
     });
 
@@ -329,17 +230,17 @@ TEST(Python3Scripts, OptionPropertyTest) {
     PythonScriptDisk script(getPath() + "option_property.py");
 
     bool status = false;
-    script.run([&](pybind11::dict dict){
+    script.run([&](pybind11::dict dict) {
 
-        auto prop = dict["p"].cast<Property*>();
-        ASSERT_TRUE(prop!=nullptr);
-        auto optionProperty = dynamic_cast<OptionPropertyInt*>(prop);
-        ASSERT_TRUE(optionProperty!=nullptr);
+        auto prop = dict["p"].cast<Property *>();
+        ASSERT_TRUE(prop != nullptr);
+        auto optionProperty = dynamic_cast<OptionPropertyInt *>(prop);
+        ASSERT_TRUE(optionProperty != nullptr);
 
         EXPECT_STREQ("test", optionProperty->getIdentifier().c_str());
         EXPECT_STREQ("Test", optionProperty->getDisplayName().c_str());
 
-        EXPECT_EQ(2,optionProperty->size());
+        EXPECT_EQ(2, optionProperty->size());
 
         EXPECT_STREQ("a", optionProperty->getIdentifiers()[0].c_str());
         EXPECT_STREQ("A", optionProperty->getDisplayNames()[0].c_str());
@@ -353,8 +254,149 @@ TEST(Python3Scripts, OptionPropertyTest) {
     });
 
     EXPECT_TRUE(status);
-
-
 }
 
-}
+class DTypeTest : public ::testing::TestWithParam<std::string> {
+protected:
+    virtual void SetUp() {}
+    virtual void TearDown() {}
+
+    void testBuffer(std::string numbers, size_t components) {
+        std::stringstream src;
+        src << "import numpy as np" << std::endl;
+        src << "a = np.array("<<numbers<<" , dtype = np." << GetParam() << " )" << std::endl;
+
+        PythonScript s;
+        s.setSource(src.str());
+        bool status = false;
+        s.run([&](pybind11::dict dict) {
+            ASSERT_TRUE(dict.contains("a"));
+
+            auto ob = dict["a"];
+            ASSERT_TRUE(pybind11::isinstance<pybind11::array>(ob));
+            auto arr = pybind11::cast<pybind11::array>(ob);
+            auto buffer = pyutil::createBuffer(arr);
+            EXPECT_EQ(3, buffer->getSize());
+            EXPECT_EQ(components, buffer->getDataFormat()->getComponents());
+
+            buffer->getEditableRepresentation<BufferRAM>()->dispatch<void>([&](auto pBuffer) {
+                auto &vec = pBuffer->getDataContainer();
+                int expected = 1;
+                for (const auto &v : vec) {
+                    for (size_t i = 0; i < pBuffer->getDataFormat()->getComponents(); i++) {
+                        EXPECT_EQ(expected++, (int)util::glmcomp(v, i));
+                    }
+                }
+            });
+
+            status = true;
+        });
+
+        EXPECT_TRUE(status);
+    }
+
+    void testLayer(std::string numbers, std::string shape, size_t components) {
+        std::stringstream src;
+        src << "import numpy as np" << std::endl;
+        src << "a = np.array(" << numbers << ", dtype=np." << GetParam() << ")" << std::endl;
+        src << "a.shape = " << shape << std::endl;
+
+
+        PythonScript s;
+        s.setSource(src.str());
+        bool status = false;
+        s.run([&](pybind11::dict dict) {
+            ASSERT_TRUE(dict.contains("a"));
+
+            auto ob = dict["a"];
+            ASSERT_TRUE(pybind11::isinstance<pybind11::array>(ob));
+            
+            auto arr = pybind11::cast<pybind11::array>(ob);
+            auto layer = pyutil::createLayer(arr);
+            EXPECT_EQ(components, layer->getDataFormat()->getComponents());
+            layer->getEditableRepresentation<LayerRAM>()->dispatch<void>([&](auto pLayer) {
+                auto dims = pLayer->getDimensions();
+                EXPECT_EQ(size2_t(2, 2), dims);
+                auto data = pLayer->getDataTyped();
+                int expected = 1;
+                for (int j = 0; j < 4; j++) {
+                    auto v = data[j];
+                    for (size_t i = 0; i < pLayer->getDataFormat()->getComponents(); i++) {
+                        EXPECT_EQ(expected++, (int)util::glmcomp(v, i));
+                    }
+                }
+
+            });
+
+            status = true;
+        });
+
+        EXPECT_TRUE(status);
+
+
+    }
+    void testVolume(std::string numbers, std::string shape, size_t components) {
+    
+        std::stringstream src;
+        src << "import numpy as np" << std::endl;
+        src << "a = np.array(" << numbers << ", dtype=np." << GetParam() << ")" << std::endl;
+        src << "a.shape = " << shape << std::endl;
+
+
+        PythonScript s;
+        s.setSource(src.str());
+        bool status = false;
+        s.run([&](pybind11::dict dict) {
+            ASSERT_TRUE(dict.contains("a"));
+
+            auto ob = dict["a"];
+            ASSERT_TRUE(pybind11::isinstance<pybind11::array>(ob));
+
+            auto arr = pybind11::cast<pybind11::array>(ob);
+            auto volume = pyutil::createVolume(arr);
+            EXPECT_EQ(components, volume->getDataFormat()->getComponents());
+            volume->getEditableRepresentation<VolumeRAM>()->dispatch<void>([&](auto pLayer) {
+                auto dims = pLayer->getDimensions();
+                EXPECT_EQ(size3_t(2, 2, 2), dims);
+                auto data = pLayer->getDataTyped();
+                int expected = 1;
+                for (int j = 0; j < 4; j++) {
+                    auto v = data[j];
+                    for (size_t i = 0; i < pLayer->getDataFormat()->getComponents(); i++) {
+                        EXPECT_EQ(expected++, (int)util::glmcomp(v, i));
+                    }
+                }
+
+            });
+
+            status = true;
+        });
+
+        EXPECT_TRUE(status);
+    
+    }
+};
+
+TEST_P(DTypeTest, BufferTypeScalarTests) { testBuffer("[1,2,3]", 1); }
+TEST_P(DTypeTest, BufferTypeVec2Tests)  { testBuffer("[[1,2], [3,4], [5,6]]", 2); }
+TEST_P(DTypeTest, BufferTypeVec3Tests)  { testBuffer("[[1,2,3], [4,5,6], [7,8,9]]", 3); }
+TEST_P(DTypeTest, BufferTypeVec4Tests)  { testBuffer("[[1,2,3,4], [5,6,7,8], [9,10,11,12]]", 4); }
+
+
+TEST_P(DTypeTest, LayerTypeScalarTests) { testLayer("[1,2,3,4]","(2,2)", 1); }
+TEST_P(DTypeTest, LayerTypeVec2Tests) { testLayer("[[1,2], [3,4], [5,6] , [7,8]]", "(2,2,2)", 2); }
+TEST_P(DTypeTest, LayerTypeVec3Tests) { testLayer("[[1,2,3], [4,5,6], [7,8,9] , [10,11,12]]", "(2,2,3)", 3); }
+TEST_P(DTypeTest, LayerTypeVec4Tests) { testLayer("[[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]", "(2,2,4)", 4); }
+
+TEST_P(DTypeTest, VolumeTypeScalarTests) { testVolume("[1,2,3,4,5,6,7,8]", "(2,2,2)", 1); }
+TEST_P(DTypeTest, VolumeTypeVec2Tests) { testVolume("[[1,2], [3,4], [5,6] , [7,8] , [9, 10] , [11,12] , [13,14] , [15,16]]", "(2,2,2,2)", 2); }
+TEST_P(DTypeTest, VolumeTypeVec3Tests) { testVolume("[[1,2,3], [4,5,6], [7,8,9] , [10,11,12],[13,14,15], [16,17,18], [19,20,21] , [22,23,24]]", "(2,2,2,3)", 3); }
+TEST_P(DTypeTest, VolumeTypeVec4Tests) { testVolume("[[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16] , [17,18,19,20], [21,22,23,24], [25,26,27,28], [29,30,31,32]]", "(2,2,2,4)", 4); }
+
+const static std::vector<std::string> dtypes = {{"float16"}, {"float32"}, {"float64"}, {"int8"},
+                                                {"int16"},   {"int32"},   {"int64"},   {"uint8"},
+                                                {"uint16"},  {"uint32"},  {"uint64"}};
+
+INSTANTIATE_TEST_CASE_P(DefaultTypes, DTypeTest, ::testing::ValuesIn(dtypes));
+
+}  // namespace inviwo
