9ec162120b8db91e79001a592b35d80c081522d2
Rickard Englund <r-englund@users.noreply.github.com>
2017-05-26 15:30:13 +0200

    Integration test: readded the GLFW include

--- tests/integrationtests/inviwo-integrationtests.cpp
+++ tests/integrationtests/inviwo-integrationtests.cpp
@@ -35,6 +35,7 @@
 #endif
 
 #include <modules/opengl/inviwoopengl.h>
+#include <modules/glfw/canvasglfw.h>
 
 #include <inviwo/core/common/defaulttohighperformancegpu.h>
 #include <inviwo/core/common/inviwo.h>
