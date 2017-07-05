482ebafab52a3af298d04c6a195db2947d2fc089
Martin Falk <martin.falk@liu.se>
2017-07-05 21:29:29 +0200

    FontRendering: minor refactoring
    * uses initialization list for font sizes instead of loop

--- modules/fontrendering/properties/fontproperty.cpp
+++ modules/fontrendering/properties/fontproperty.cpp
@@ -36,15 +36,13 @@ namespace inviwo {
 
 PropertyClassIdentifier(FontProperty, "org.inviwo.FontProperty");
 
-FontProperty::FontProperty(
-    const std::string& identifier, const std::string& displayName,
-    InvalidationLevel invalidationLevel,
-    PropertySemantics semantics)
+FontProperty::FontProperty(const std::string& identifier, const std::string& displayName,
+                           InvalidationLevel invalidationLevel, PropertySemantics semantics)
     : CompositeProperty(identifier, displayName, invalidationLevel, semantics)
     , fontFace_("fontFace", "Font Face")
-    , fontSize_("fontSize", "Font size")
-    , anchorPos_("anchor", "Anchor", vec2(-1.0f), vec2(-1.0f), vec2(1.0f), vec2(0.01f))
-{
+    , fontSize_("fontSize", "Font size", {6, 8, 10, 11, 12, 14, 16, 20, 24, 28, 36, 48, 60, 72, 96},
+                5)
+    , anchorPos_("anchor", "Anchor", vec2(-1.0f), vec2(-1.0f), vec2(1.0f), vec2(0.01f)) {
     auto fonts = util::getAvailableFonts();
 
     for (auto font : fonts) {
@@ -55,22 +53,16 @@ FontProperty::FontProperty(
     fontFace_.setSelectedIdentifier("Montserrat-Medium");
     fontFace_.setCurrentStateAsDefault();
 
-    // set up different font sizes
-    std::vector<int> fontSizes = {6, 8, 10, 11, 12, 14, 16, 20, 24, 28, 36, 48, 60, 72, 96};
-    for (auto size : fontSizes) {
-        std::string str = std::to_string(size);
-        fontSize_.addOption(str, str, size);
-    }
-    fontSize_.setSelectedIndex(5);
-    fontSize_.setCurrentStateAsDefault();
-
     addProperty(fontFace_);
     addProperty(fontSize_);
     addProperty(anchorPos_);
 }
 
 FontProperty::FontProperty(const FontProperty& rhs)
-    : CompositeProperty(rhs), fontFace_(rhs.fontFace_), fontSize_(rhs.fontSize_), anchorPos_(rhs.anchorPos_) {
+    : CompositeProperty(rhs)
+    , fontFace_(rhs.fontFace_)
+    , fontSize_(rhs.fontSize_)
+    , anchorPos_(rhs.anchorPos_) {
     addProperty(fontFace_);
     addProperty(fontSize_);
     addProperty(anchorPos_);
