3f2cd56efd6608946725165136a27f1d49b982aa
scanberg <scanberg@gmail.com>
2017-09-14 19:48:11 +0200

    Core: glm.h: Removed GLM_FORCE_CXX14, solves issue inviwo/inviwo-dev#1864 (inviwo/inviwo-dev#1877)

--- include/inviwo/core/util/glm.h
+++ include/inviwo/core/util/glm.h
@@ -36,9 +36,6 @@
 #ifndef GLM_FORCE_SIZE_T_LENGTH
 #define GLM_FORCE_SIZE_T_LENGTH
 #endif
-#ifndef GLM_FORCE_CXX14
-#define GLM_FORCE_CXX14
-#endif
 
 #include <warn/push>
 #include <warn/ignore/shadow>
