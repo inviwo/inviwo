ed93f8692b113eedca31f1dc56938d7971489634
Rickard Englund <r-englund@users.noreply.github.com>
2017-10-24 22:00:25 +0200

    QtWidgets: SyntaxHightlighter: Missing include

--- modules/qtwidgets/properties/syntaxhighlighter.cpp
+++ modules/qtwidgets/properties/syntaxhighlighter.cpp
@@ -31,6 +31,8 @@
 #include <inviwo/core/common/inviwomodule.h>
 #include <inviwo/core/util/filesystem.h>
 #include <modules/qtwidgets/qtwidgetssettings.h>
+#include <inviwo/core/common/inviwoapplication.h>
+#include <inviwo/core/util/settings/systemsettings.h>
 
 #include <warn/push>
 #include <warn/ignore/all>
