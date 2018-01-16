eed6256f90f4d5613d22e33bb442c732f15d92d6
Peter Steneteg <peter@steneteg.se>
2018-01-16 11:19:03 +0100

    Base: Volume generation

---
+++ modules/base/algorithm/volume/volumegeneration.h
@@ -0,0 +1,71 @@
+/*********************************************************************************
+ *
+ * Inviwo - Interactive Visualization Workshop
+ *
+ * Copyright (c) 2017 Inviwo Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************************/
+
+#ifndef IVW_VOLUMEGENERATION_H
+#define IVW_VOLUMEGENERATION_H
+
+#include <modules/base/basemoduledefine.h>
+#include <inviwo/core/common/inviwo.h>
+
+#include <inviwo/core/datastructures/volume/volume.h>
+#include <inviwo/core/datastructures/volume/volumeramprecision.h>
+#include <inviwo/core/util/volumeramutils.h>
+#include <inviwo/core/util/indexmapper.h>
+
+namespace inviwo {
+
+namespace util {
+
+/**
+ * Convenience function for generating volumes
+ * @param dimensions Volume grid dimensions
+ * @param basisAndOffset Volume basis and offset matrix
+ * @param function Functor called for each volume voxel. T(const size3_t& ind).
+ */
+template <typename Functor>
+std::unique_ptr<Volume> generateVolume(const size3_t& dimensions, const mat4& basisAndOffset,
+                                       Functor&& function) {
+    using T = decltype(function(dimensions));
+
+    auto ram = std::make_shared<VolumeRAMPrecision<T>>(dimensions);
+    auto data = ram->getDataTyped();
+    IndexMapper3D im(dimensions);
+
+    forEachVoxelParallel(*ram, [&](const size3_t& ind) { data[im(ind)] = function(ind); });
+
+    auto volume = std::make_unique<Volume>(ram);
+    volume->setModelMatrix(basisAndOffset);
+    return volume;
+}
+
+}  // namespace util
+
+}  // namespace inviwo
+
+#endif  // IVW_VOLUMEGENERATION_H
