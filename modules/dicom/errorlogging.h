dcad6bf508ea01fce1ddcb2c656718dbfa703b28
Marius Kircher <marius.kircher@uni-ulm.de>
2018-09-12 16:40:47 +0200

    edit gdcm dicom loader

---
+++ modules/dicom/errorlogging.h
@@ -0,0 +1,14 @@
+#ifndef IVW_GDCM_MODULE_ERRORLOGGING_H
+#define IVW_GDCM_MODULE_ERRORLOGGING_H
+
+#include <inviwo/core/util/logcentral.h>
+#include <inviwo/core/util/exception.h>
+
+namespace inviwo {
+
+void enable_libtiff4_errorlogging(void);
+void enable_libgdcm_errorlogging(void);
+
+}
+
+#endif
