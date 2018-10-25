761128aade6f6c604bb82fc3e3031275d0346ab2
Albin Bergström <albinbergstrom@gmail.com>
2018-10-25 09:39:32 +0200

    WebBrowser: Now uses the previously created visitor. Also fixed a bug.

--- modules/webbrowser/properties/propertycefsynchronizer.cpp
+++ modules/webbrowser/properties/propertycefsynchronizer.cpp
@@ -53,8 +53,7 @@ void PropertyCefSynchronizer::OnLoadEnd(CefRefPtr<CefBrowser> browser, CefRefPtr
     // synchronize all properties
     // Ok to send javascript commands when frame loaded
     for (auto& widget : widgets_) {
-        widget->setFrame(frame);
-        widget->updateFromProperty();
+        widget->setFrameIfPartOfFrame(frame);
     }
 }
 
@@ -67,7 +66,7 @@ bool PropertyCefSynchronizer::OnQuery(CefRefPtr<CefBrowser> browser, CefRefPtr<C
     auto key = std::string(R"("id":")");
     auto keyStart = requestStr.find(key);
     if (keyStart == std::string::npos) {
-        LogWarn(R"(No id found. Expected {"id":"elementId", "value":"x")" + requestStr);
+        LogWarn(R"(No id found. Expected {"id":"elementId", "value":"x"})" + requestStr);
         return false;
     }
     size_t offset = keyStart + key.length();
