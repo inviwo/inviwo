315f79ed098bb304ab2f92994028a94c59a611ae
Peter Steneteg <peter@steneteg.se>
2018-11-18 19:58:54 +0100

    Python3: include fix

--- modules/python3/tests/unittests/scripts-test.cpp
+++ modules/python3/tests/unittests/scripts-test.cpp
@@ -35,6 +35,7 @@
 #include <inviwo/core/common/inviwoapplication.h>
 #include <modules/python3/python3module.h>
 #include <modules/python3/pythonscript.h>
+#include <modules/python3/pybindutils.h>
 
 #include <inviwo/core/datastructures/image/image.h>
 #include <inviwo/core/datastructures/image/layer.h>
@@ -53,9 +54,6 @@
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <modules/python3/pybindutils.h>
-#include <modules/python3/python3module.h>
-#include <modules/python3/interface/pybuffer.h>
 
 #include <glm/gtc/epsilon.hpp>
 
