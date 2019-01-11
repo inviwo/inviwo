b3e3f962e604f4b3c006e46925893318eb2a5ba6
Peter Steneteg <peter@steneteg.se>
2019-01-11 13:16:26 +0100

    Jenkins: typo

--- tools/jenkins/filter-compilecommands.py
+++ tools/jenkins/filter-compilecommands.py
@@ -7,4 +7,4 @@ with open("compile_commands.json", "r") as f:
 with open("compile_commands.json", "w") as f:
     data = [x for x in data if re.fullmatch(".*/ext/.*", x['file']) == None]
     data = [x for x in data if re.fullmatch(".*/build/.*", x['file']) == None]
-    json.dump(fdata, f, sort_keys=True, indent=4)
+    json.dump(data, f, sort_keys=True, indent=4)
