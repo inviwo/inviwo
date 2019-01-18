9819b19a0436ac84240fdd7991c3ed6f5adce5f9
Martin Falk <martin.falk@liu.se>
2019-01-18 10:32:00 +0100

    Tools: changelog to html fix * ensured that platform-dependent line endings are used * timestamp of output file is updated to ensure that the CMake target suceeds, #363

--- tools/changelog-to-html.py
+++ tools/changelog-to-html.py
@@ -29,7 +29,7 @@
 import os
 import sys
 import argparse
-import codecs
+import pathlib
 
 import ivwpy.util
 from ivwpy.colorprint import *
@@ -49,14 +49,8 @@ except ImportError:
 try:
 	import gfm
 except ImportError:
-	missing_modules['[py-gfm'] = "extension for markdown, needed for parsing github-flavored markdown (GFM)"
+	missing_modules['py-gfm'] = "extension for markdown, needed for parsing github-flavored markdown (GFM)"
 
-if len(missing_modules)>0: 
-	print_error("Error: Missing python modules:")
-	for k,v in missing_modules.items():
-		print_error("    {:20s} {}".format(k,v))	
-	print_info("    To install run: 'pip3 install {}'".format(" ".join(missing_modules.keys())))
-	exit(0)
 
 def makeCmdParser():
 	parser = argparse.ArgumentParser(
@@ -165,11 +159,22 @@ changelogBegin = "Here we document changes"
 def main(args):
 	args = makeCmdParser();
 
+	if len(missing_modules)>0: 
+		print_error("Warning: Could not generate HTML changelog. Missing python modules:")
+		for k,v in missing_modules.items():
+			print_error("    {:20s} {}".format(k,v))	
+		print_info("    To install run: 'python -m pip install {}'".format(" ".join(missing_modules.keys())))
+
+		# touch target file to update time stamp even though we could not update it
+		pathlib.Path(args.output).touch()
+
+		exit(0)
+
 	if not os.path.exists(args.input):
 		print_error("changelog-to-html.py was unable to locate the input file " + args.input)
 		sys.exit(1)
 
-	with codecs.open(args.input, mode="r", encoding="utf-8") as f:
+	with open(args.input, mode="r", encoding="utf-8") as f:
 		text = f.read();
 	
 	# remove first line starting with "Here we document changes..."
@@ -183,9 +188,8 @@ def main(args):
 
 	(path, filename)  = os.path.split(os.path.abspath(args.output))
 	ivwpy.util.mkdir(path)
-	with codecs.open(args.output, mode="w", encoding="utf-8", errors="xmlcharrefreplace") as f:
+	with open(args.output, mode="w", encoding="utf-8", errors="xmlcharrefreplace") as f:
 		f.write(html)
-
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
