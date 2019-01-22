c564e63ffa4037e27a52938f12a16f6102601b74
Rickard Englund <gefle.rickard@gmail.com>
2019-01-22 09:40:31 +0100

    Eigen: Fix for port inspector not working

--- modules/eigenutils/src/eigenutilsmodule.cpp
+++ modules/eigenutils/src/eigenutilsmodule.cpp
@@ -59,8 +59,8 @@ EigenUtilsModule::EigenUtilsModule(InviwoApplication* app) : InviwoModule(app, "
 
     // Ports
     registerDefaultsForDataType<Eigen::MatrixXf>();
-
-    registerPortInspector("EigenMatrixXfOutport",
+    
+    registerPortInspector(PortTraits<DataOutport<Eigen::MatrixXf>>::classIdentifier(),
                           this->getPath(ModulePath::PortInspectors) + "/eigenmatrix.inv");
 
     // PropertyWidgets
