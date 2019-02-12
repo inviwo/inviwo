4fa7641fd1519c02bedcfb0659f41e2955ce426a
Peter Steneteg <peter@steneteg.se>
2019-02-12 12:34:41 +0100

    Jenkins: more tests

--- tools/jenkins/util.groovy
+++ tools/jenkins/util.groovy
@@ -335,9 +335,9 @@ def build(Map args = [:]) {
 def buildStandard(Map args = [:]) {
     stage('Build') {
         sh '''
-            printf "\e[31mHello\e[0m\n"
-            printf "\033[31mHello\033[0m\n"
-            printf "\x1b[31mHello\x1b[0m\n"
+            printf "\\e[31mHello\\e[0m\n"
+            printf "\\033[31mHello\\033[0m\n"
+            printf "\\x1b[31mHello\\x1b[0m\n"
         '''
 
         if (args.state.env.Clean_Build?.equals("true")) clean()
