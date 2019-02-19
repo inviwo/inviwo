9b0bbe9414840613227ab47f26a506a90e18091f
Peter Steneteg <peter@steneteg.se>
2019-02-19 16:57:17 +0100

    CMake: Testing find scripts

--- cmake/modules/FindFilesystem.cmake
+++ cmake/modules/FindFilesystem.cmake
@@ -22,6 +22,15 @@ elseif(EXPERIMENTAL_FILESYSTEM_EXISTS)
     endif()
 endif()
 
+if(NOT FILESYSTEM_HAVE_FS)
+    if(Filesystem_FIND_REQUIRED)
+        message(FATAL_ERROR "The used compiler does not have std::filesystem support")
+    else()
+        set(Filesystem_FOUND FALSE CACHE BOOL "" FORCE)
+    endif()
+    return()
+endif()
+
 string(CONFIGURE [[
     #include <@FILESYSTEM_HEADER@>
 
@@ -38,29 +47,26 @@ set(CMAKE_REQUIRED_LIBRARIES -lc++fs)
 check_cxx_source_compiles("${code}" FILESYSTEM_CPPFS_NEEDED)
 cmake_pop_check_state()
 
-if(FILESYSTEM_HAVE_FS)
-    add_library(std::filesystem INTERFACE IMPORTED GLOBAL)
-    target_compile_definitions(
-        std::filesystem
-        INTERFACE
-        STD_FS_IS_EXPERIMENTAL=$<NOT:$<BOOL:${FILESYSTEM_EXISTS}>>)
-
-    if(FILESYSTEM_NO_LINK_NEEDED)
-        # Nothing to add...
-    elseif(FILESYSTEM_STDCPPFS_NEEDED)
-        target_link_libraries(std::filesystem INTERFACE -lstdc++fs)
-    elseif(FILESYSTEM_CPPFS_NEEDED)
-        target_link_libraries(std::filesystem INTERFACE -lc++fs)
-    else()
-        if(Filesystem_FIND_REQUIRED)
-            message(FATAL_ERROR "Cannot Compile Simple Program using std::filesystem")
-        endif()
-    endif()
-
-    set(Filesystem_FOUND TRUE CACHE BOOL "" FORCE)
-else()
-    set(Filesystem_FOUND FALSE CACHE BOOL "" FORCE)
+if(NOT FILESYSTEM_NO_LINK_NEEDED OR NOT FILESYSTEM_STDCPPFS_NEEDED OR NOT FILESYSTEM_CPPFS_NEEDED)
     if(Filesystem_FIND_REQUIRED)
         message(FATAL_ERROR "Cannot Compile Simple Program using std::filesystem")
+    else()
+        set(Filesystem_FOUND FALSE CACHE BOOL "" FORCE)
     endif()
+    return()
 endif()
+
+add_library(std::filesystem INTERFACE IMPORTED GLOBAL)
+target_compile_definitions(std::filesystem
+    INTERFACE 
+    STD_FS_IS_EXPERIMENTAL=$<NOT:$<BOOL:${FILESYSTEM_EXISTS}>>
+)
+
+if(FILESYSTEM_STDCPPFS_NEEDED)
+    target_link_libraries(std::filesystem INTERFACE -lstdc++fs)
+elseif(FILESYSTEM_CPPFS_NEEDED)
+    target_link_libraries(std::filesystem INTERFACE -lc++fs)
+endif()
+
+set(Filesystem_FOUND TRUE CACHE BOOL "" FORCE)
+
