a3f04a43091ee402292c2ae5a8338d9d5b229506
Wiebke Köpp <wiebke.koepp@googlemail.com>
2019-04-11 22:43:29 +0200

    Register const channel, always return numCells

--- modules/discretedata/src/connectivity/structuredgrid.cpp
+++ modules/discretedata/src/connectivity/structuredgrid.cpp
@@ -316,12 +316,12 @@ ind StructuredGrid::getNumCellsInDimension(ind dim) const {
 void StructuredGrid::getNumCells(std::vector<ind>& result) const {
     result.clear();
     for (ind numCells : numCellsPerDimension_) {
-        if (numCells > 0)
-            result.push_back(numCells);
-        else {
+        //if (numCells > 0)
+        result.push_back(numCells);
+        /*else {
             result.clear();
             return;
-        }
+        }*/
     }
 }
 
