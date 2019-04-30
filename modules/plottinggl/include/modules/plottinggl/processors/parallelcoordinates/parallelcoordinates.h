inviwo/inviwo

commit 547e4a6577539af7a8dd77b7e448908526692a11

PlottingGL: PCP brushing fix * filtering was occasionally not applied after deserialization

modules/plottinggl/src/processors/parallelcoordinates/parallelcoordinates.cpp
@@ -358,10 +358,8 @@ void ParallelCoordinates::createOrUpdateProperties() {
             axisProperties_.addProperty(newProp.release());
             return ptr;
         }();
-        prop->setParallelCoordinates(this);
         prop->setColumnId(axes_.size());
         prop->setVisible(true);
-        prop->updateFromColumn(c);
 
         // Create axis for rendering
         auto renderer = std::make_unique<AxisRenderer>(*prop);
@@ -379,6 +377,12 @@ void ParallelCoordinates::createOrUpdateProperties() {
 
         axes_.push_back({prop, std::move(renderer), std::move(slider)});
     }
+
+    for (auto& axis : axes_) {
+        axis.pcp->updateFromColumn(data->getColumn(axis.pcp->columnId()));
+        axis.pcp->setParallelCoordinates(this);
+    }
+    updateBrushing();
 }
 
 void ParallelCoordinates::buildLineMesh() {

modules/plottinggl/include/modules/plottinggl/processors/parallelcoordinates/pcpaxissettings.h
@@ -166,13 +166,7 @@ public:
     size_t columnId() const { return columnId_; }
     void setColumnId(size_t id) { columnId_ = id; }
 
-    void setParallelCoordinates(ParallelCoordinates* pcp) {
-        pcp_ = pcp;
-        labelSettings_.setSettings(this);
-        captionSettings_.setSettings(this);
-        major_.setSettings(this);
-        minor_.setSettings(this);
-    }
+    void setParallelCoordinates(ParallelCoordinates* pcp);
 
     const std::vector<bool>& getBrushed() const { return brushed_; }
 

modules/plottinggl/src/processors/parallelcoordinates/pcpaxissettings.cpp
@@ -144,16 +144,6 @@ void PCPAxisSettings::updateFromColumn(std::shared_ptr<const Column> col) {
             at = [vec = &dataVector](size_t idx) { return static_cast<double>(vec->at(idx)); };
         });
 
-    auto updateLables = [this]() {
-        const auto tickmarks = plot::getMajorTickPositions(major_, range);
-        labels_.clear();
-        const auto& format = pcp_->labelFormat_.get();
-        std::transform(tickmarks.begin(), tickmarks.end(), std::back_inserter(labels_),
-                       [&](auto tick) { return fmt::sprintf(format, tick); });
-    };
-    labelUpdateCallback_ = pcp_->labelFormat_.onChangeScoped(updateLables);
-    updateLables();
-
     range.propertyModified();
 }
 
@@ -239,6 +229,24 @@ void PCPAxisSettings::moveHandle(bool upper, double mouseY) {
         rangeTmp.x = value;
     }
     range.set(rangeTmp);
+}
+
+inline void PCPAxisSettings::setParallelCoordinates(ParallelCoordinates* pcp) {
+    pcp_ = pcp;
+    labelSettings_.setSettings(this);
+    captionSettings_.setSettings(this);
+    major_.setSettings(this);
+    minor_.setSettings(this);
+
+    auto updateLables = [this]() {
+        const auto tickmarks = plot::getMajorTickPositions(major_, range);
+        labels_.clear();
+        const auto& format = pcp_->labelFormat_.get();
+        std::transform(tickmarks.begin(), tickmarks.end(), std::back_inserter(labels_),
+                       [&](auto tick) { return fmt::sprintf(format, tick); });
+    };
+    labelUpdateCallback_ = pcp_->labelFormat_.onChangeScoped(updateLables);
+    updateLables();
 }
 
 void PCPAxisSettings::updateBrushing() {

commit 7b590dacd9a85364111dc8083df8c4575efce6eb

PlottingGL: PCP fix * fixed a crash caused by refactoring in 547e4a6

modules/plottinggl/include/modules/plottinggl/processors/parallelcoordinates/parallelcoordinates.h
@@ -215,6 +215,7 @@ private:
     int hoveredAxis_ = -1;
 
     bool brushingDirty_;
+    bool updating_ = false;
 };
 
 }  // namespace plot

modules/plottinggl/src/processors/parallelcoordinates/parallelcoordinates.cpp
@@ -342,6 +342,7 @@ void ParallelCoordinates::createOrUpdateProperties() {
     if (!data->getNumberOfRows()) return;
     if (!data->getNumberOfColumns()) return;
 
+    updating_ = true;
     axisPicking_.resize(data->getNumberOfColumns());
     lines_.axisFlipped.resize(data->getNumberOfColumns());
     for (size_t i = 0; i < data->getNumberOfColumns(); i++) {
@@ -393,6 +394,7 @@ void ParallelCoordinates::createOrUpdateProperties() {
         axis.pcp->updateFromColumn(data->getColumn(axis.pcp->columnId()));
         axis.pcp->setParallelCoordinates(this);
     }
+    updating_ = false;
     updateBrushing();
 }
 
@@ -760,6 +762,8 @@ void ParallelCoordinates::axisPicked(PickingEvent* p, size_t pickedID, PickType 
 void ParallelCoordinates::updateBrushing(PCPAxisSettings&) { updateBrushing(); }
 
 void ParallelCoordinates::updateBrushing() {
+    if (updating_) return;
+
     brushingDirty_ = false;
 
     auto iCol = dataFrame_.getData()->getIndexColumn();