b8d5fe1d219827af428b80837c94382cf7892937
Marius Kircher <marius.kircher@uni-ulm.de>
2019-08-28 19:50:30 -0700

    Crosshair: added support for showing slab thickness

--- modules/basegl/src/processors/simplecrosshairoverlay.cpp
+++ modules/basegl/src/processors/simplecrosshairoverlay.cpp
@@ -53,7 +53,7 @@ namespace inviwo {
         , cursorAngle_("cursorAngle", "Cursor Angle", 0.0f, 0.0f, glm::two_pi<float>())
         , cursorPos_("cursorPos", "Cursor Position", vec2(0.5f), vec2(0.0f), vec2(1.0f))
         , cursorRadius_("cursorRadius", "Cursor Radius", 0.1f, 0.0f, 1.0f)
-        , showCenterIndicator_("showCenterIndicator_", "Show Cursor Center", true)
+        , showCenterIndicator_("showCenterIndicator", "Show Cursor Center", true)
         , mouseEvent_("mouseEvent", "Mouse Event", [this](Event* e) { updateMouse(e); },
             MouseButton::Left, MouseState::Press | MouseState::Move | MouseState::Release)
         , interactionState_(InteractionState::NONE)
@@ -62,9 +62,16 @@ namespace inviwo {
         , color2_("color2", "Vertical Axis Color", vec4(1), vec4(0), vec4(1))
         , color3_("color3", "Border Color", vec4(1), vec4(0), vec4(1))
         , color4_("color4", "Center Indicator Color", vec4(1), vec4(0), vec4(1))
+		, color5_("color5", "Slab Color", vec4(1.f,1.f,1.f,.5f), vec4(0), vec4(1))
         , thickness1_("thickness1", "Crosshair Thickness", 2u, 0u, 20u)
         , thickness2_("thickness2", "Border Thickness", 2u, 0u, 20u)
+		, slab0Offset0_("slab0Offset0", "Slab H Offset 0", -0.01f, -1.0f, 1.0f, 0.001f)
+		, slab0Offset1_("slab0Offset1", "Slab H Offset 1", 0.01f, -1.0f, 1.0f, 0.001f)
+		, slab1Offset0_("slab1Offset0", "Slab V Offset 0", -0.01f, -1.0f, 1.0f, 0.001f)
+		, slab1Offset1_("slab1Offset1", "Slab V Offset 1", 0.01f, -1.0f, 1.0f, 0.001f)
+		, showSlabs_("showSlabs", "Show Slabs", true)
         , crosshairMesh_(nullptr)
+		, slabMesh_(nullptr)
         , outlineMesh_(nullptr)
         , cursorCenterMesh_(nullptr)
         , shader_("standard.vert", "standard.frag") {
@@ -90,9 +97,13 @@ namespace inviwo {
         addProperty(color3_);
         color4_.setSemantics(PropertySemantics::Color);
         addProperty(color4_);
+		color5_.setSemantics(PropertySemantics::Color);
+		addProperty(color5_);
 
         addProperty(thickness1_);
         addProperty(thickness2_);
+
+		addProperties(slab0Offset0_, slab0Offset1_, slab1Offset0_, slab1Offset1_, showSlabs_);
     }
 
     void SimpleCrosshairOverlay::process() {
@@ -108,6 +119,7 @@ namespace inviwo {
         const auto pos = cursorPos_.get() * 2.0f - 1.0f;
 
         crosshairMesh_ = std::make_shared<Mesh>(DrawType::Triangles, ConnectivityType::None);
+		slabMesh_ = std::make_shared<Mesh>(DrawType::Triangles, ConnectivityType::None);
         outlineMesh_ = std::make_shared<Mesh>(DrawType::Triangles, ConnectivityType::None);
         cursorCenterMesh_ = std::make_shared<Mesh>(DrawType::Lines, ConnectivityType::Loop);
 
@@ -119,11 +131,29 @@ namespace inviwo {
             // vertical
             vec2(pos.x + thicknessCrosshair, bar_length), vec2(pos.x - thicknessCrosshair, bar_length), vec2(pos.x - thicknessCrosshair, -bar_length), // left triangle
             vec2(pos.x + thicknessCrosshair, -bar_length), vec2(pos.x + thicknessCrosshair, bar_length), vec2(pos.x - thicknessCrosshair, -bar_length) // right triangle
-            }));
-        crosshairMesh_->addBuffer(BufferType::ColorAttrib, util::makeBuffer<vec4>(std::vector<vec4>{
-            color1_, color1_, color1_, color1_, color1_, color1_,
-                color2_, color2_, color2_, color2_, color2_, color2_
         }));
+        crosshairMesh_->addBuffer(BufferType::ColorAttrib, util::makeBuffer<vec4>(std::vector<vec4>{
+            color1_, color1_, color1_, 
+			color1_, color1_, color1_,
+            color2_, color2_, color2_, 
+			color2_, color2_, color2_
+        }));
+
+		// Create background for crosshair to show slab thickness (useful in MPR)
+		slabMesh_->addBuffer(BufferType::PositionAttrib, util::makeBuffer<vec2>({
+			// background horizontal
+			vec2(bar_length, pos.y + slab0Offset0_), vec2(-bar_length, pos.y + slab0Offset0_), vec2(-bar_length, pos.y + slab0Offset1_), // upper triangle
+			vec2(bar_length, pos.y + slab0Offset1_), vec2(bar_length, pos.y + slab0Offset0_), vec2(-bar_length, pos.y + slab0Offset1_), // lower triangle
+			// background vertical
+			vec2(pos.x + slab1Offset1_, bar_length), vec2(pos.x + slab1Offset0_, bar_length), vec2(pos.x + slab1Offset0_, -bar_length), // left triangle
+			vec2(pos.x + slab1Offset1_, -bar_length), vec2(pos.x + slab1Offset1_, bar_length), vec2(pos.x + slab1Offset0_, -bar_length) // right triangle
+		}));
+		slabMesh_->addBuffer(BufferType::ColorAttrib, util::makeBuffer<vec4>(std::vector<vec4>{
+			color5_, color5_, color5_,
+			color5_, color5_, color5_,
+			color5_, color5_, color5_,
+			color5_, color5_, color5_
+		}));
 
         // Create viewport outline as useful indicator when having multiple views, e.g. in MPR
         outlineMesh_->addBuffer(BufferType::PositionAttrib, util::makeBuffer<vec2>({
@@ -161,6 +191,7 @@ namespace inviwo {
 
         utilgl::activateTargetAndCopySource(imageOut_, imageIn_, ImageType::ColorDepth);
         utilgl::DepthFuncState depth(GL_ALWAYS);
+		utilgl::BlendModeState blendModeStateGL(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         shader_.activate();
 
         const mat4 scale(mat2(1, 0, 0, aspect_ratio));
@@ -170,7 +201,10 @@ namespace inviwo {
         mat4 transl2(1); transl2[3] = vec4(pos, 0, 1);
 
         shader_.setUniform("dataToClip", transl2 * scale * rot * transl1);
-        MeshDrawerGL(crosshairMesh_.get()).draw();
+		if (showSlabs_.get()) {
+			MeshDrawerGL(slabMesh_.get()).draw();
+		}
+		MeshDrawerGL(crosshairMesh_.get()).draw();
         if (showCenterIndicator_.get()) {
             MeshDrawerGL(cursorCenterMesh_.get()).draw();
         }
