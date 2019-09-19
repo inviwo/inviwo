8acc10c9f8e1be70c2bcbd22ffc8230d4df6a5a1
Wiebke Köpp <wiebke.koepp@googlemail.com>
2019-09-19 14:13:10 +0200

    Add missing property include

--- modules/discretedata/include/modules/discretedata/processors/createchannel.h
+++ modules/discretedata/include/modules/discretedata/processors/createchannel.h
@@ -33,6 +33,7 @@
 #include <inviwo/core/common/inviwo.h>
 #include <inviwo/core/processors/processor.h>
 #include <inviwo/core/properties/optionproperty.h>
+#include <inviwo/core/properties/stringproperty.h>
 #include <modules/discretedata/ports/datasetport.h>
 #include <modules/discretedata/channels/analyticchannel.h>
 #include <modules/discretedata/channels/channeldispatching.h>
