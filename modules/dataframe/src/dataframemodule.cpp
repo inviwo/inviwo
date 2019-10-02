ea4b7b0148ffb4a083781c603c74051ce1a13ade
Daniel Jönsson <daniel.jonsson@liu.se>
2019-10-02 16:28:11 +0200

    DataFrame: Added DataFrameColumnProperty to StringProperty converter

--- modules/dataframe/src/dataframemodule.cpp
+++ modules/dataframe/src/dataframemodule.cpp
@@ -36,11 +36,12 @@
 #include <inviwo/dataframe/processors/syntheticdataframe.h>
 #include <inviwo/dataframe/processors/volumetodataframe.h>
 #include <inviwo/dataframe/processors/volumesequencetodataframe.h>
-
 #include <inviwo/dataframe/properties/colormapproperty.h>
 
 #include <inviwo/dataframe/io/csvreader.h>
 #include <inviwo/dataframe/io/jsonreader.h>
+
+#include <inviwo/core/properties/propertyconverter.h>
 
 #include <modules/json/jsonmodule.h>
 
@@ -62,12 +63,13 @@ DataFrameModule::DataFrameModule(InviwoApplication* app) : InviwoModule(app, "Da
     // Properties
     registerProperty<ColormapProperty>();
     registerProperty<DataFrameColumnProperty>();
-
+    
     // Readers and writes
     registerDataReader(std::make_unique<CSVReader>());
     registerDataReader(std::make_unique<JSONDataFrameReader>());
 
     // Data converters
+    registerPropertyConverter(std::make_unique<OptionToStringConverter<DataFrameColumnProperty>>());
     app->getModuleByType<JSONModule>()->registerPropertyJSONConverter<DataFrameColumnProperty>();
 }
 
