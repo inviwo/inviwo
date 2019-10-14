e31f381decbf594a4f7d8163bf929642b2fcf496
Peter Steneteg <peter@steneteg.se>
2019-10-14 19:18:52 +0200

    Core: Added a utility class to redirect a stream to the logcentral

---
+++ include/inviwo/core/util/logstream.h
@@ -0,0 +1,66 @@
+/*********************************************************************************
+ *
+ * Inviwo - Interactive Visualization Workshop
+ *
+ * Copyright (c) 2019 Inviwo Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************************/
+#pragma once
+
+#include <inviwo/core/common/inviwocoredefine.h>
+#include <inviwo/core/util/logcentral.h>
+
+#include <streambuf>
+#include <string>
+#include <ostream>
+#include <string_view>
+#include <mutex>
+
+namespace inviwo {
+
+class IVW_CORE_API LogStream : public std::streambuf {
+    using int_type = typename std::streambuf::int_type;
+    using size_type = typename std::string::size_type;
+    using traits = typename std::string::traits_type;
+
+public:
+    LogStream(std::ostream& stream, std::string source, LogLevel level, LogAudience audience);
+    ~LogStream();
+    std::streamsize xsputn(const char* s, std::streamsize count) override;
+
+protected:
+    virtual int sync() override;
+
+private:
+    std::string source_;
+    LogLevel level_;
+    LogAudience audience_;
+
+    std::string buffer_;
+    std::ostream& stream_;
+    std::streambuf* orgBuffer_;
+    std::mutex mutex_; 
+};
+
+}  // namespace inviwo
