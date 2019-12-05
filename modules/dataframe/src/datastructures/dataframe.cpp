de2acd1ffb94937256040c36edd4a7c5ccacac95
Jochen Jankowai <jochen.jankowai@liu.se>
2019-12-05 22:52:37 +0100

    dropColumn erase-remove

--- modules/dataframe/src/datastructures/dataframe.cpp
+++ modules/dataframe/src/datastructures/dataframe.cpp
@@ -71,8 +71,10 @@ std::shared_ptr<Column> DataFrame::addColumnFromBuffer(const std::string &identi
 }
 
 void DataFrame::dropColumn(const std::string &header) {
-    std::remove(std::begin(columns_), std::end(columns_),
-                [header](std::shared_ptr<Column> col) { return col->getHeader() == header; });
+    columns_.erase(
+        std::remove(std::begin(columns_), std::end(columns_),
+                    [header](std::shared_ptr<Column> col) { return col->getHeader() == header; }),
+        std::end(columns_));
 }
 
 std::shared_ptr<CategoricalColumn> DataFrame::addCategoricalColumn(const std::string &header,
