d8255cacabf0529658f779df8e3fb6c72fa83247
Martin Falk <martin.falk@liu.se>
2020-01-08 14:10:58 +0100

    Core: Interpolation refactoring * minor refactoring for better inlining

--- include/inviwo/core/util/interpolation.h
+++ include/inviwo/core/util/interpolation.h
@@ -27,8 +27,7 @@
  *
  *********************************************************************************/
 
-#ifndef IVW_INTERPOLATION_H
-#define IVW_INTERPOLATION_H
+#pragma once
 
 #include <inviwo/core/common/inviwocoredefine.h>
 #include <inviwo/core/common/inviwo.h>
@@ -95,26 +94,46 @@ inline T Interpolation<T, P>::linearVector(const T &a, const T &b, P x) {
 template <typename T, typename P>
 inline T Interpolation<T, P>::bilinear(const T &a, const T &b, const T &c, const T &d,
                                        const Vector<2, P> &interpolants) {
-    return linear(linear(a, b, interpolants.x), linear(c, d, interpolants.x), interpolants.y);
+    const T l1 = glm::mix(a, b, interpolants.x);
+    const T l2 = glm::mix(c, d, interpolants.x);
+
+    return glm::mix(l1, l2, interpolants.y);
 }
 
 template <typename T, typename P>
 inline T Interpolation<T, P>::bilinear(const T samples[4], const Vector<2, P> &interpolants) {
-    return bilinear(samples[0], samples[1], samples[2], samples[3], interpolants);
+    const T l1 = glm::mix(samples[0], samples[1], interpolants.x);
+    const T l2 = glm::mix(samples[2], samples[3], interpolants.x);
+
+    return glm::mix(l1, l2, interpolants.y);
 }
 
 template <typename T, typename P>
 inline T Interpolation<T, P>::trilinear(const T &a, const T &b, const T &c, const T &d, const T &e,
                                         const T &f, const T &g, const T &h,
                                         const Vector<3, P> &interpolants) {
-    return linear(bilinear(a, b, c, d, Vector<2, P>(interpolants)),
-                  bilinear(e, f, g, h, Vector<2, P>(interpolants)), interpolants.z);
+    const T l1 = glm::mix(a, b, interpolants.x);
+    const T l2 = glm::mix(c, d, interpolants.x);
+    const T l3 = glm::mix(e, f, interpolants.x);
+    const T l4 = glm::mix(g, h, interpolants.x);
+
+    const T b1 = glm::mix(l1, l2, interpolants.y);
+    const T b2 = glm::mix(l3, l4, interpolants.y);
+
+    return glm::mix(b1, b2, interpolants.z);
 }
 
 template <typename T, typename P>
 inline T Interpolation<T, P>::trilinear(const T samples[8], const Vector<3, P> &interpolants) {
-    return trilinear(samples[0], samples[1], samples[2], samples[3], samples[4], samples[5],
-                     samples[6], samples[7], interpolants);
+    const T l1 = glm::mix(samples[0], samples[1], interpolants.x);
+    const T l2 = glm::mix(samples[2], samples[3], interpolants.x);
+    const T l3 = glm::mix(samples[4], samples[5], interpolants.x);
+    const T l4 = glm::mix(samples[6], samples[7], interpolants.x);
+
+    const T b1 = glm::mix(l1, l2, interpolants.y);
+    const T b2 = glm::mix(l3, l4, interpolants.y);
+
+    return glm::mix(b1, b2, interpolants.z);
 }
 
 template <typename T, typename P>
@@ -134,5 +153,3 @@ inline T Interpolation<T, P>::quadlinear(const T samples[16], const Vector<4, P>
 }
 
 }  // namespace inviwo
-
-#endif  // IVW_INTERPOLATION_H
