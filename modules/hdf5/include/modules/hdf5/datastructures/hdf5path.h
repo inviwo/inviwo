fd62d20d66e46a13c18608aec3142a695545f27b
Peter Steneteg <peter@steneteg.se>
2020-04-21 11:05:52 +0200

    HDF5: export bug fix

--- modules/hdf5/include/modules/hdf5/datastructures/hdf5path.h
+++ modules/hdf5/include/modules/hdf5/datastructures/hdf5path.h
@@ -70,8 +70,7 @@ IVW_MODULE_HDF5_API Path operator+(const Path &lhs, const Path &rhs);
 IVW_MODULE_HDF5_API Path operator+(const Path &lhs, const std::string &rhs);
 
 template <typename CTy, typename CTr>
-IVW_MODULE_HDF5_API std::basic_ostream<CTy, CTr> &operator<<(std::basic_ostream<CTy, CTr> &os,
-                                                             const Path &path) {
+std::basic_ostream<CTy, CTr> &operator<<(std::basic_ostream<CTy, CTr> &os, const Path &path) {
     return os << path.toString();
 }
 
