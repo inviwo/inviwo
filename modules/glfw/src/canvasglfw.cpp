1ea755a6e52c9cb0b8321d987d6a2a3a6f5e2641
Peter Steneteg <peter@steneteg.se>
2020-06-05 17:01:38 +0200

    GLFW: chash fix test

--- modules/glfw/src/canvasglfw.cpp
+++ modules/glfw/src/canvasglfw.cpp
@@ -40,6 +40,8 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
+#include <fmt/format.h>
+
 namespace inviwo {
 
 GLFWwindow* CanvasGLFW::sharedContext_ = nullptr;
@@ -216,33 +218,54 @@ void CanvasGLFW::provideExternalContext(GLFWwindow* sharedContext) {
 GLFWwindow* CanvasGLFW::sharedContext() { return sharedContext_; }
 
 ivec2 CanvasGLFW::movePointOntoDesktop(ivec2 pos, ivec2 size) {
-    int count;
-    GLFWmonitor** monitors = glfwGetMonitors(&count);
+    auto check_error = []() {
+        const char* description;
+        int code = glfwGetError(&description);
+        if (description) {
+            throw Exception(fmt::format("GLFW Error: {} {}", code, description));
+        }
+    };
 
-    for (int i = 0; i < count; ++i) {
-        GLFWmonitor* monitor = monitors[i];
+    try {
+        int count;
+        GLFWmonitor** monitors = glfwGetMonitors(&count);
+        check_error();
+
+        for (int i = 0; i < count; ++i) {
+            GLFWmonitor* monitor = monitors[i];
+            ivec2 screenPos{};
+            ivec2 screenSize{};
+            glfwGetMonitorWorkarea(monitor, &screenPos.x, &screenPos.y, &screenSize.x,
+                                   &screenSize.y);
+            check_error();
+
+            if (glm::all(glm::greaterThanEqual(pos, screenPos)) &&
+                glm::all(glm::lessThanEqual(pos, screenPos + screenSize))) {
+
+                return pos;
+            }
+
+            if (glm::all(glm::greaterThanEqual(pos + size, screenPos)) &&
+                glm::all(glm::lessThanEqual(pos + size, screenPos + screenSize))) {
+
+                return glm::clamp(pos, screenPos, screenPos + screenSize);
+            }
+        }
+
+        auto monitor = glfwGetPrimaryMonitor();
+        check_error();
         ivec2 screenPos{};
         ivec2 screenSize{};
         glfwGetMonitorWorkarea(monitor, &screenPos.x, &screenPos.y, &screenSize.x, &screenSize.y);
-
-        if (glm::all(glm::greaterThanEqual(pos, screenPos)) &&
-            glm::all(glm::lessThanEqual(pos, screenPos + screenSize))) {
-
-            return pos;
-        }
-
-        if (glm::all(glm::greaterThanEqual(pos + size, screenPos)) &&
-            glm::all(glm::lessThanEqual(pos + size, screenPos + screenSize))) {
-
-            return glm::clamp(pos, screenPos, screenPos + screenSize);
-        }
+        check_error();
+        return glm::clamp(pos, screenPos, screenPos + screenSize);
+    } catch (const Exception& e) {
+        LogErrorCustom("CanvasGLFW", e.getMessage());
+        return pos;
+    } catch (...) {
+        LogErrorCustom("CanvasGLFW", "MovePointOntoDestop unknown exception");
+        return pos;
     }
-
-    auto monitor = glfwGetPrimaryMonitor();
-    ivec2 screenPos{};
-    ivec2 screenSize{};
-    glfwGetMonitorWorkarea(monitor, &screenPos.x, &screenPos.y, &screenSize.x, &screenSize.y);
-    return glm::clamp(pos, screenPos, screenPos + screenSize);
 }
 
 }  // namespace inviwo
