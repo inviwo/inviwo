037c14a831486fff3d38e55f896da9d17f376cb3
Daniel Jönsson <daniel.jonsson@liu.se>
2020-07-06 18:30:40 +0200

    Webbrowser: Webbrowser: Regression test update

--- modules/webbrowser/tests/regression/web-property-sync/script.py
+++ modules/webbrowser/tests/regression/web-property-sync/script.py
@@ -1,7 +1,9 @@
 # Inviwo Python script 
 import inviwopy
 import ivw.regression
+import ivw.utils as inviwo_utils
 import time
+
 
 network = inviwopy.app.network;
 lineWidth = network.LineRenderer.lineSettings.lineWidth;
@@ -9,6 +11,7 @@ lineWidth.value = 2;
 while (network.Webbrowser.isLoading.value): 
     time.sleep(0.01);
 
+inviwo_utils.update()
 canvas = network.Canvas;
 ivw.regression.saveCanvas(canvas, "Canvas");
 
