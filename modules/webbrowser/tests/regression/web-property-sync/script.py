3e4aa3a2d8de6747bed89cadbdb7754bf6c66464
Daniel Jönsson <daniel.jonsson@liu.se>
2020-08-17 11:57:02 +0200

    Webbrowser: Fix regression test

--- modules/webbrowser/tests/regression/web-property-sync/script.py
+++ modules/webbrowser/tests/regression/web-property-sync/script.py
@@ -1,17 +1,14 @@
 # Inviwo Python script 
 import inviwopy
 import ivw.regression
-import ivw.utils as inviwo_utils
 import time
-
 
 network = inviwopy.app.network;
 lineWidth = network.LineRenderer.lineSettings.lineWidth;
 # Line width will be set to two when the webpage has loaded
 while (lineWidth.value != 2): 
-    time.sleep(0.01);
+    inviwopy.qt.update();
 
-inviwo_utils.update()
 canvas = network.Canvas;
 ivw.regression.saveCanvas(canvas, "Canvas");
 
