19cfc811d298ea13a0df1b7f97c56042cb78a517
Daniel Jönsson <daniel.jonsson@liu.se>
2020-08-18 10:22:01 +0200

    Webbrowser: Regression test

--- modules/webbrowser/tests/regression/web-property-sync/script.py
+++ modules/webbrowser/tests/regression/web-property-sync/script.py
@@ -8,7 +8,7 @@ lineWidth = network.LineRenderer.lineSettings.lineWidth;
 # Line width will be set to two when the webpage has loaded
 while (lineWidth.value != 2): 
     inviwopy.qt.update();
-
+inviwopy.qt.update();
 canvas = network.Canvas;
 ivw.regression.saveCanvas(canvas, "Canvas");
 
