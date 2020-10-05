d9ca98878c955fb6032fae5b942625ecd5f620eb
Inviwo Team <team@inviwo.org>
2020-10-05 13:03:21 +0200

    Jenkins: Format fixes

--- tools/meta/src/creator.cpp
+++ tools/meta/src/creator.cpp
@@ -47,7 +47,7 @@
 #include <nlohmann/json.hpp>
 
 #ifdef WIN32
-#include <ciso646> // required for logical operators 'not', 'and', ... used by inja.hpp
+#include <ciso646>  // required for logical operators 'not', 'and', ... used by inja.hpp
 #endif
 #include <inja.hpp>
 #include <warn/pop>
