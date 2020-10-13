ff978e0956f579abf5cf8ff3dd58f33a1f029596
Martin Falk <martin.falk@liu.se>
2020-10-13 17:19:44 +0200

    PlottingGL: warn fix

--- modules/plottinggl/src/processors/parallelcoordinates/pcpaxissettings.cpp
+++ modules/plottinggl/src/processors/parallelcoordinates/pcpaxissettings.cpp
@@ -115,7 +115,6 @@ void PCPAxisSettings::updateFromColumn(std::shared_ptr<const Column> col) {
 
     col->getBuffer()->getRepresentation<BufferRAM>()->dispatch<void, dispatching::filter::Scalars>(
         [&](auto ram) -> void {
-            using T = typename util::PrecisionValueType<decltype(ram)>;
             auto& dataVector = ram->getDataContainer();
 
             auto minMax = util::bufferMinMax(ram, IgnoreSpecialValues::Yes);
