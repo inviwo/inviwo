3c591402b44ba57b096f67c1f724b719f8960df7
Daniel Jönsson <daniel.jonsson@liu.se>
2020-10-26 00:31:43 +0100

    Animation: Revert template export definitions

--- modules/animation/include/modules/animation/animationmoduledefine.h
+++ modules/animation/include/modules/animation/animationmoduledefine.h
@@ -6,33 +6,18 @@
 #ifdef IVW_MODULE_ANIMATION_EXPORTS
 #ifdef _WIN32
 #define IVW_MODULE_ANIMATION_API __declspec(dllexport)
-#define IVW_MODULE_ANIMATION_EXT
-#define IVW_MODULE_ANIMATION_TMPL_EXP
-#define IVW_MODULE_ANIMATION_TMPL_INST __declspec(dllexport)
 #else  // UNIX (GCC)
 #define IVW_MODULE_ANIMATION_API __attribute__((visibility("default")))
-#define IVW_MODULE_ANIMATION_EXT
-#define IVW_MODULE_ANIMATION_TMPL_EXP __attribute__((__visibility__("default")))
-#define IVW_MODULE_ANIMATION_TMPL_INST
 #endif
 #else
 #ifdef _WIN32
 #define IVW_MODULE_ANIMATION_API __declspec(dllimport)
-#define IVW_MODULE_ANIMATION_EXT extern
-#define IVW_MODULE_ANIMATION_TMPL_EXP __declspec(dllimport)
-#define IVW_MODULE_ANIMATION_TMPL_INST
 #else
 #define IVW_MODULE_ANIMATION_API
-#define IVW_MODULE_ANIMATION_EXT extern
-#define IVW_MODULE_ANIMATION_TMPL_EXP __attribute__((__visibility__("default")))
-#define IVW_MODULE_ANIMATION_TMPL_INST
 #endif
 #endif
 #else  // STATIC
 #define IVW_MODULE_ANIMATION_API
-#define IVW_MODULE_ANIMATION_EXT extern
-#define IVW_MODULE_ANIMATION_TMPL_EXP
-#define IVW_MODULE_ANIMATION_TMPL_INST
 #endif
 
 #endif /* _IVW_MODULE_ANIMATION_DEFINE_H_ */
