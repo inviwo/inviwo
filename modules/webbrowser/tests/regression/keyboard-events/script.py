9cd10baeed34a8fe7161e8dbdb1dfd794a2fab87
Daniel Jönsson <daniel.jonsson@liu.se>
2020-11-25 22:59:09 +0100

    Webbrowser: Added KeyboardEvent regression test

---
+++ modules/webbrowser/tests/regression/keyboard-events/script.py
@@ -0,0 +1,18 @@
+# Inviwo Python script 
+import inviwopy
+import ivw.regression
+import time
+
+network = inviwopy.app.network
+key_a = inviwopy.KeyboardEvent(key = inviwopy.IvwKey.A, state = inviwopy.KeyState.Press, nativeVirtualKey = 65, utfText = 'a')
+browser = network.Webbrowser
+browser.invokeEvent(key_a)
+key_A = inviwopy.KeyboardEvent(key = inviwopy.IvwKey.A, state = inviwopy.KeyState.Press, modifiers = inviwopy.KeyModifiers(inviwopy.KeyModifier.Shift), nativeVirtualKey = 65, utfText = 'A')
+browser.invokeEvent(key_A)
+
+# HACK: Wait for as little as possible while ensuring that the webpage has re-rendered. 
+#time.sleep(0.1);
+
+canvas = network.Canvas;
+ivw.regression.saveCanvas(canvas, "Canvas");
+
