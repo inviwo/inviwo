626360632fd71dbd7bec03a05777a6a6feef83aa
Daniel Jönsson <daniel.jonsson@liu.se>
2020-12-02 11:11:53 +0100

    Webbrowser: Regression test updates

--- modules/webbrowser/tests/regression/keyboard-events/script.py
+++ modules/webbrowser/tests/regression/keyboard-events/script.py
@@ -8,11 +8,10 @@ network = inviwopy.app.network
 browser = network.Webbrowser
 canvas = network.Canvas;
 
-# HACK: Wait for as little as possible while ensuring that the webpage has rendered. 
-while canvas.image.colorLayers[0].data[80, 67, 0] == 255:
-    time.sleep(0.15);
-    print("Waiting for pixel [80, 67] to change color.")
+# HACK: Wait until page is loaded. Color will be set to 1
+while (network.Background.bgColor2.value[0] != 1): 
     inviwopy.qt.update();
+
 # Send key events
 network.lock()
 key_a = inviwopy.KeyboardEvent(key = inviwopy.IvwKey.A, state = inviwopy.KeyState.Press, nativeVirtualKey = 65, utfText = 'a')
@@ -20,6 +19,11 @@ browser.invokeEvent(key_a)
 key_A = inviwopy.KeyboardEvent(key = inviwopy.IvwKey.A, state = inviwopy.KeyState.Press, modifiers = inviwopy.KeyModifiers(inviwopy.KeyModifier.Shift), nativeVirtualKey = 65, utfText = 'A')
 browser.invokeEvent(key_A)
 network.unlock()
+
+# HACK: Wait for as little as possible while ensuring that the webpage has rendered.
+while canvas.image.colorLayers[0].data[80, 67, 0] == 255:
+    print("Waiting for pixel [80, 67] to change color.")
+    inviwopy.qt.update();
 
 ivw.regression.saveCanvas(canvas, "Canvas")
 
