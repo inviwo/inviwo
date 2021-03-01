50f0daa1309f0b0de0eeb3366210edd6d062f10c
Martin Falk <martin.falk@liu.se>
2021-03-01 16:10:55 +0100

    Core: Apple fix for debugbreak
    * added check if debugger is present on Apple

--- src/core/util/assertion.cpp
+++ src/core/util/assertion.cpp
@@ -34,8 +34,43 @@
 
 #ifndef WIN32
 #include <signal.h>
-#else
+#endif
+
+#if defined(WIN32)
 extern "C" __declspec(dllimport) int __stdcall IsDebuggerPresent();
+#elif defined(__APPLE__)
+#include <assert.h>
+#include <unistd.h>
+#include <sys/sysctl.h>
+
+// Function is taken from https://developer.apple.com/library/mac/qa/qa1361/_index.html
+// Returns true if the current process is being debugged (either running under the
+// debugger or has a debugger attached post facto).
+static bool runningInDebugger() {
+    int junk;
+    int mib[4];
+    struct kinfo_proc info;
+    size_t size;
+
+    // Initialize the flags so that, if sysctl fails for some bizarre
+    // reason, we get a predictable result.
+    info.kp_proc.p_flag = 0;
+
+    // Initialize mib, which tells sysctl the info we want, in this case
+    // we're looking for information about a specific process ID.
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_PROC;
+    mib[2] = KERN_PROC_PID;
+    mib[3] = getpid();
+
+    // Call sysctl.
+    size = sizeof(info);
+    junk = sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, NULL, 0);
+    ghoul_assert(junk == 0, "sysctl call failed");
+
+    // We're being debugged if the P_TRACED flag is set.
+    return ((info.kp_proc.p_flag & P_TRACED) != 0);
+}
 #endif
 
 namespace inviwo {
@@ -65,6 +100,10 @@ void util::debugBreak() {
 #ifdef WIN32
     if (IsDebuggerPresent()) {
         __debugbreak();
+    }
+#elif __APPLE__
+    if (runningInDebugger()) {
+        raise(SIGTRAP);
     }
 #else
     raise(SIGTRAP);
