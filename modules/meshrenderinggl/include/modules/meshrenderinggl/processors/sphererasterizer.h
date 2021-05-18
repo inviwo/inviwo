fe6ebdb7c4f5e9493da565a38fa5741d65e16a5f
Martin Falk <martin.falk@liu.se>
2021-05-18 20:46:35 +0200

    BaseGL,OpenGL,MeshRenderingGL: requested changes
    * refactoring of common glyph shader functions (glyphs.glsl)

--- modules/meshrenderinggl/include/modules/meshrenderinggl/processors/sphererasterizer.h
+++ modules/meshrenderinggl/include/modules/meshrenderinggl/processors/sphererasterizer.h
@@ -63,9 +63,9 @@ namespace inviwo {
  * ### Inports
  *   * __geometry__ Input meshes
  *       The input mesh uses the following buffers:
- * PositionAttrib vec3
- * ColorAttrib    vec4   (optional will fall-back to use __Custom Color__)
- * RadiiAttrib    float  (optional will fall-back to use __Custom Radius__)
+ *          PositionAttrib vec3
+ *          ColorAttrib    vec4   (optional will fall-back to use __Custom Color__)
+ *          RadiiAttrib    float  (optional will fall-back to use __Custom Radius__)
  *
  * ### Outports
  *   * __rasterization__ rasterization functor rendering  the molecule either opaquely or into
@@ -75,7 +75,7 @@ namespace inviwo {
  *   * __Render Mode__               render only input meshes marked as points or everything
  *   * __Clip Mode__                 defines the handling of spheres clipped at the camera
  *   * __Clip Surface Adjustment__   brighten/darken glyph color on clip surface
- *   * __Shaded Clipped Area__       enable illumination computations for the clipped surface
+ *   * __Shade Clipped Area__        enable illumination computations for the clipped surface
  *   * __Force Radius__              enable a fixed user-defined radius for all spheres
  *   * __Default Radius__            radius of the rendered spheres (in world coordinates)
  *   * __Force Color__               if enabled, all spheres will share the same custom color
