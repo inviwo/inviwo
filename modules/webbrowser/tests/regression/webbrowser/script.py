b34c7a1d10a2f6780d1b901bf3a9371a08fa9658
Martin Falk <martin.falk@liu.se>
2021-05-25 13:47:26 +0200

    Testing: minor adjustments

--- modules/webbrowser/tests/regression/webbrowser/script.py
+++ modules/webbrowser/tests/regression/webbrowser/script.py
@@ -4,13 +4,8 @@ import ivw.regression
 import time
 
 network = inviwopy.app.network;
-lineWidth = network.LineRenderer.lineSettings.lineWidth;
-# Line width will be set to two when the webpage has loaded
-while (lineWidth.value != 2): 
-    inviwopy.qt.update();
 # HACK: Wait for as little as possible while ensuring that the webpage has re-rendered. 
 time.sleep(1.5);
 inviwopy.qt.update();
 canvas = network.Canvas;
 ivw.regression.saveCanvas(canvas, "Canvas");
-
