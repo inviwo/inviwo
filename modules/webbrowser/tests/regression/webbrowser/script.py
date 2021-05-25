46cec7718df71da8f29993cc6700af876745d841
Martin Falk <martin.falk@liu.se>
2021-05-25 16:10:03 +0200

    Testing: increase timeout in webbrowser

--- modules/webbrowser/tests/regression/webbrowser/script.py
+++ modules/webbrowser/tests/regression/webbrowser/script.py
@@ -5,7 +5,7 @@ import time
 
 network = inviwopy.app.network;
 # HACK: Wait for as little as possible while ensuring that the webpage has re-rendered. 
-time.sleep(1.5);
+time.sleep(5.0);
 inviwopy.qt.update();
 canvas = network.Canvas;
 ivw.regression.saveCanvas(canvas, "Canvas");
