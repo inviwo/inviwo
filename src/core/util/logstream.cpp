b4f86981afd7cf69f6382a9c8d4a31be134256bc
Peter Steneteg <peter@steneteg.se>
2021-08-18 11:31:34 +0200

    Core: logstream, strip console color codes

--- src/core/util/logstream.cpp
+++ src/core/util/logstream.cpp
@@ -28,6 +28,7 @@
  *********************************************************************************/
 
 #include <inviwo/core/util/logstream.h>
+#include <inviwo/core/util/stringconversion.h>
 
 namespace inviwo {
 
@@ -44,12 +45,38 @@ LogStream::~LogStream() {
     stream_.rdbuf(orgBuffer_);
 }
 
+namespace {
+constexpr std::string_view bold = "\33[0;1m";
+constexpr std::string_view grey = "\33[2;1m";
+constexpr std::string_view italic = "\33[3;1m";
+constexpr std::string_view underlined = "\33[4;1m";
+constexpr std::string_view flashing = "\33[5;1m";
+constexpr std::string_view inverted = "\33[7;1m";
+constexpr std::string_view strikethrough = "\33[9;1m";
+constexpr std::string_view darkgrey = "\33[30;1m";
+constexpr std::string_view red = "\33[31;1m";
+constexpr std::string_view green = "\33[32;1m";
+constexpr std::string_view yellow = "\33[33;1m";
+constexpr std::string_view blue = "\33[34;1m";
+constexpr std::string_view pink = "\33[35;1m";
+constexpr std::string_view lightblue = "\33[36;1m";
+constexpr std::string_view brightwhite = "\33[37;1m";
+constexpr std::string_view endcolor = "\33[0m";
+}  // namespace
+
 std::streamsize LogStream::xsputn(const char* s, std::streamsize count) {
     std::scoped_lock lock{mutex_};
     std::copy(s, s + count, std::back_inserter(buffer_));
     const auto found = buffer_.find_last_of('\n');
     if (found != std::string::npos) {
-        LogCentral::getPtr()->log(source_, level_, audience_, "", "", 0, buffer_.substr(0, found));
+        auto line = buffer_.substr(0, found);
+        for (auto&& elem :
+             {bold, grey, italic, underlined, flashing, inverted, strikethrough, darkgrey, red,
+              green, yellow, blue, pink, lightblue, brightwhite, endcolor}) {
+            replaceInString(line, elem, "");
+        }
+
+        LogCentral::getPtr()->log(source_, level_, audience_, "", "", 0, line);
         buffer_.erase(buffer_.begin(), buffer_.begin() + found + 1);
     }
 
@@ -59,6 +86,11 @@ std::streamsize LogStream::xsputn(const char* s, std::streamsize count) {
 int LogStream::sync() {
     std::scoped_lock lock{mutex_};
     if (!buffer_.empty()) {
+        for (auto&& elem :
+             {bold, grey, italic, underlined, flashing, inverted, strikethrough, darkgrey, red,
+              green, yellow, blue, pink, lightblue, brightwhite, endcolor}) {
+            replaceInString(buffer_, elem, "");
+        }
         LogCentral::getPtr()->log(source_, level_, audience_, "", "", 0, buffer_);
         buffer_.clear();
     }
