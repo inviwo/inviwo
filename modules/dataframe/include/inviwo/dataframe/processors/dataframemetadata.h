b42e05452bad954109a5ca1892f9a7ae4a19b989
Martin Falk <martin.falk@liu.se>
2021-10-29 00:18:30 +0200

    DataFrame: processor for overriding DataFrame metadata

---
+++ modules/dataframe/include/inviwo/dataframe/processors/dataframemetadata.h
@@ -0,0 +1,66 @@
+/*********************************************************************************
+ *
+ * Inviwo - Interactive Visualization Workshop
+ *
+ * Copyright (c) 2021 Inviwo Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************************/
+
+#pragma once
+
+#include <inviwo/dataframe/dataframemoduledefine.h>
+#include <inviwo/core/processors/processor.h>
+#include <inviwo/dataframe/properties/columnmetadatalistproperty.h>
+#include <inviwo/dataframe/datastructures/dataframe.h>
+
+namespace inviwo {
+
+/** \docpage{org.inviwo.DataFrameMetaData, Data Frame Meta Data}
+ * ![](org.inviwo.DataFrameMetaData.png?classIdentifier=org.inviwo.DataFrameMetaData)
+ * Augment a DataFrame with column-specific metadata like minimum and maximum values.
+ *
+ * ### Inports
+ *   * __inport__ input DataFrame to be augmented
+ *
+ * ### Outports
+ *   * __outport__ copy of the input DataFrame along with column MetaData
+ */
+class IVW_MODULE_DATAFRAME_API DataFrameMetaData : public Processor {
+public:
+    DataFrameMetaData();
+    virtual ~DataFrameMetaData() = default;
+
+    virtual void process() override;
+
+    virtual const ProcessorInfo getProcessorInfo() const override;
+    static const ProcessorInfo processorInfo_;
+
+private:
+    DataFrameInport inport_;
+    DataFrameOutport outport_;
+
+    ColumnMetaDataListProperty columns_;
+};
+
+}  // namespace inviwo
