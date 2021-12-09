e850b45253b5542aaa244522261c39784614d5f4
Peter Steneteg <peter@steneteg.se>
2021-12-09 10:33:56 +0100

    Core: removed duplicated code

--- include/inviwo/core/util/glm.h
+++ include/inviwo/core/util/glm.h
@@ -830,36 +830,7 @@ bool almostEqual(const T& x, const T& y, int ulp = 2) {
 
 }  // namespace inviwo
 
-namespace std {
-template <auto N, class T, auto Q>
-struct tuple_size<glm::vec<N, T, Q>> : std::integral_constant<std::size_t, N> {};
-
-template <std::size_t I, auto N, class T, auto Q>
-struct tuple_element<I, glm::vec<N, T, Q>> {
-    using type = typename glm::vec<N, T, Q>::value_type;
-};
-}  // namespace std
-
 namespace glm {
-template <std::size_t I, auto N, class T, auto Q>
-constexpr auto& get(glm::vec<N, T, Q>& v) noexcept {
-    return v[I];
-}
-
-template <std::size_t I, auto N, class T, auto Q>
-constexpr const auto& get(const glm::vec<N, T, Q>& v) noexcept {
-    return v[I];
-}
-
-template <std::size_t I, auto N, class T, auto Q>
-constexpr auto&& get(glm::vec<N, T, Q>&& v) noexcept {
-    return v[I];
-}
-
-template <std::size_t I, auto N, class T, auto Q>
-constexpr const auto&& get(const glm::vec<N, T, Q>&& v) noexcept {
-    return v[I];
-}
 
 namespace detail {
 
@@ -1079,15 +1050,11 @@ decltype(auto) get(glm::vec<L, T, Q>& vec) {
 
 }  // namespace glm
 
-namespace std {
-
 // Needed to get structured bindings to work for glm vecs
 template <glm::length_t L, typename T, glm::qualifier Q>
-struct tuple_size<glm::vec<L, T, Q>> : std::integral_constant<std::size_t, L> {};
+struct std::tuple_size<glm::vec<L, T, Q>> : std::integral_constant<std::size_t, L> {};
 
 template <std::size_t N, glm::length_t L, typename T, glm::qualifier Q>
-struct tuple_element<N, glm::vec<L, T, Q>> {
+struct std::tuple_element<N, glm::vec<L, T, Q>> {
     using type = T;
 };
-
-}  // namespace std
