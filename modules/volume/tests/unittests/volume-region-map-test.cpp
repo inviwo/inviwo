23802bd9d63add0a0a34608f413b3c0c08100de3
Rasmus Hogslatt <r.hogslatt@gmail.com>
2022-02-26 18:59:54 +0100

    Added unit test
    Remapping a 3x3 volume.

--- modules/volume/tests/unittests/volume-region-map-test.cpp
+++ modules/volume/tests/unittests/volume-region-map-test.cpp
@@ -32,10 +32,88 @@
 #include <gtest/gtest.h>
 #include <warn/pop>
 
+#include <inviwo/volume/processors/volumeregionmap.h>
+#include <inviwo/core/datastructures/volume/volumeramprecision.h>
+#include <inviwo/core/util/templatesampler.h>
+
 namespace inviwo {
 
+namespace {
+std::array<float, 27> sampledata = {{1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f,
+                                     1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f,
+                                     1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f}};
+
+std::shared_ptr<inviwo::Volume> createVolume() {
+    auto volumeram = std::make_shared<VolumeRAMPrecision<float>>(size3_t{3, 3, 3});
+    std::copy(sampledata.begin(), sampledata.end(), volumeram->getDataTyped());
+    return std::make_shared<Volume>(volumeram);
+}
+} // namespace
+
+void remap(std::shared_ptr<Volume>& volume, std::vector<float> src, std::vector<float> dst) {
+    auto volRep = volume->getEditableRepresentation<VolumeRAM>();
+
+    volRep->dispatch<void, dispatching::filter::Scalars>([&](auto volram) {
+        using ValueType = util::PrecisionValueType<decltype(volram)>;
+        ValueType* dataPtr = volram->getDataTyped();
+
+        const auto& dim = volram->getDimensions();
+
+        std::transform(dataPtr, dataPtr + dim.x * dim.y * dim.z, dataPtr, [&](const ValueType& v) {
+            for (size_t i = 0; i < src.size(); ++i) {
+                if (static_cast<uint32_t>(v) == src[i]) {
+                    return static_cast<ValueType>(dst[i]);
+                }
+            }
+            return ValueType{0};
+        });
+    });
+}
+
 TEST(Volume, volume_region_map_test) {
-    EXPECT_EQ(1, 1);
+    auto volume = createVolume();
+    std::vector<float> src = {1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f};
+    std::vector<float> dst = {1.f, 1.f, 1.f, 2.f, 2.f, 2.f, 3.f, 3.f, 3.f};
+    remap(volume, src, dst);
+
+    using VolSampler = TemplateVolumeSampler<float, float, float, 1>;
+    const VolSampler sampler(volume);
+
+    EXPECT_FLOAT_EQ(1.0f, sampler.sample(vec3(0.f, 0.f, 0.f)));
+    EXPECT_FLOAT_EQ(1.0f, sampler.sample(vec3(0.f, 0.f, 0.5f)));
+    EXPECT_FLOAT_EQ(1.0f, sampler.sample(vec3(0.f, 0.f, 1.f)));
+
+    EXPECT_FLOAT_EQ(1.0f, sampler.sample(vec3(0.5f, 0.0f, 0.f)));
+    EXPECT_FLOAT_EQ(1.0f, sampler.sample(vec3(0.5f, 0.0f, 0.5f)));
+    EXPECT_FLOAT_EQ(1.0f, sampler.sample(vec3(0.5f, 0.0f, 1.f)));
+
+    EXPECT_FLOAT_EQ(1.0f, sampler.sample(vec3(1.f, 0.0f, 0.f)));
+    EXPECT_FLOAT_EQ(1.0f, sampler.sample(vec3(1.f, 0.0f, 0.5f)));
+    EXPECT_FLOAT_EQ(1.0f, sampler.sample(vec3(1.f, 0.0f, 1.f)));
+
+    EXPECT_FLOAT_EQ(2.0f, sampler.sample(vec3(0.f, 0.5f, 0.f)));
+    EXPECT_FLOAT_EQ(2.0f, sampler.sample(vec3(0.f, 0.5f, 0.5f)));
+    EXPECT_FLOAT_EQ(2.0f, sampler.sample(vec3(0.f, 0.5f, 1.f)));
+
+    EXPECT_FLOAT_EQ(2.0f, sampler.sample(vec3(0.5f, 0.5f, 0.f)));
+    EXPECT_FLOAT_EQ(2.0f, sampler.sample(vec3(0.5f, 0.5f, 0.5f)));
+    EXPECT_FLOAT_EQ(2.0f, sampler.sample(vec3(0.5f, 0.5f, 1.f)));
+
+    EXPECT_FLOAT_EQ(2.0f, sampler.sample(vec3(1.f, 0.5f, 0.f)));
+    EXPECT_FLOAT_EQ(2.0f, sampler.sample(vec3(1.f, 0.5f, 0.5f)));
+    EXPECT_FLOAT_EQ(2.0f, sampler.sample(vec3(1.f, 0.5f, 1.f)));
+
+    EXPECT_FLOAT_EQ(3.0f, sampler.sample(vec3(0.f, 1.f, 0.f)));
+    EXPECT_FLOAT_EQ(3.0f, sampler.sample(vec3(0.f, 1.f, 0.5f)));
+    EXPECT_FLOAT_EQ(3.0f, sampler.sample(vec3(0.f, 1.f, 1.f)));
+
+    EXPECT_FLOAT_EQ(3.0f, sampler.sample(vec3(0.5f, 1.f, 0.f)));
+    EXPECT_FLOAT_EQ(3.0f, sampler.sample(vec3(0.5f, 1.f, 0.5f)));
+    EXPECT_FLOAT_EQ(3.0f, sampler.sample(vec3(0.5f, 1.f, 1.f)));
+
+    EXPECT_FLOAT_EQ(3.0f, sampler.sample(vec3(1.f, 1.f, 0.f)));
+    EXPECT_FLOAT_EQ(3.0f, sampler.sample(vec3(1.f, 1.f, 0.5f)));
+    EXPECT_FLOAT_EQ(3.0f, sampler.sample(vec3(1.f, 1.f, 1.f)));
 }   
 
 }  // namespace inviwo 
