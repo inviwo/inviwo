3f6075ca797f55679e5d1775863d98d0e7c4e91e
Daniel Jönsson <daniel.jonsson@liu.se>
2022-04-22 09:30:57 +0200

    Core: GCC compile error fix
    class template placeholder ‘inviwo::util::span’ not permitted in this context
    til::Combinations comb(util::span{bases}, searchGroupSize);
    
    Turned on linux CI

--- src/core/datastructures/unitsystem.cpp
+++ src/core/datastructures/unitsystem.cpp
@@ -160,7 +160,8 @@ util::findBestSetOfNamedUnits(Unit unit, const unitgroups::EnabledGroups& enable
         std::vector<std::pair<units::detail::unit_data, int>> match;
         match.reserve(groups.size());
         for (size_t searchGroupSize = 1; searchGroupSize <= groups.size(); ++searchGroupSize) {
-            util::Combinations comb(util::span{bases}, searchGroupSize);
+            auto s =  util::span{bases}; // GCC compile error if not separated from line below
+            util::Combinations comb(s, searchGroupSize);
             do {
                 auto test = Unit{}.base_units();
                 match.clear();
