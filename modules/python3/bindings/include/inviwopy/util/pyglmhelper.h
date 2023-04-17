98f94fbb6fd40b81f837a7ddf59cc82ae69903ec
Martin Falk <martin.falk@liu.se>
2023-04-17 10:29:11 +0200

    Python3: requested change

--- modules/python3/bindings/include/inviwopy/util/pyglmhelper.h
+++ modules/python3/bindings/include/inviwopy/util/pyglmhelper.h
@@ -38,7 +38,6 @@
 
 #include <warn/push>
 #include <warn/ignore/shadow>
-// #include <pybind11/pybind11.h>
 #include <pybind11/operators.h>
 #include <pybind11/stl_bind.h>
 #include <pybind11/numpy.h>
