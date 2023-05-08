fbc9905cd3eb2b68897905397a282dc7698efc84
Peter Steneteg <peter@steneteg.se>
2023-05-08 13:55:03 +0200

    Tools: Meta using IVVIWO_META_API on the grammer failes to compile in MSVC "illegal storage specifier..."

--- tools/meta/include/inviwo/meta/cmake/grammar.hpp
+++ tools/meta/include/inviwo/meta/cmake/grammar.hpp
@@ -45,21 +45,20 @@ namespace cmake {
 namespace p = tao::pegtl;
 
 // Grammar
-struct INVIWO_META_API space : p::plus<p::blank> {};
-struct INVIWO_META_API newline : p::sor<p::string<'\r', '\n'>, p::one<'\n'>> {};
-struct INVIWO_META_API line_comment
-    : p::seq<p::one<'#'>, p::star<p::seq<p::not_at<newline>, p::any>>> {};
-struct INVIWO_META_API line_ending : p::seq<p::opt<line_comment>, newline> {};
-struct INVIWO_META_API begin_brace : p::one<'('> {};
-struct INVIWO_META_API end_brace : p::one<')'> {};
-struct INVIWO_META_API identifier : p::identifier {};
-struct INVIWO_META_API separation : p::sor<space, line_ending> {};
+struct space : p::plus<p::blank> {};
+struct newline : p::sor<p::string<'\r', '\n'>, p::one<'\n'>> {};
+struct line_comment : p::seq<p::one<'#'>, p::star<p::seq<p::not_at<newline>, p::any>>> {};
+struct line_ending : p::seq<p::opt<line_comment>, newline> {};
+struct begin_brace : p::one<'('> {};
+struct end_brace : p::one<')'> {};
+struct identifier : p::identifier {};
+struct separation : p::sor<space, line_ending> {};
 
-struct INVIWO_META_API bracket_id : p::star<p::one<'='>> {};
-struct INVIWO_META_API bracket_open : p::seq<p::one<'['>, bracket_id, p::one<'['>> {};
-struct INVIWO_META_API bracket_mark {
+struct bracket_id : p::star<p::one<'='>> {};
+struct bracket_open : p::seq<p::one<'['>, bracket_id, p::one<'['>> {};
+struct bracket_mark {
     using rule_t = bracket_mark;
-    
+
     using subs_t = p::empty_list;
 
     template <p::apply_mode, p::rewind_mode, template <typename...> class Action,
@@ -75,49 +74,43 @@ struct INVIWO_META_API bracket_mark {
         return false;
     }
 };
-struct INVIWO_META_API bracket_close : p::seq<p::one<']'>, bracket_mark, p::one<']'>> {};
-struct INVIWO_META_API bracket_body : p::any {};
-struct INVIWO_META_API bracket_argument
-    : p::seq<bracket_open, p::until<bracket_close, bracket_body>> {};
-struct INVIWO_META_API bracket_comment : p::seq<p::one<'#'>, bracket_argument> {};
+struct bracket_close : p::seq<p::one<']'>, bracket_mark, p::one<']'>> {};
+struct bracket_body : p::any {};
+struct bracket_argument : p::seq<bracket_open, p::until<bracket_close, bracket_body>> {};
+struct bracket_comment : p::seq<p::one<'#'>, bracket_argument> {};
 
-struct INVIWO_META_API escape_identity
+struct escape_identity
     : p::seq<p::one<'\\'>,
              p::seq<p::not_at<p::ranges<'A', 'Z', 'a', 'z', '0', '9', ';'>>, p::any>> {};
-struct INVIWO_META_API escape_encoded
-    : p::sor<p::string<'\\', 't'>, p::string<'\\', 'r'>, p::string<'\\', 'n'>> {};
-struct INVIWO_META_API escape_semicolon : p::string<'\\', ';'> {};
-struct INVIWO_META_API escape_sequence : p::sor<escape_identity, escape_encoded, escape_semicolon> {
-};
+struct escape_encoded : p::sor<p::string<'\\', 't'>, p::string<'\\', 'r'>, p::string<'\\', 'n'>> {};
+struct escape_semicolon : p::string<'\\', ';'> {};
+struct escape_sequence : p::sor<escape_identity, escape_encoded, escape_semicolon> {};
 
-struct INVIWO_META_API unquoted_element
+struct unquoted_element
     : p::sor<p::not_one<'(', ')', '#', '"', '\\', ' ', '\t', '\n', '\r'>, escape_sequence> {};
 
-struct INVIWO_META_API quoted_continuation : p::seq<p::one<'\\'>, newline> {};
-struct INVIWO_META_API quoted_element
-    : p::sor<p::not_one<'\\', '"'>, escape_sequence, quoted_continuation> {};
+struct quoted_continuation : p::seq<p::one<'\\'>, newline> {};
+struct quoted_element : p::sor<p::not_one<'\\', '"'>, escape_sequence, quoted_continuation> {};
 
-struct INVIWO_META_API quoted_argument : p::seq<p::one<'"'>, p::star<quoted_element>, p::one<'"'>> {
-};
-struct INVIWO_META_API unquoted_argument : p::plus<unquoted_element> {};  // TODO unquoted_legacy
+struct quoted_argument : p::seq<p::one<'"'>, p::star<quoted_element>, p::one<'"'>> {};
+struct unquoted_argument : p::plus<unquoted_element> {};  // TODO unquoted_legacy
 
-struct INVIWO_META_API argument : p::sor<bracket_argument, quoted_argument, unquoted_argument> {};
+struct argument : p::sor<bracket_argument, quoted_argument, unquoted_argument> {};
 
 struct arguments;
-struct INVIWO_META_API seperated_arguments
+struct seperated_arguments
     : p::sor<p::seq<p::plus<separation>, p::opt<argument>>,
              p::seq<p::star<separation>, begin_brace, arguments, end_brace>> {};
 
-struct INVIWO_META_API arguments : p::seq<p::opt<argument>, p::star<seperated_arguments>> {};
+struct arguments : p::seq<p::opt<argument>, p::star<seperated_arguments>> {};
 
-struct INVIWO_META_API command
+struct command
     : p::seq<p::star<space>, identifier, p::star<space>, begin_brace, arguments, end_brace> {};
 
-struct INVIWO_META_API file_element
-    : p::sor<p::seq<command, p::opt<space>, line_ending>,
-             p::seq<p::star<p::sor<bracket_comment, space>>, line_ending>> {};
+struct file_element : p::sor<p::seq<command, p::opt<space>, line_ending>,
+                             p::seq<p::star<p::sor<bracket_comment, space>>, line_ending>> {};
 
-struct INVIWO_META_API file : p::must<p::seq<p::bof, p::star<file_element>, p::eof>> {};
+struct file : p::must<p::seq<p::bof, p::star<file_element>, p::eof>> {};
 
 template <typename Rule>
 struct action : p::nothing<Rule> {};
