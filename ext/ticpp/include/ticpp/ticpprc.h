8f8ca2786abfb5c137538d56a3d02daf02effa8f
Peter Steneteg <peter@steneteg.se>
2023-08-10 20:30:32 +0200

    TiCpp: removed tinystr, and format code

--- ext/ticpp/include/ticpp/ticpprc.h
+++ ext/ticpp/include/ticpp/ticpprc.h
@@ -20,7 +20,7 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
- /*
+/*
  * THIS FILE WAS ALTERED BY Matt Janisz, 12. October 2012.
  *
  * - added ticppapi.h include and TICPP_API dll-interface to support building DLL using VS200X
@@ -35,9 +35,8 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <vector>
 
 // Forward declare ticpp::Node, so it can be made a friend of TiCppRC
-namespace ticpp
-{
-	class TICPP_API Base;
+namespace ticpp {
+class TICPP_API Base;
 }
 
 // Forward declare TiCppRCImp so TiCppRC can hold a pointer to it
@@ -46,84 +45,81 @@ class TICPP_API TiCppRCImp;
 /**
 Base class for reference counting functionality
 */
-class TICPP_API TiCppRC
-{
-	// Allow ticpp::Node to directly modify reference count
-	friend class ticpp::Base;
+class TICPP_API TiCppRC {
+    // Allow ticpp::Node to directly modify reference count
+    friend class ticpp::Base;
 
 private:
-
-	TiCppRCImp* m_tiRC; /**< Pointer to reference counter */
+    TiCppRCImp* m_tiRC; /**< Pointer to reference counter */
 
 public:
+    /**
+    Constructor
+    Spawns new reference counter with a pointer to this
+    */
+    TiCppRC();
 
-	/**
-	Constructor
-	Spawns new reference counter with a pointer to this
-	*/
-	TiCppRC();
+    /**
+    Destructor
+    Nullifies the pointer to this held by the reference counter
+    Decrements reference count
+    */
+    virtual ~TiCppRC();
 
-	/**
-	Destructor
-	Nullifies the pointer to this held by the reference counter
-	Decrements reference count
-	*/
-	virtual ~TiCppRC();
-	
-	std::vector< ticpp::Base* > m_spawnedWrappers; /**< Remember all wrappers that we've created with 'new' - ( e.g. NodeFactory, FirstChildElement, etc. )*/
+    std::vector<ticpp::Base*>
+        m_spawnedWrappers; /**< Remember all wrappers that we've created with 'new' - ( e.g.
+                              NodeFactory, FirstChildElement, etc. )*/
 
-	/**
-	Delete all container objects we've spawned with 'new'.
-	*/
-	void DeleteSpawnedWrappers();
+    /**
+    Delete all container objects we've spawned with 'new'.
+    */
+    void DeleteSpawnedWrappers();
 };
 
-class TICPP_API TiCppRCImp
-{
+class TICPP_API TiCppRCImp {
 private:
+    int m_count; /**< Holds reference count to me, and to the node I point to */
 
-	int m_count;		/**< Holds reference count to me, and to the node I point to */
-
-	TiCppRC* m_tiCppRC;	/**< Holds pointer to an object inheriting TiCppRC */
+    TiCppRC* m_tiCppRC; /**< Holds pointer to an object inheriting TiCppRC */
 
 public:
+    /**
+    Initializes m_tiCppRC pointer, and set reference count to 1
+    */
+    TiCppRCImp(TiCppRC* tiCppRC);
 
-	/**
-	Initializes m_tiCppRC pointer, and set reference count to 1
-	*/
-	TiCppRCImp( TiCppRC* tiCppRC );
+    /**
+    Allows the TiCppRC object to set the pointer to itself ( m_tiCppRc ) to NULL when the TiCppRC
+    object is deleted
+    */
+    void Nullify();
 
-	/**
-	Allows the TiCppRC object to set the pointer to itself ( m_tiCppRc ) to NULL when the TiCppRC object is deleted
-	*/
-	void Nullify();
+    /**
+    Increment Reference Count
+    */
+    void IncRef();
 
-	/**
-	Increment Reference Count
-	*/
-	void IncRef();
+    /**
+    Decrement Reference Count
+    */
+    void DecRef();
 
-	/**
-	Decrement Reference Count
-	*/
-	void DecRef();
+    /**
+    Set Reference Count to 1 - dangerous! - Use only if you are sure of the consequences
+    */
+    void InitRef();
 
-	/**
-	Set Reference Count to 1 - dangerous! - Use only if you are sure of the consequences
-	*/
-	void InitRef();
+    /**
+    Get internal pointer to the TiCppRC object - not reference counted, use at your own risk
+    */
+    TiCppRC* Get();
 
-	/**
-	Get internal pointer to the TiCppRC object - not reference counted, use at your own risk
-	*/
-	TiCppRC* Get();
-
-	/**
-	Returns state of internal pointer - will be null if the object was deleted
-	*/
-	bool IsNull();
+    /**
+    Returns state of internal pointer - will be null if the object was deleted
+    */
+    bool IsNull();
 };
 
-#endif // TICPP_INCLUDED
+#endif  // TICPP_INCLUDED
 
-#endif // TIXML_USE_TICPP
+#endif  // TIXML_USE_TICPP
