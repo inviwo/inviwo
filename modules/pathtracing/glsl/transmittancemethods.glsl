35c4c6d89ccec3661445dcc27784e5a9aee63923
Hljung <henlj389@student.liu.se>
2023-10-11 16:00:19 +0200

    PathTracing: Simple light based volume rendering
    Consider cleaning the code at times.

--- modules/pathtracing/glsl/transmittancemethods.glsl
+++ modules/pathtracing/glsl/transmittancemethods.glsl
@@ -15,3 +15,31 @@ vec4 HelloWorld(vec4 inVec4) {
     outVec4.z = outVec4.z/zDiff;
     return inVec4;
 }
+
+vec4 RMVolumeRender_simple(inout float T, float rayStep, float tau, vec4 tfSample, vec4 acc_radiance) {
+    acc_radiance = acc_radiance + T*tau*tfSample*rayStep;
+    T = T*exp(-tau*rayStep);
+
+    return acc_radiance;
+}
+
+vec4 RMVolumeRender_simpleWithLight(inout float T, float rayStep, float tau, vec4 tfSample, vec4 acc_radiance, 
+    vec3 samplePos, vec3 lightPos, float lightRadiance) {
+    
+    float lightCoeff = lightRadiance/pow(distance(vec3(4,4,0), samplePos), 2f);
+    vec4 lightColor = vec4(1.0f)*lightCoeff;
+    // what am i doing wrong? Why am i incapable of getting a gradient from the light?
+    // Because im normalizing... dont do any scaling off of a vector
+    //lightColor = vec4((vec3(2) - samplePos), 1f);
+    //lightColor = vec4(length(lightColor)*0.05);
+    //lightColor *= 0.3f;
+    //lightColor.y -= -3;
+    
+    lightColor.w = 1f;
+
+
+    acc_radiance = acc_radiance + T*tau*(tfSample + lightColor)*rayStep;
+    T = T*exp(-tau*rayStep);
+
+    return acc_radiance;
+}
