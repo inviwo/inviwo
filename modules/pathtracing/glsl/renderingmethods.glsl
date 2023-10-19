9d4e5feb817d51b313991b2e116a8b8e089a555d
Hljung <henlj389@student.liu.se>
2023-10-19 18:44:22 +0200

    PathTracing: RotateFrom(vec3 float float) tested
    matrix had a small insidious miss spelling

--- modules/pathtracing/glsl/renderingmethods.glsl
+++ modules/pathtracing/glsl/renderingmethods.glsl
@@ -4,9 +4,15 @@
 mat3 rotMatrixAroundAxel(vec3 v, float theta) {
     float costheta = cos(theta); float sintheta = sin(theta);
     mat3 thetaRot = mat3(                                                
-        vec3(costheta+v.x*v.x*(1-costheta), v.x*v.y*(1-costheta)-v.z*sintheta, v.x*v.z*(1-costheta)+v.y*sintheta),
-        vec3(v.y*v.x*(1-costheta)+v.z*sintheta, costheta+v.y*v.y*(1-costheta), v.y*v.z*(1-costheta)-v.x*sintheta),
-        vec3(v.z*v.x*(1-costheta)+v.z*sintheta, v.z*v.y*(1-costheta)+v.x*sintheta, costheta+v.z*v.z*(1-costheta))
+        vec3(costheta+v.x*v.x*(1-costheta), 
+            v.x*v.y*(1-costheta)-v.z*sintheta, 
+                v.x*v.z*(1-costheta)+v.y*sintheta),
+        vec3(v.y*v.x*(1-costheta)+v.z*sintheta, 
+            costheta+v.y*v.y*(1-costheta), 
+                v.y*v.z*(1-costheta)-v.x*sintheta),
+        vec3(v.z*v.x*(1-costheta)-v.y*sintheta, 
+            v.z*v.y*(1-costheta)+v.x*sintheta, 
+                costheta+v.z*v.z*(1-costheta))
     );
 
     return thetaRot;
@@ -16,13 +22,18 @@ vec3 rotateFrom(vec3 v, float theta, float phi) {
     
     // produce for mat3's one for v, and one for v perpendicular
     mat3 thetaRot = rotMatrixAroundAxel(v, theta);
-    vec3 w = vec3(0,1f,0);
-    if(v == vec3(0,1f,0)) {
-        w = vec3(0,0,1f);
-    }
-    w = normalize(cross(v,w));
-    mat3 phiRot = rotMatrixAroundAxel(w, phi);
 
+    // så länge v är inte ungefär lika med e_z (med epsilon marginal) så 'är det intuitivt' att det existerar
+    // en punkt på planet som bildas av v som normal där z = 0, denna punkt kommer var ortogonal med v
+
+
+    vec3 w = vec3(-v.y, v.x, 0f);
+    if(v.x == 0 && v.y == 0) {
+        w = vec3(0,0,1);
+    }
+    w = normalize(w);
+    mat3 phiRot = rotMatrixAroundAxel(w, phi);
+    //thetaRot*phiRot*v
     vec3 axis = thetaRot*phiRot*v;
 
     return axis;
