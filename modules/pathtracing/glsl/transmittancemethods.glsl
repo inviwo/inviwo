470ffeb47cf6c9101118628439481e0fa2e9ba2e
Hljung <henlj389@student.liu.se>
2023-11-22 16:10:09 +0100

    PathTracing: RayBoxIntersection_Texturespace still WIP
    just refuses

--- modules/pathtracing/glsl/transmittancemethods.glsl
+++ modules/pathtracing/glsl/transmittancemethods.glsl
@@ -2,6 +2,7 @@
 #include "utils/shading.glsl"
 #include "utils/intersection.glsl"
 #include "renderingmethods.glsl"
+#include "utils/gradients.glsl"
 
 /* --- 
     My stuff now
@@ -16,7 +17,7 @@ float tfToExtinction(float s_max, float toExtMod) {
 float WoodcockTracking(vec3 raystart, vec3 raydir, float tStart, float tEnd, inout uint hashSeed, 
     sampler3D volume, VolumeParameters volumeParameters, sampler2D transferFunction, float sigma_upperbound, out float sigma) {
     
-    float invExtinction = 1.f/tfToExtinction(sigma_upperbound, 1.0f);
+    float invExtinction = 1.f/tfToExtinction(sigma_upperbound, 3f);
     float invSigmaUpperbound = 1.f/sigma_upperbound;
     float t = tStart; 
     float sigmaSample;
@@ -24,7 +25,7 @@ float WoodcockTracking(vec3 raystart, vec3 raydir, float tStart, float tEnd, ino
     do {    
         t += -log(random_1dto1d(pcg_rehash(hashSeed)))*invExtinction;
         r = raystart + t*raydir;
-        vec4 volumeSample = getNormalizedVoxel(volume, volumeParameters, r);
+        vec4 volumeSample = getVoxel(volume, volumeParameters, r);
         sigmaSample = applyTF(transferFunction, volumeSample).a;
 
     } while (random_1dto1d(pcg_rehash(hashSeed)) >= sigmaSample*invSigmaUpperbound && t <= tEnd);
@@ -46,7 +47,7 @@ float WoodcockTracking_InvT(vec3 raystart, vec3 raydir, float tStart, float tEnd
         t = t - log(1f - random_1dto1d(hashSeed))*invSigmaUpperbound;   
 
         r = raystart - t*raydir;
-        sigma = applyTF(transferFunction, getNormalizedVoxel(volume, volumeParameters, r)).a;
+        sigma = applyTF(transferFunction, getNormalizedVoxel(volume, volumeParameters, r).x).a;
         if(random_1dto1d(hashSeed + 1) < sigma*sigma_upperbound) {
             break;
         }
@@ -109,13 +110,14 @@ vec4 RMVolumeRender_simple(inout float T, float rayStep, float tau, vec4 tfSampl
     return acc_radiance;
 }
 
-vec4 RMVolumeRender_SingleBounceLight(float T, float rayStep, sampler3D volume, VolumeParameters volParam, sampler2D tf, vec4 acc_radiance, 
-    vec3 samplePos, vec3 cameraDir, LightParameters light, PlaneParameters[6] bb, uint hashSeed) {
+vec4 RMVolumeRender_SingleBounceLight(float T, float rayStep, sampler3D volume, VolumeParameters volParam, sampler2D tf, 
+    vec3 samplePos, vec3 cameraDir, LightParameters light, PlaneParameters[6] bb, uint hashSeed, int rcChannel, float extinctionUpper) {
     
     //need worldpos
     vec3 sampleWorldPos = (volParam.textureToWorld*vec4(samplePos,1f)).xyz;
     
-    vec4 color;
+    vec4 color = vec4(0f);
+    vec4 result = vec4(0f);
     
     vec4 voxel = getNormalizedVoxel(volume, volParam, samplePos);
     vec4 tfSample = applyTF(tf, voxel);
@@ -125,8 +127,8 @@ vec4 RMVolumeRender_SingleBounceLight(float T, float rayStep, sampler3D volume, 
     vec3 sampleDiffuse = tfSample.rgb;
     vec3 sampleSpecular = tfSample.rgb;
     
-    vec3 toLight = normalize(light.position - sampleWorldPos);
-
+    vec3 toLightV = light.position - sampleWorldPos;
+    vec3 toLightD = normalize(toLightV);
     /*
         Light Attenuation source to sample:
         Attenuate color from sampleWorldPos to lightPos
@@ -159,38 +161,66 @@ vec4 RMVolumeRender_SingleBounceLight(float T, float rayStep, sampler3D volume, 
         }
 
     */
+    vec3 toLightTextureV = (volParam.worldToTexture*vec4(toLightV,1f)).xyz;
+    vec3 toLightTextureD = normalize(toLightTextureV);
     float t0 = 0.0f;
-    float t1;
+    float t1 = 100f;
     float tau = 1f;   
     float meanfreepath_l = 0f; 
     float Tl = 1f;
-    
-    vec3 toLightTexture = (volParam.worldToTexture*vec4(toLight,1f)).xyz;
 
-    RayBBIntersection_TextureSpace(samplePos, toLightTexture, t0, t1);
-    meanfreepath_l = WoodcockTracking(samplePos, toLightTexture, 0f, t1, hashSeed, 
-        volume, volParam, tf, 1.0f, tau);
+    //normalizing toLight results in no attenuation
+    //!false == true
+    if(!RayBBIntersection_TextureSpace(samplePos, toLightTextureD, t0, t1)) {
+        vec4 debugres = vec4(0f);
+        debugres.w = 1f;
+        debugres.y = 1f;
+        /*
+        if(t0 >= t1) {
+            debugres.x = 1f;
+            debugres.y = 0f;
+        } 
+        */
+        if(t1 == 0) {
+            debugres.x = 1f;
+            debugres.y = 0f;
+        } 
 
-    // TODO: this looks to always result in true, without some dumb coefficient
-    Tl = meanfreepath_l >= t1 ? 1.0f : 0.0f; 
 
-    
+
+
+        return debugres;
+    }
+
+    meanfreepath_l = WoodcockTracking(samplePos, toLightTextureD, 0f, t1, hashSeed, 
+        volume, volParam, tf, extinctionUpper, tau);
+
+    Tl = meanfreepath_l >= t0 ? 1.0f : 0.0f;     
+
+    // TODO: use gradient as normal. Look at gradients.glsl
     //color.rgb = shadeSpecularPhongCalculation(light, tfSample.rgb, /*what is the normal of a particle*/ -cameraDir, 
     //    toLight, cameraDir);
     
+    vec3 gradient = gradientCentralDiff(vec4(0f), volume, volParam, samplePos, 0);
+
     color.rgb = shadeBlinnPhong(light, tfSample.rgb, tfSample.rgb, tfSample.rgb,
-        sampleWorldPos, /*what is the normal of a particle*/ cameraDir, cameraDir);
-    
+        sampleWorldPos,
+        gradient, 
+        cameraDir);
+    color.rgb = clamp(color.rgb, vec3(0f), vec3(1f));
     // pseudo gamma
     float g = 1.0f;
 
     // Q: Does the alpha channel not do anything?
     color.a = 1f;
     
-    float faux_radiance = light.specularExponent / distance(sampleWorldPos, toLight);
-    faux_radiance = 1f;
+    // This is ideally replaced by the tracking. Or use a light model that supports radiance.
+    float faux_radiance = 1000/log(light.specularExponent + 1);
+    float Li = faux_radiance / (length(toLightV)*length(toLightV));
+    //Li = 1f;
 
-    acc_radiance = g*color*Tl*faux_radiance;
-
-    return acc_radiance;
+    // multiplying by tfSample.a looks good but is dubious
+    result = g*Li*color*Tl;
+    result *= tfSample.a;
+    return result;
 }
