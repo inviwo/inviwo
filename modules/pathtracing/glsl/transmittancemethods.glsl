ef8b9fb6bbbf3cde3b2b16c090774258dd9672eb
Hljung <henlj389@student.liu.se>
2023-11-30 13:19:31 +0100

    PathTracing: Cleanup of shader files

--- modules/pathtracing/glsl/transmittancemethods.glsl
+++ modules/pathtracing/glsl/transmittancemethods.glsl
@@ -1,18 +1,13 @@
 #include "random.glsl"
 #include "utils/shading.glsl"
 #include "utils/intersection.glsl"
-#include "renderingmethods.glsl"
 #include "utils/gradients.glsl"
-
-/* --- 
-    My stuff now
-*/
+#include "util.glsl"
 
 float tfToExtinction(float s_max) {
     return s_max*150f;
 }
 
-// Returns how far to step, supposedly.
 // raystart, raydir and raylength ought to be in data coordinate
 float WoodcockTracking(vec3 raystart, vec3 raydir, float tStart, float tEnd, inout uint hashSeed, 
     sampler3D volume, VolumeParameters volumeParameters, sampler2D transferFunction, float sigma_upperbound, out float sigma) {
@@ -31,27 +26,6 @@ float WoodcockTracking(vec3 raystart, vec3 raydir, float tStart, float tEnd, ino
     } while (random_1dto1d(pcg_rehash(hashSeed)) >= sigmaSample*invSigmaUpperbound && t <= tEnd);
 
     return t;
-}
-
-float WoodcockTracking_uvec2Hashseed(vec3 raystart, vec3 raydir, float raylength, uvec2 hashSeed, 
-    sampler3D volume, VolumeParameters volumeParameters, sampler2D transferFunction, float sigma_upperbound ) {
-
-    float meanfreepath = 0f; // tau
-
-    vec3 r = vec3(0);
-    while (meanfreepath < raylength) {
-        meanfreepath += -log(random_2dto1d(hashSeed))/sigma_upperbound;
-
-        r = raystart - meanfreepath*raydir;
-        //this is really slow since we are now doing 2 texture look ups multiple times, per ray.
-        float sigma = applyTF(transferFunction, getNormalizedVoxel(volume, volumeParameters, r)).a*700;
-        //float sigma = 1.5;
-        if(random_2dto1d(hashSeed + uvec2(1)) < sigma/sigma_upperbound) {
-            break;
-        }
-    }
-
-    return meanfreepath;
 }
 
 float RatioTrackingEstimator(inout vec3 raystart, vec3 raydir, float raylength, uint hashSeed, 
@@ -75,90 +49,4 @@ float RatioTrackingEstimator(inout vec3 raystart, vec3 raydir, float raylength, 
 
 
     return T;
-}
-
-float SimpleTracking(float T, float rayStep, float tau) {
-    return T*exp(-tau*rayStep);
-}
-
-vec4 RMVolumeRender_simple(inout float T, float rayStep, float tau, vec4 tfSample, vec4 acc_radiance) {
-    acc_radiance = acc_radiance + T*tau*tfSample*rayStep;
-    
-
-    return acc_radiance;
-}
-
-vec3 RMVolumeRender_SingleBounceLight(float T, float rayStep, sampler3D volume, VolumeParameters volParam, sampler2D tf, 
-    vec3 samplePos, vec3 cameraDir, LightParameters light, uint hashSeed, int rcChannel, float extinctionUpper) {
-    /*
-        Light Attenuation source to sample:
-        Attenuate color from sampleWorldPos to lightPos
-        send a ray from sampleWorldPos to lightPos.
-        attenuate for the distance from samplePos to boundingbox intersection.
-        There are many examples of ray-box, -plane intersection tests, it is fairly trivial.
-        if our volume had a bounding box that would make this very simple, but Boron is sadly rectangular.
-
-        look for estimateDirectFromOneLightSource
-        estimateLight {
-            estimateDirectFromOneLightSource {
-                sampleLightSource {
-                    *decrease the light power by square of distance
-
-                }
-                rayBoxIntersection {
-                    Its identical to the cl version but assuming bb is from 000 to 111
-                }
-                transmittanceTracking {
-                    if(transmittance_type == WOODCOCK) {
-                        return WOODCOCK_TRACKING();
-                    }
-                }
-                applyShading {
-                    if(shading_type == BLINN_PHONG) {
-                        return BLINN_PHONG_SHADING()
-                    }
-                }
-            }
-        }
-
-    */
-    vec3 result = vec3(0f);
-    vec3 sampleWorldPos = (volParam.textureToWorld*vec4(samplePos,1f)).xyz;
-    vec4 voxel = getNormalizedVoxel(volume, volParam, samplePos);
-    
-    vec4 tfSample = applyTF(tf, voxel);
-
-    // Made up colors of the particle
-    vec3 sampleAmbient = tfSample.rgb;
-    vec3 sampleDiffuse = tfSample.rgb;
-    vec3 sampleSpecular = tfSample.rgb;
-
-    vec3 toLightTextureV = (volParam.worldToTexture*vec4(light.position, 1f)).xyz - samplePos;
-    vec3 toLightTextureD = normalize(toLightTextureV);
-    float t0 = 0.0f;
-    float t1 = length(toLightTextureV);
-    float tau = 1f;   
-
-    RayBBIntersection_TextureSpace(samplePos, toLightTextureD, t0, t1);
-
-    float meanfreepath_l = WoodcockTracking(samplePos, toLightTextureD, 0f, t1, hashSeed, 
-        volume, volParam, tf, extinctionUpper, tau);
-    float Tl = meanfreepath_l >= t1 ? 1.0f : 0.0f;     
-
-
-
-    vec3 gradient = gradientCentralDiff(vec4(0f), volume, volParam, samplePos, 0);
-    gradient = normalize(gradient);
-    gradient *= sign(voxel[0] / (1.0 - volParam.formatScaling) - volParam.formatOffset);
-
-
-
-    vec3 color = shadeBlinnPhong(light, sampleAmbient, sampleDiffuse, sampleSpecular,
-        sampleWorldPos,
-        gradient, // should it be minus?
-        cameraDir);
-
-    result = color*Tl;
-    
-    return result;
 }
---
+++ modules/pathtracing/glsl/util.glsl
@@ -0,0 +1,49 @@
+#include "random.glsl"
+#include "transmittancemethods.glsl"
+
+mat3 rotMatrixAroundAxel(vec3 v, float theta) {
+    float costheta = cos(theta); float sintheta = sin(theta);
+    mat3 thetaRot = mat3(                                                
+        vec3(costheta+v.x*v.x*(1-costheta), 
+            v.x*v.y*(1-costheta)-v.z*sintheta, 
+                v.x*v.z*(1-costheta)+v.y*sintheta),
+        vec3(v.y*v.x*(1-costheta)+v.z*sintheta, 
+            costheta+v.y*v.y*(1-costheta), 
+                v.y*v.z*(1-costheta)-v.x*sintheta),
+        vec3(v.z*v.x*(1-costheta)-v.y*sintheta, 
+            v.z*v.y*(1-costheta)+v.x*sintheta, 
+                costheta+v.z*v.z*(1-costheta))
+    );
+
+    return thetaRot;
+}
+
+vec3 rotateFrom(vec3 v, float theta, float phi) {
+    
+    mat3 thetaRot = rotMatrixAroundAxel(v, theta);
+
+    vec3 w = vec3(-v.y, v.x, 0f);
+    if(v.x == 0 && v.y == 0) {
+        w = vec3(0,0,1);
+    }
+    w = normalize(w);
+    mat3 phiRot = rotMatrixAroundAxel(w, phi);
+    vec3 axis = thetaRot*phiRot*v;
+
+    return axis;
+}
+
+// Assumes sample space to be from 000 to 111
+bool RayBBIntersection_TextureSpace(vec3 origin, vec3 dir, out float t0, out float t1) {
+    float FLT_MAX = 3.402823466e+38;
+    
+    vec3 invDir = 1.f/dir;
+    vec3 tNearV = (vec3(0f, 0f, 0f)- origin)*invDir;
+    vec3 tFarV = (vec3(1f, 1f, 1f) - origin)*invDir;
+    vec3 tNear = min(tNearV, tFarV);
+    vec3 tFar = max(tNearV, tFarV);
+
+    t0 = max(t0, max( max(tNear.x, tNear.y), tNear.z ) );
+    t1 = min(t1, min( min(tFar.x, tFar.y), tFar.z ) );
+    return (t0 < t1) && (t1 < FLT_MAX);
+}
