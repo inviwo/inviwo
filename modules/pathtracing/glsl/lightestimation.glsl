1a8097b1500672e885670c5582659396e777db4e
Hljung <henlj389@student.liu.se>
2023-12-05 16:09:44 +0100

    PathTracing: Addressing review comments

--- modules/pathtracing/glsl/lightestimation.glsl
+++ modules/pathtracing/glsl/lightestimation.glsl
@@ -1,20 +1,11 @@
 #include "random.glsl"
-#include "transmittancemethods.glsl"
-#include "util.glsl"
+#include "transmittance/transmittancemethods.glsl"
+#include "util/vectormatrixmethods.glsl"
 
 vec3 estimateDirectLight(
     float rayStep, sampler3D volume, VolumeParameters volParam, sampler2D tf, vec3 samplePos, 
     vec3 cameraDir, LightParameters light, uint hashSeed, int rcChannel, float extinctionUpper) {
     
-    vec3 result = vec3(0f);
-    vec3 sampleWorldPos = (volParam.textureToWorld*vec4(samplePos,1f)).xyz;
-    vec4 voxel = getNormalizedVoxel(volume, volParam, samplePos);
-    
-    vec4 tfSample = applyTF(tf, voxel);
-
-    vec3 sampleAmbient = tfSample.rgb;
-    vec3 sampleDiffuse = tfSample.rgb;
-    vec3 sampleSpecular = tfSample.rgb;
 
     vec3 toLightTextureVector = (volParam.worldToTexture*vec4(light.position, 1f)).xyz - samplePos;
     vec3 toLightTextureDir = normalize(toLightTextureVector);
@@ -27,18 +18,28 @@ vec3 estimateDirectLight(
         volume, volParam, tf, extinctionUpper);
     float Tl = meanfreepath_l >= t1 ? 1.0f : 0.0f;     
 
+    if(Tl == 0.0f) {
+        return vec3(0f);
+    }
 
     vec3 gradient = gradientCentralDiff(vec4(0f), volume, volParam, samplePos, 0);
     gradient = normalize(gradient);
+
+    vec4 voxel = getNormalizedVoxel(volume, volParam, samplePos);
     gradient *= sign(voxel[0] / (1.0 - volParam.formatScaling) - volParam.formatOffset);
 
+    vec4 tfSample = applyTF(tf, voxel);
+
+    vec3 sampleAmbient = tfSample.rgb;
+    vec3 sampleDiffuse = tfSample.rgb;
+    vec3 sampleSpecular = tfSample.rgb;
+
+    vec3 sampleWorldPos = (volParam.textureToWorld*vec4(samplePos,1f)).xyz;
     vec3 color = shadeBlinnPhong(light, 
         sampleAmbient, sampleDiffuse, sampleSpecular,
         sampleWorldPos,
         gradient, 
         cameraDir);
-
-    result = color*Tl;
     
-    return result;
+    return color*Tl;
 }
