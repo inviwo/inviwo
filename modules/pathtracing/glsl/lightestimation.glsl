50c4c46018f22f1367e0c3c67ac6ac1e26632498
Hljung <henlj389@student.liu.se>
2023-12-08 13:53:19 +0100

    Pathtracing: Formatting and error fixes

--- modules/pathtracing/glsl/lightestimation.glsl
+++ modules/pathtracing/glsl/lightestimation.glsl
@@ -2,23 +2,23 @@
 #include "transmittance/transmittancemethods.glsl"
 #include "util/vectormatrixmethods.glsl"
 
-vec3 estimateDirectLight(
-    float rayStep, sampler3D volume, VolumeParameters volParam, sampler2D tf, vec3 samplePos, 
-    vec3 cameraDir, LightParameters light, uint hashSeed, int rcChannel, float extinctionUpper) {
-    
+vec3 estimateDirectLight(float rayStep, sampler3D volume, VolumeParameters volParam, sampler2D tf,
+                         vec3 samplePos, vec3 cameraDir, LightParameters light, uint hashSeed,
+                         int rcChannel, float extinctionUpper) {
 
-    vec3 toLightTextureVector = (volParam.worldToTexture*vec4(light.position, 1f)).xyz - samplePos;
+    vec3 toLightTextureVector =
+        (volParam.worldToTexture * vec4(light.position, 1f)).xyz - samplePos;
     vec3 toLightTextureDir = normalize(toLightTextureVector);
     float t0 = 0.0f;
-    float t1 = 1.f; 
+    float t1 = 1.f;
 
     RayBBIntersection_TextureSpace(samplePos, toLightTextureDir, t0, t1);
 
-    float meanfreepath_l = WoodcockTracking(samplePos, toLightTextureDir, t0, t1, hashSeed, 
-        volume, volParam, tf, extinctionUpper);
-    float Tl = meanfreepath_l >= t1 ? 1.0f : 0.0f;     
+    float meanfreepath_l = WoodcockTracking(samplePos, toLightTextureDir, t0, t1, hashSeed, volume,
+                                            volParam, tf, extinctionUpper);
+    float Tl = meanfreepath_l >= t1 ? 1.0f : 0.0f;
 
-    if(Tl == 0.0f) {
+    if (Tl == 0.0f) {
         return vec3(0f);
     }
 
@@ -34,12 +34,9 @@ vec3 estimateDirectLight(
     vec3 sampleDiffuse = tfSample.rgb;
     vec3 sampleSpecular = tfSample.rgb;
 
-    vec3 sampleWorldPos = (volParam.textureToWorld*vec4(samplePos,1f)).xyz;
-    vec3 color = shadeBlinnPhong(light, 
-        sampleAmbient, sampleDiffuse, sampleSpecular,
-        sampleWorldPos,
-        gradient, 
-        cameraDir);
-    
-    return color*Tl;
+    vec3 sampleWorldPos = (volParam.textureToWorld * vec4(samplePos, 1f)).xyz;
+    vec3 color = shadeBlinnPhong(light, sampleAmbient, sampleDiffuse, sampleSpecular,
+                                 sampleWorldPos, gradient, cameraDir);
+
+    return color * Tl;
 }
--- modules/pathtracing/glsl/random.glsl
+++ modules/pathtracing/glsl/random.glsl
@@ -1,7 +1,7 @@
 /*
-   Code for generating random numbers in [0 1] with a uniform distribution. 
+   Code for generating random numbers in [0 1] with a uniform distribution.
    Note: It has not been tested that these methods generate a true uniform distribution.
-   This code has been shared publicly at: 
+   This code has been shared publicly at:
     https://stackoverflow.com/questions/4200224/random-noise-functions-for-glsl
     static.frag
     by Statial
@@ -11,84 +11,74 @@
 */
 
 // A single iteration of Bob Jenkins' One-At-A-Time hashing algorithm.
-uint hash( uint x ) {
-    x += ( x << 10u );
-    x ^= ( x >>  6u );
-    x += ( x <<  3u );
-    x ^= ( x >> 11u );
-    x += ( x << 15u );
+uint hash(uint x) {
+    x += (x << 10u);
+    x ^= (x >> 6u);
+    x += (x << 3u);
+    x ^= (x >> 11u);
+    x += (x << 15u);
     return x;
 }
 
 // Compound versions of the hashing algorithm I whipped together.
-uint hash( uvec2 v ) { return hash( v.x ^ hash(v.y)                         ); }
-uint hash( uvec3 v ) { return hash( v.x ^ hash(v.y) ^ hash(v.z)             ); }
-uint hash( uvec4 v ) { return hash( v.x ^ hash(v.y) ^ hash(v.z) ^ hash(v.w) ); }
+uint hash(uvec2 v) { return hash(v.x ^ hash(v.y)); }
+uint hash(uvec3 v) { return hash(v.x ^ hash(v.y) ^ hash(v.z)); }
+uint hash(uvec4 v) { return hash(v.x ^ hash(v.y) ^ hash(v.z) ^ hash(v.w)); }
 
 // Construct a float with half-open range [0:1] using low 23 bits.
 // All zeroes yields 0.0, all ones yields the next smallest representable value below 1.0.
-float floatConstruct( uint m ) {
-    const uint ieeeMantissa = 0x007FFFFFu; // binary32 mantissa bitmask
-    const uint ieeeOne      = 0x3F800000u; // 1.0 in IEEE binary32
+float floatConstruct(uint m) {
+    const uint ieeeMantissa = 0x007FFFFFu;  // binary32 mantissa bitmask
+    const uint ieeeOne = 0x3F800000u;       // 1.0 in IEEE binary32
 
-    m &= ieeeMantissa;                     // Keep only mantissa bits (fractional part)
-    m |= ieeeOne;                          // Add fractional part to 1.0
+    m &= ieeeMantissa;  // Keep only mantissa bits (fractional part)
+    m |= ieeeOne;       // Add fractional part to 1.0
 
-    float  f = uintBitsToFloat( m );       // Range [1:2]
-    return f - 1.0;                        // Range [0:1]
+    float f = uintBitsToFloat(m);  // Range [1:2]
+    return f - 1.0;                // Range [0:1]
 }
 
 // Pseudo-random value in half-open range [0:1].
-float random( float x ) { return floatConstruct(hash(floatBitsToUint(x))); }
-float random( vec2  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
-float random( vec3  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
-float random( vec4  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
+float random(float x) { return floatConstruct(hash(floatBitsToUint(x))); }
+float random(vec2 v) { return floatConstruct(hash(floatBitsToUint(v))); }
+float random(vec3 v) { return floatConstruct(hash(floatBitsToUint(v))); }
+float random(vec4 v) { return floatConstruct(hash(floatBitsToUint(v))); }
 
 /*  ---
     Hashes copied from Mark Jarzynski's and Marc Olano's 2020 paper
     Hash Functions for GPU Rendering
-    
+
     These hashes are provided under Creative Commons CC BY-ND 3.0
     --- */
 
 // largest val representable by a 32bit uint
-float uintToFloatbyDiv(uint v) {
-    return v/4294967295.0f;
-}
+float uintToFloatbyDiv(uint v) { return v / 4294967295.0f; }
 
-vec2 uintToFloatbyDiv(uvec2 v) {
-    return v/4294967295.0f;
-}
+vec2 uintToFloatbyDiv(uvec2 v) { return v / 4294967295.0f; }
 
-vec3 uintToFloatbyDiv(uvec3 v) {
-    return v/4294967295.0f;
-}
+vec3 uintToFloatbyDiv(uvec3 v) { return v / 4294967295.0f; }
 
-vec4 uintToFloatbyDiv(uvec4 v) {
-    return v/4294967295.0f;
-}
+vec4 uintToFloatbyDiv(uvec4 v) { return v / 4294967295.0f; }
 
 uint getHashSeed(uint x, uint y, uint timeBits) {
-    uint globalDimX = gl_WorkGroupSize.x*gl_NumWorkGroups.x;
-    uint globalDimY = gl_WorkGroupSize.y*gl_NumWorkGroups.y;
-    uint gid = gl_GlobalInvocationID.x + gl_GlobalInvocationID.y*globalDimX;
+    uint globalDimX = x;
+    uint globalDimY = y;
+    uint gid = gl_GlobalInvocationID.x + gl_GlobalInvocationID.y * globalDimX;
 
     uint hashSeed = gid + floatBitsToUint(timeBits);
     return hashSeed;
-} 
+}
 
 uint pcg(uint v) {
     uint state = v * 747796405u + 2891336453u;
-    uint word = ((state >> ((state >> 28u) + 4u)) ^ state)
-        * 277803737u;
+    uint word = ((state >> ((state >> 28u) + 4u)) ^ state) * 277803737u;
 
     return (word >> 22u) ^ word;
 }
 
 uint pcg_rehash(inout uint v) {
     uint state = v * 747796405u + 2891336453u;
-    uint word = ((state >> ((state >> 28u) + 4u)) ^ state)
-        * 277803737u;
+    uint word = ((state >> ((state >> 28u) + 4u)) ^ state) * 277803737u;
     v = (word >> 22u) ^ word;
     return v;
 }
@@ -112,15 +102,15 @@ uvec2 pcg2d(uvec2 v) {
 uvec3 pcg3d(uvec3 v) {
     v = v * 1664525u + 1013904223u;
 
-    v.x += v.y*v.z;
-    v.y += v.x*v.z;
-    v.z += v.x*v.y;
+    v.x += v.y * v.z;
+    v.y += v.x * v.z;
+    v.z += v.x * v.y;
 
-    v = v ^ (v>>16u);
+    v = v ^ (v >> 16u);
 
-    v.x += v.y*v.z;
-    v.y += v.x*v.z;
-    v.z += v.x*v.y;
+    v.x += v.y * v.z;
+    v.y += v.x * v.z;
+    v.z += v.x * v.y;
 
     return v;
 }
@@ -128,45 +118,33 @@ uvec3 pcg3d(uvec3 v) {
 uvec4 pcg4d(uvec4 v) {
     v = v * 1664525u + 1013904223u;
 
-    v.x += v.y*v.w;
-    v.y += v.z*v.x;
-    v.z += v.x*v.y;
-    v.w += v.y*v.z;
+    v.x += v.y * v.w;
+    v.y += v.z * v.x;
+    v.z += v.x * v.y;
+    v.w += v.y * v.z;
 
-    v = v ^ (v>>16u);
+    v = v ^ (v >> 16u);
 
-    v.x += v.y*v.w;
-    v.y += v.z*v.x;
-    v.z += v.x*v.y;
-    v.w += v.y*v.z;
+    v.x += v.y * v.w;
+    v.y += v.z * v.x;
+    v.z += v.x * v.y;
+    v.w += v.y * v.z;
 
     return v;
 }
 
-float random_1dto1d(uint v) {
-    return uintToFloatbyDiv(pcg(v));
-}
+float random_1dto1d(uint v) { return uintToFloatbyDiv(pcg(v)); }
 
-float random_1dto1d_f(float v) {
-    return uintToFloatbyDiv(pcg(floatBitsToUint(v)));
-}
+float random_1dto1d_f(float v) { return uintToFloatbyDiv(pcg(floatBitsToUint(v))); }
 
-vec3 random_1dto3d (uint v) {
+vec3 random_1dto3d(uint v) {
     uint hash = pcg(v);
     uint hash2 = pcg(v + hash);
     uint hash3 = pcg(v + hash2);
 
-    return uintToFloatbyDiv(uvec3(
-        hash,
-        hash2,
-        hash3
-    )); 
+    return uintToFloatbyDiv(uvec3(hash, hash2, hash3));
 }
 
-vec2 random_2dto2d (uvec2 v) {
-    return uintToFloatbyDiv(pcg2d(v));
-}
+vec2 random_2dto2d(uvec2 v) { return uintToFloatbyDiv(pcg2d(v)); }
 
-float random_2dto1d (uvec2 v) {
-    return uintToFloatbyDiv(pcg(pcg(v.x) + v.y));
-}
+float random_2dto1d(uvec2 v) { return uintToFloatbyDiv(pcg(pcg(v.x) + v.y)); }
