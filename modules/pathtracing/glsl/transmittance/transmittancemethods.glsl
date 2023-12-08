1f5a220e25ef4299663e371e2c7ababdf82f16bd
Hljung <henlj389@student.liu.se>
2023-12-08 13:53:19 +0100

    Pathtracing: Formatting and error fixes

--- modules/pathtracing/glsl/transmittance/transmittancemethods.glsl
+++ modules/pathtracing/glsl/transmittance/transmittancemethods.glsl
@@ -1,54 +1,53 @@
-#include "../random.glsl"
+#include "random.glsl"
 #include "utils/shading.glsl"
 #include "utils/intersection.glsl"
 #include "utils/gradients.glsl"
-#include "../util/vectormatrixmethods.glsl"
+#include "util/vectormatrixmethods.glsl"
 
-float tfToExtinction(float s_max) {
-    return s_max*150f;
-}
+float tfToExtinction(float s_max) { return s_max * 150f; }
 
 // raystart, raydir and raylength ought to be in data coordinate
-float WoodcockTracking(vec3 raystart, vec3 raydir, float tStart, float tEnd, inout uint hashSeed, 
-    sampler3D volume, VolumeParameters volumeParameters, sampler2D transferFunction, float sigma_upperbound) {
-    
-    float invExtinction = 1.f/tfToExtinction(sigma_upperbound);
-    float invSigmaUpperbound = 1.f/sigma_upperbound;
-    float t = tStart; 
+float WoodcockTracking(vec3 raystart, vec3 raydir, float tStart, float tEnd, inout uint hashSeed,
+                       sampler3D volume, VolumeParameters volumeParameters,
+                       sampler2D transferFunction, float sigma_upperbound) {
+
+    float invExtinction = 1.f / tfToExtinction(sigma_upperbound);
+    float invSigmaUpperbound = 1.f / sigma_upperbound;
+    float t = tStart;
     float sigmaSample;
     vec3 r = vec3(0);
-    do {    
-        t += -log(random_1dto1d(pcg_rehash(hashSeed)))*invExtinction;
-        r = raystart + t*raydir;
+    do {
+        t += -log(random_1dto1d(pcg_rehash(hashSeed))) * invExtinction;
+        r = raystart + t * raydir;
         vec4 volumeSample = getNormalizedVoxel(volume, volumeParameters, r);
         sigmaSample = applyTF(transferFunction, volumeSample).a;
 
-    } while (random_1dto1d(pcg_rehash(hashSeed)) >= sigmaSample*invSigmaUpperbound && t <= tEnd);
+    } while (random_1dto1d(pcg_rehash(hashSeed)) >= sigmaSample * invSigmaUpperbound && t <= tEnd);
 
     return t;
 }
 
-float RatioTrackingEstimator(inout vec3 raystart, vec3 raydir, float raylength, uint hashSeed, 
-    sampler3D volume, VolumeParameters volumeParameters, sampler2D transferFunction, float sigma_upperbound) {
-    
+float RatioTrackingEstimator(inout vec3 raystart, vec3 raydir, float raylength, uint hashSeed,
+                             sampler3D volume, VolumeParameters volumeParameters,
+                             sampler2D transferFunction, float sigma_upperbound) {
+
     float t = 0f;
     float T = 1f;
 
     do {
-       float epsilon = random_1dto1d(hashSeed);
+        float epsilon = random_1dto1d(hashSeed);
 
-       t += -log(epsilon)/sigma_upperbound;
-        if(t >= raylength) {
+        t += -log(epsilon) / sigma_upperbound;
+        if (t >= raylength) {
             break;
         }
-        raystart = raystart + t*raydir;
-        float sigma = applyTF(transferFunction, getNormalizedVoxel(volume, volumeParameters, raystart)).a*700;
-        T = T * (1 - sigma/sigma_upperbound);
+        raystart = raystart + t * raydir;
+        float sigma =
+            applyTF(transferFunction, getNormalizedVoxel(volume, volumeParameters, raystart)).a *
+            700;
+        T = T * (1 - sigma / sigma_upperbound);
 
-    } while(true);
-
+    } while (true);
 
     return T;
 }
-
-
