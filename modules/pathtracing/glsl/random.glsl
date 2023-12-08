1ad2214a22e0c2c7fbc99d6b89bfeb9fada88b31
Hljung <henlj389@student.liu.se>
2023-12-08 14:23:39 +0100

    Pathtracing, random: removed old methods and preamble

--- modules/pathtracing/glsl/random.glsl
+++ modules/pathtracing/glsl/random.glsl
@@ -1,55 +1,9 @@
-/*
-   Code for generating random numbers in [0 1] with a uniform distribution.
-   Note: It has not been tested that these methods generate a true uniform distribution.
-   This code has been shared publicly at:
-    https://stackoverflow.com/questions/4200224/random-noise-functions-for-glsl
-    static.frag
-    by Statial
-    05 July 2013
-
-    Not necesarily uniform by this standard however. Consider better hashes
-*/
-
-// A single iteration of Bob Jenkins' One-At-A-Time hashing algorithm.
-uint hash(uint x) {
-    x += (x << 10u);
-    x ^= (x >> 6u);
-    x += (x << 3u);
-    x ^= (x >> 11u);
-    x += (x << 15u);
-    return x;
-}
-
-// Compound versions of the hashing algorithm I whipped together.
-uint hash(uvec2 v) { return hash(v.x ^ hash(v.y)); }
-uint hash(uvec3 v) { return hash(v.x ^ hash(v.y) ^ hash(v.z)); }
-uint hash(uvec4 v) { return hash(v.x ^ hash(v.y) ^ hash(v.z) ^ hash(v.w)); }
-
-// Construct a float with half-open range [0:1] using low 23 bits.
-// All zeroes yields 0.0, all ones yields the next smallest representable value below 1.0.
-float floatConstruct(uint m) {
-    const uint ieeeMantissa = 0x007FFFFFu;  // binary32 mantissa bitmask
-    const uint ieeeOne = 0x3F800000u;       // 1.0 in IEEE binary32
-
-    m &= ieeeMantissa;  // Keep only mantissa bits (fractional part)
-    m |= ieeeOne;       // Add fractional part to 1.0
-
-    float f = uintBitsToFloat(m);  // Range [1:2]
-    return f - 1.0;                // Range [0:1]
-}
-
-// Pseudo-random value in half-open range [0:1].
-float random(float x) { return floatConstruct(hash(floatBitsToUint(x))); }
-float random(vec2 v) { return floatConstruct(hash(floatBitsToUint(v))); }
-float random(vec3 v) { return floatConstruct(hash(floatBitsToUint(v))); }
-float random(vec4 v) { return floatConstruct(hash(floatBitsToUint(v))); }
-
-/*  ---
+/*  
     Hashes copied from Mark Jarzynski's and Marc Olano's 2020 paper
     Hash Functions for GPU Rendering
 
     These hashes are provided under Creative Commons CC BY-ND 3.0
-    --- */
+*/
 
 // largest val representable by a 32bit uint
 float uintToFloatbyDiv(uint v) { return v / 4294967295.0f; }
