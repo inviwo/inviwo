9e6b22f5069a601cb9b554ef6ae085d0008efec1
Hljung <henlj389@student.liu.se>
2023-12-13 17:16:21 +0100

    PathTracing: Various requested changes

--- modules/pathtracing/glsl/util/vectormatrixmethods.glsl
+++ modules/pathtracing/glsl/util/vectormatrixmethods.glsl
@@ -1,41 +1,26 @@
 #include "random.glsl"
 
-mat3 rotMatrixAroundAxel(vec3 v, float theta) {
-    float costheta = cos(theta);
-    float sintheta = sin(theta);
-    mat3 thetaRot = mat3(
-        vec3(costheta + v.x * v.x * (1 - costheta), v.x * v.y * (1 - costheta) - v.z * sintheta,
-             v.x * v.z * (1 - costheta) + v.y * sintheta),
-        vec3(v.y * v.x * (1 - costheta) + v.z * sintheta, costheta + v.y * v.y * (1 - costheta),
-             v.y * v.z * (1 - costheta) - v.x * sintheta),
-        vec3(v.z * v.x * (1 - costheta) - v.y * sintheta,
-             v.z * v.y * (1 - costheta) + v.x * sintheta, costheta + v.z * v.z * (1 - costheta)));
-
-    return thetaRot;
-}
-
-vec3 rotateFrom(vec3 v, float theta, float phi) {
-
-    mat3 thetaRot = rotMatrixAroundAxel(v, theta);
-
-    vec3 w = vec3(-v.y, v.x, 0f);
-    if (v.x == 0 && v.y == 0) {
-        w = vec3(0, 0, 1);
-    }
-    w = normalize(w);
-    mat3 phiRot = rotMatrixAroundAxel(w, phi);
-    vec3 axis = thetaRot * phiRot * v;
-
-    return axis;
-}
-
-// Assumes sample space to be from 000 to 111
-bool rayBoxIntersection_TextureSpace(vec3 origin, vec3 dir, out float t0, out float t1) {
+/**
+ * Test intersection between ray and axis-aligned bounding box.
+ * If intersecting, t0 will contain the first point of intersection along the ray and t1 the last.
+ * If ray is inside the box, t0 will be set to 0.
+ * If t0 is negative it may contain the first hit point on the line (along the negative direction).
+ *
+ * @param bbMin Minimum point of axis-aligned bounding box
+ * @param bbMax Maximum point of axis-aligned bounding box
+ * @param rayOrigin   Ray origin
+ * @param rayDir    Ray direction (not normalized)
+ * @param t0   Start point along ray
+ * @param t1   End point along ray
+ * @return true if an intersection is found, false otherwise.
+ */
+bool rayBoxIntersection(vec3 bbMin, vec3 bbMax vec3 rayOrigin, vec3 rayDir,
+                                     out float t0, out float t1) {
     float FLT_MAX = 3.402823466e+38;
 
     vec3 invDir = 1.f / dir;
-    vec3 tNearV = (vec3(0f, 0f, 0f) - origin) * invDir;
-    vec3 tFarV = (vec3(1f, 1f, 1f) - origin) * invDir;
+    vec3 tNearV = (bbMin - origin) * invDir;
+    vec3 tFarV = (bbMax - origin) * invDir;
     vec3 tNear = min(tNearV, tFarV);
     vec3 tFar = max(tNearV, tFarV);
 
