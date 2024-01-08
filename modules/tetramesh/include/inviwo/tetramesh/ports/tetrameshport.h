0af75a827368ff3eb8b40e473a71c761934808f5
Peter Steneteg <peter@steneteg.se>
2024-01-08 12:34:43 +0100

    Meta: Copyright updates

--- modules/tetramesh/include/inviwo/tetramesh/ports/tetrameshport.h
+++ modules/tetramesh/include/inviwo/tetramesh/ports/tetrameshport.h
@@ -2,7 +2,7 @@
  *
  * Inviwo - Interactive Visualization Workshop
  *
- * Copyright (c) 2023 Inviwo Foundation
+ * Copyright (c) 2023-2024 Inviwo Foundation
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
