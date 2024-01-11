facba1f06f3e33ed5a99609e3951d0be68699d83
Martin Falk <martin.falk@liu.se>
2024-01-11 17:23:51 +0100

    MeshAndVolume: updated copyright

--- modules/meshandvolume/include/inviwo/meshandvolume/meshandvolumemodule.h
+++ modules/meshandvolume/include/inviwo/meshandvolume/meshandvolumemodule.h
@@ -2,7 +2,7 @@
  *
  * Inviwo - Interactive Visualization Workshop
  *
- * Copyright (c) 2022 Inviwo Foundation
+ * Copyright (c) 2022-2024 Inviwo Foundation
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
