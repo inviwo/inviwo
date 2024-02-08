7c06343226f2d171a5c3c44b79f512668b86807a
Hljung <henlj389@student.liu.se>
2024-02-08 18:11:08 +0100

    PathTracing: RayMinMax added
    Rendering ready to use rayMinMax, but calling the function leads to strange side effects

--- modules/pathtracing/glsl/lightestimation.glsl
+++ modules/pathtracing/glsl/lightestimation.glsl
@@ -2,9 +2,11 @@
 #include "transmittance/transmittancemethods.glsl"
 #include "util/rayboxintersection.glsl"
 
-vec3 estimateDirectLight(float rayStep, sampler3D volume, VolumeParameters volParam, sampler2D tf,
-                         vec3 samplePos, vec3 cameraDir, LightParameters light, uint hashSeed,
-                         int rcChannel, int transmittanceMethod) {
+vec3 estimateDirectLightUniformGrid(sampler3D volume, VolumeParameters volParam, sampler2D tf,
+                                    sampler3D opacity, VolumeParameters opacityParam,
+                                    bool partitionedTransmittance, vec3 samplePos, vec3 cameraDir,
+                                    LightParameters light, uint hashSeed, int rcChannel,
+                                    int transmittanceMethod) {
 
     vec3 toLightPath = (volParam.worldToTexture * vec4(light.position, 1f)).xyz - samplePos;
     vec3 toLightDir = normalize(toLightPath);
@@ -13,18 +15,24 @@ vec3 estimateDirectLight(float rayStep, sampler3D volume, VolumeParameters volPa
 
     rayBoxIntersection(vec3(0f), vec3(1f), samplePos, toLightDir, t0, t1);
 
-    float Tl =
-        transmittance(transmittanceMethod, samplePos, toLightDir, t0, t1, hashSeed, volume, volParam, tf);
+    float Tl = 0.0f;
+
+    // Tl = partitionedTransmittanceTracking(transmittanceMethod, samplePos, toLightDir, t0, t1,
+    //                                       hashSeed, volume, volParam, tf, opacity, opacityParam);
+
+    Tl = transmittance(transmittanceMethod, samplePos, toLightDir, t0, t1, hashSeed, volume,
+                       volParam, tf);
 
     if (Tl == 0.0f) {
         return vec3(0f);
     }
-    
-    // NOTE: Shader macros have a tendency to not be recognized sometimes
 
+    // NOTE: Shader macros have a tendency to not be recognized sometimes when first loading test
+    // workspace NOTE: If this occurs, theres a chance the program crashes due to segfault. Is it
+    // related?
     vec4 voxel = getNormalizedVoxel(volume, volParam, samplePos);
     vec3 gradient = COMPUTE_GRADIENT_FOR_CHANNEL(voxel, volume, volParam, samplePos, rcChannel);
-    //vec3 gradient = gradientCentralDiff(vec4(0f), volume, volParam, samplePos, rcChannel);
+    // vec3 gradient = gradientCentralDiff(vec4(0f), volume, volParam, samplePos, rcChannel);
     gradient = normalize(gradient);
 
     gradient *= sign(voxel[rcChannel] / (1.0 - volParam.formatScaling) - volParam.formatOffset);
@@ -39,8 +47,53 @@ vec3 estimateDirectLight(float rayStep, sampler3D volume, VolumeParameters volPa
     vec3 color = APPLY_LIGHTING(light, sampleAmbient, sampleDiffuse, sampleSpecular, sampleWorldPos,
                                 -gradient, cameraDir);
 
-    //vec3 color = shadeBlinnPhong(light, sampleAmbient, sampleDiffuse, sampleSpecular,
-    //                             sampleWorldPos, -gradient, cameraDir);
+    // vec3 color = shadeBlinnPhong(light, sampleAmbient, sampleDiffuse, sampleSpecular,
+    //                              sampleWorldPos, -gradient, cameraDir);
+
+    return color * Tl;
+}
+
+vec3 estimateDirectLight(sampler3D volume, VolumeParameters volParam, sampler2D tf, vec3 samplePos,
+                         vec3 cameraDir, LightParameters light, uint hashSeed, int rcChannel,
+                         int transmittanceMethod) {
+
+    vec3 toLightPath = (volParam.worldToTexture * vec4(light.position, 1f)).xyz - samplePos;
+    vec3 toLightDir = normalize(toLightPath);
+    float t0 = 0.0f;
+    float t1 = 1.f;
+
+    rayBoxIntersection(vec3(0f), vec3(1f), samplePos, toLightDir, t0, t1);
+
+    float Tl = transmittance(transmittanceMethod, samplePos, toLightDir, t0, t1, hashSeed, volume,
+                             volParam, tf);
+
+    if (Tl == 0.0f) {
+        return vec3(0f);
+    }
+
+    // NOTE: Shader macros have a tendency to not be recognized sometimes when first loading test
+    // workspace NOTE: If this occurs, theres a chance the program crashes due to segfault. Is it
+    // related?
+    vec4 voxel = getNormalizedVoxel(volume, volParam, samplePos);
+    // vec3 gradient = COMPUTE_GRADIENT_FOR_CHANNEL(voxel, volume, volParam, samplePos, rcChannel);
+    vec3 gradient = gradientCentralDiff(vec4(0f), volume, volParam, samplePos, rcChannel);
+    gradient = normalize(gradient);
+
+    gradient *= sign(voxel[rcChannel] / (1.0 - volParam.formatScaling) - volParam.formatOffset);
+
+    vec4 tfSample = applyTF(tf, voxel);
+
+    vec3 sampleAmbient = tfSample.rgb;
+    vec3 sampleDiffuse = tfSample.rgb;
+    vec3 sampleSpecular = tfSample.rgb;
+
+    vec3 sampleWorldPos = (volParam.textureToWorld * vec4(samplePos, 1f)).xyz;
+    // vec3 color = APPLY_LIGHTING(light, sampleAmbient, sampleDiffuse, sampleSpecular,
+    // sampleWorldPos,
+    //                            -gradient, cameraDir);
+
+    vec3 color = shadeBlinnPhong(light, sampleAmbient, sampleDiffuse, sampleSpecular,
+                                 sampleWorldPos, -gradient, cameraDir);
 
     return color * Tl;
 }
