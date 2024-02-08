f88d521c3a0787a71823c0d96ebef39e7a190925
Hljung <henlj389@student.liu.se>
2024-02-08 18:08:43 +0100

    UniformGrid: Bug fixing
    TODO: Remove comments and clean up

--- modules/pathtracing/glsl/util/rayminmax.glsl
+++ modules/pathtracing/glsl/util/rayminmax.glsl
@@ -1,7 +1,8 @@
 #include "uniformgrid.glsl"
 
 vec2 rayMinMax(vec3 pos, vec3 dir, float t0, float t1, sampler3D opacity,
-               VolumeParameters opacityParameters, VolumeParameters dataParameters, bool partitionedTransmittance) {
+               VolumeParameters opacityParameters, VolumeParameters dataParameters,
+               bool partitionedTransmittance) {
 
     // problem that needs solving: can we get the cell dimension from the volume?
     // From the data volume and the opacity volume we can solve for region size. data.dimensions /
@@ -9,15 +10,20 @@ vec2 rayMinMax(vec3 pos, vec3 dir, float t0, float t1, sampler3D opacity,
     // opacity processor, it is defaulted to 1,1,1 But I'm thinking 'How could it be anything
     // other?'
 
-    if(!partitionedTransmittance) {
+    if (!partitionedTransmittance) {
         return vec2(0, 1f);
     }
 
+    // NOTE: after testing, it shows that cellDim needs to be 1,1,1, or the sampling from opacity becomes scaled. Why???
     vec3 cellDim = dataParameters.dimensions * opacityParameters.reciprocalDimensions;
+    cellDim = vec3(1f);
 
-    // cast down texttoindex or cast up pos + t0*dir
-    vec3 x1 = (opacityParameters.textureToIndex * vec4(pos + t0 * dir, 1.f)).xyz + 0.5f;
-    vec3 x2 = (opacityParameters.textureToIndex * vec4(pos + t1 * dir, 1.f)).xyz + 0.5f;
+    vec3 x_1 = pos + t0 * dir;
+    vec3 x_2 = pos + t1 * dir;
+    mat4 m = opacityParameters.textureToIndex;
+
+    vec3 x1 = (m * vec4(x_1, 1.f)).xyz + 0.5f;
+    vec3 x2 = (m * vec4(x_2, 1.f)).xyz + 0.5f;
 
     ivec3 cellCoord, cellCoordEnd, di;
     vec3 dt, deltatx;
@@ -27,12 +33,15 @@ vec2 rayMinMax(vec3 pos, vec3 dir, float t0, float t1, sampler3D opacity,
     bool continueTraversal = true;
 
     vec3 minMax = getNormalizedVoxel(opacity, opacityParameters, cellCoord).xyz;
+    int c = 0;
 
     continueTraversal = stepToNextCellNoHit(deltatx, di, cellCoordEnd, dt, cellCoord);
 
-    while(continueTraversal) {
+    while (continueTraversal) {
         vec3 gridMinMaxVal = getNormalizedVoxel(opacity, opacityParameters, cellCoord).xyz;
-        minMax = vec3(min(minMax.x, gridMinMaxVal.x), max(minMax.y, gridMinMaxVal.y), gridMinMaxVal.z);
+        c += 1;
+        minMax =
+            vec3(min(minMax.x, gridMinMaxVal.x), max(minMax.y, gridMinMaxVal.y), gridMinMaxVal.z);
 
         continueTraversal = stepToNextCellNoHit(deltatx, di, cellCoordEnd, dt, cellCoord);
     }
