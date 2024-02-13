1391e9624c028e04b7fabece0a40b81471931747
Hljung <henlj389@student.liu.se>
2024-02-13 17:42:17 +0100

    PathTrace: PartitionedTransmittance
    Transmittance tracking makes use of MinMaxOpacity volume when available

--- modules/pathtracing/glsl/transmittance/transmittancemethods.glsl
+++ modules/pathtracing/glsl/transmittance/transmittancemethods.glsl
@@ -91,44 +91,73 @@ float residualRatioTrackingTransmittance(vec3 raystart, vec3 raydir, float tStar
     return Tr * Tc;
 }
 
-float partitionedTransmittanceTracking(int METHOD, vec3 raystart, vec3 raydir, float tStart, float tEnd,
-                    inout uint hashSeed, sampler3D volume, VolumeParameters volumeParameters,
-                    sampler2D transferFunction, sampler3D opacity, VolumeParameters opacityParameters) {
-    
-    
+float partitionedTransmittanceTracking(int METHOD, vec3 raystart, vec3 raydir, float tStart,
+                                       float tEnd, inout uint hashSeed, sampler3D volume,
+                                       VolumeParameters volumeParameters,
+                                       sampler2D transferFunction, sampler3D opacity,
+                                       VolumeParameters opacityParameters) {
 
-    float opacityUpperbound = 1.0f;
-    float opacityControl = 0.5f;
+    mat4 volumeTexToIndMat = volumeParameters.textureToIndex;
 
-    /*
-    vec2 minMax = rayMinMax(raystart, raydir, tStart, tEnd, opacity, opacityParameters, volumeParameters);
-    opacityUpperbound = minMax.y;
-    */
+    vec3 x1 = (volumeTexToIndMat * vec4(raystart + tStart * raydir, 1.0f)).xyz + 0.5f;
+    vec3 x2 = (volumeTexToIndMat * vec4(raystart + tEnd * raydir, 1.0f)).xyz + 0.5f;
 
-    switch (METHOD) {
-        case WOODCOCK:
-            float meanFreePath =
-                woodcockTracking(raystart, raydir, tStart, tEnd, hashSeed, volume, volumeParameters,
-                                 transferFunction, opacityUpperbound);
-            return meanFreePath >= tEnd ? 1f : 0f;
-        case RATIO:
-            return ratioTrackingTransmittance(raystart, raydir, tStart, tEnd, hashSeed, volume,
-                                              volumeParameters, transferFunction,
-                                              opacityUpperbound);
-        case RESIDUALRATIO:
-            return residualRatioTrackingTransmittance(raystart, raydir, tStart, tEnd, hashSeed,
-                                                      volume, volumeParameters, transferFunction,
-                                                      opacityUpperbound, opacityControl);
+    vec3 cellDim = volumeParameters.dimensions * opacityParameters.reciprocalDimensions;
+    ivec3 cellCoord = ivec3(x1 / cellDim);
+
+    if (tEnd <= tStart) {
+        return 1.f;
     }
 
-    return 0f;
+    ivec3 cellCoordEnd, di;
+    vec3 dt, deltatx;
+    setupUniformGridTraversal(x1, x2, cellDim, ivec3(volumeParameters.dimensions), cellCoord, cellCoordEnd,
+                              di, dt, deltatx);
+    float dirLen = tEnd - tStart;
+    float t = 0.f;
+
+    bool continueTraversal = true;
+    float T = 1.f;
+    while(continueTraversal) {
+        vec3 minMax = getNormalizedVoxel(opacity, opacityParameters, cellCoord).xyz;
+        float tPrev = t;
+        continueTraversal = stepToNextCellHit(deltatx, di, cellCoordEnd, dt, cellCoord, t);
+        if(minMax.y <= 0) {
+            continue;
+        }
+        
+        float t0 = tStart + dirLen * tPrev;
+        float t1 = tStart + dirLen * min(1.f, t);
+
+        minMax.y += 1e-6;
+        float avg = minMax.z;
+
+        switch (METHOD) {
+            case WOODCOCK:
+                float meanFreePath =
+                    woodcockTracking(raystart, raydir, t0, t1, hashSeed, volume, volumeParameters,
+                                    transferFunction, minMax.y);
+                T *= meanFreePath >= t1 ? 1f : 0f;
+                break;
+            case RATIO:
+                T *= ratioTrackingTransmittance(raystart, raydir, t0, t1, hashSeed, volume,
+                                                volumeParameters, transferFunction,
+                                                minMax.y);
+                break;
+            case RESIDUALRATIO:
+                T *= residualRatioTrackingTransmittance(raystart, raydir, t0, t1, hashSeed,
+                                                        volume, volumeParameters, transferFunction,
+                                                        minMax.y, avg);
+                break;
+        }
+    }
+
+    return T;
 }
 
 float transmittance(int METHOD, vec3 raystart, vec3 raydir, float tStart, float tEnd,
                     inout uint hashSeed, sampler3D volume, VolumeParameters volumeParameters,
                     sampler2D transferFunction) {
-
-
 
     float opacityUpperbound = 1.0f;
     float opacityControl = 0.5f;
