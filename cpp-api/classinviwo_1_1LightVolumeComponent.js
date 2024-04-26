66b97fdf134684d753f9cb42f497e3f1719adb64
Inviwo Team <team@inviwo.org>
2024-04-26 13:12:05 +0000

    Update C++ API Docs

---
+++ cpp-api/classinviwo_1_1LightVolumeComponent.js
@@ -0,0 +1,9 @@
+var classinviwo_1_1LightVolumeComponent =
+[
+    [ "getInports", "classinviwo_1_1LightVolumeComponent.html#acba5e48c0fccc5fdf5fa852ba9229c0d", null ],
+    [ "getName", "classinviwo_1_1LightVolumeComponent.html#ae7fbee0ea5c7159405e4b8a87a513cdc", null ],
+    [ "getProperties", "classinviwo_1_1LightVolumeComponent.html#aa71388d583feb2aa001bf2396f03d6c5", null ],
+    [ "getSegments", "classinviwo_1_1LightVolumeComponent.html#a1ee8d4616a2431442302086f593e9f0e", null ],
+    [ "initializeResources", "classinviwo_1_1LightVolumeComponent.html#a6cdcd6af70d95a2cc4f9bda140a3496e", null ],
+    [ "process", "classinviwo_1_1LightVolumeComponent.html#ab68246bd4b6b31535b4bd296e292732b", null ]
+];
