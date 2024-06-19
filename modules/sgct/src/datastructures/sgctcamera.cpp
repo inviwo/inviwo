70f5f7f950e607e1fdaeb2e0e9902daaf26ef808
Inviwo Team <team@inviwo.org>
2024-06-19 12:14:22 +0200

    Jenkins: Format fixes

--- modules/sgct/src/datastructures/sgctcamera.cpp
+++ modules/sgct/src/datastructures/sgctcamera.cpp
@@ -50,9 +50,9 @@ SGCTCamera::SGCTCamera(const SGCTCamera& other) = default;
 
 SGCTCamera& SGCTCamera::operator=(const SGCTCamera& other) = default;
 
-SGCTCamera::SGCTCamera( SGCTCamera&& other) = default;
+SGCTCamera::SGCTCamera(SGCTCamera&& other) = default;
 
-SGCTCamera& SGCTCamera::operator=( SGCTCamera&& other) = default;
+SGCTCamera& SGCTCamera::operator=(SGCTCamera&& other) = default;
 
 SGCTCamera* SGCTCamera::clone() const { return new SGCTCamera(*this); }
 
