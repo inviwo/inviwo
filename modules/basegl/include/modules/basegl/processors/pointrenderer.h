1a5275aa388be471d5451924209bd51dd1925f30
Martin Falk <martin.falk@liu.se>
2024-07-16 11:47:04 +0200

    BaseGL: depth test in PointRenderer

--- modules/basegl/include/modules/basegl/processors/pointrenderer.h
+++ modules/basegl/include/modules/basegl/processors/pointrenderer.h
@@ -29,16 +29,17 @@
 
 #pragma once
 
-#include <modules/basegl/baseglmoduledefine.h>  // for IVW_MODULE_BASEGL_API
+#include <modules/basegl/baseglmoduledefine.h>
 
-#include <inviwo/core/interaction/cameratrackball.h>  // for CameraTrackball
-#include <inviwo/core/ports/imageport.h>              // for ImageInport, ImageOutport
-#include <inviwo/core/ports/meshport.h>               // for MeshFlatMultiInport
-#include <inviwo/core/processors/processor.h>         // for Processor
-#include <inviwo/core/processors/processorinfo.h>     // for ProcessorInfo
-#include <inviwo/core/properties/cameraproperty.h>    // for CameraProperty
-#include <inviwo/core/properties/ordinalproperty.h>   // for FloatProperty, FloatVec4Property
-#include <modules/opengl/shader/shader.h>             // for Shader
+#include <inviwo/core/interaction/cameratrackball.h>
+#include <inviwo/core/ports/imageport.h>
+#include <inviwo/core/ports/meshport.h>
+#include <inviwo/core/processors/processor.h>
+#include <inviwo/core/processors/processorinfo.h>
+#include <inviwo/core/properties/cameraproperty.h>
+#include <inviwo/core/properties/ordinalproperty.h>
+#include <inviwo/core/properties/boolproperty.h>
+#include <modules/opengl/shader/shader.h>
 
 namespace inviwo {
 
@@ -86,6 +87,7 @@ private:
     FloatProperty borderWidth_;
     FloatVec4Property borderColor_;
     FloatProperty antialising_;
+    BoolProperty depthTest_;
 
     CameraProperty camera_;
     CameraTrackball trackball_;
