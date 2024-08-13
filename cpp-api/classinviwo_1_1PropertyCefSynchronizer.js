9c8111cc289dc859458f4719582bc51551ee3d86
Inviwo Team <team@inviwo.org>
2024-08-13 11:52:33 +0000

    Update C++ API Docs

--- cpp-api/classinviwo_1_1PropertyCefSynchronizer.js
+++ cpp-api/classinviwo_1_1PropertyCefSynchronizer.js
@@ -2,5 +2,5 @@ var classinviwo_1_1PropertyCefSynchronizer =
 [
     [ "OnLoadEnd", "classinviwo_1_1PropertyCefSynchronizer.html#ac3caea1097e92d8c144128b8fab535ec", null ],
     [ "OnLoadStart", "classinviwo_1_1PropertyCefSynchronizer.html#a13415cd5dc12bc35e66e832d9099e0c7", null ],
-    [ "OnQuery", "classinviwo_1_1PropertyCefSynchronizer.html#a37207640dd7737391616b141e544b1bc", null ]
+    [ "OnQuery", "classinviwo_1_1PropertyCefSynchronizer.html#a90c59ed1881c01a1ea75bc842fe0ba00", null ]
 ];
