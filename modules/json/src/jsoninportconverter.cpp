2dd468aa36165d70b35561e15fde9f9896869de2
Peter Steneteg <peter@steneteg.se>
2024-09-25 17:36:19 +0200

    WebBrowser/WebQt/JSON/Various: Major refactoring and enhancements
    - Added `WebQt` module with HTML/JS syntax highlighting.
    - Introduced `NetWorkCefSynchronizer` for CEF synchronization.
    - Refactored `WebBrowserClient` and `WebBrowserModule`.
    - Added `BasicWebBrowser` processor as a generic webbrowser proessor
    - Improved error handling and logging.
    - CEF uses opengl textures to pass frames instead of CPU on windows
    - Major refactoring of the JSON converter "factories"
    - Removed the HTMLWidget factory.
    - Added a new inviwoapiv2.js API

--- modules/json/src/jsoninportconverter.cpp
+++ modules/json/src/jsoninportconverter.cpp
@@ -2,7 +2,7 @@
  *
  * Inviwo - Interactive Visualization Workshop
  *
- * Copyright (c) 2019-2024 Inviwo Foundation
+ * Copyright (c) 2024 Inviwo Foundation
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,32 +27,6 @@
  *
  *********************************************************************************/
 
-#pragma once
+#include <modules/json/jsoninportconverter.h>
 
-#include <modules/webbrowser/webbrowsermoduledefine.h>  // for IVW_MODULE_WEB...
-
-#include <inviwo/core/util/factory.h>                                      // for StandardFactory
-#include <modules/webbrowser/properties/propertywidgetcef.h>               // for PropertyWidgetCEF
-#include <modules/webbrowser/properties/propertywidgetceffactoryobject.h>  // for PropertyWidget...
-
-#include <memory>  // for unique_ptr
-#include <string>  // for string
-#include <vector>  // for vector
-
-namespace inviwo {
-class Property;
-
-/**
- * Factory for creating PropertyWidgetCEF for a property.
- * The PropertyWidgetCEF requires a PropertyJSONConverter its corresponding
- * Property.
- */
-class IVW_MODULE_WEBBROWSER_API PropertyWidgetCEFFactory
-    : public StandardFactory<PropertyWidgetCEF, PropertyWidgetCEFFactoryObject, const std::string&,
-                             Property*> {
-public:
-    PropertyWidgetCEFFactory();
-    virtual ~PropertyWidgetCEFFactory();
-};
-
-}  // namespace inviwo
+namespace inviwo {}  // namespace inviwo
