7ae41ab7899cd865883cb7f09d36a4c17441bf58
Peter Steneteg <peter@steneteg.se>
2024-10-26 21:09:08 +0200

    Core: Added serialization benchmark

---
+++ tests/benchmarks/serialization-benchmark.cpp
@@ -0,0 +1,117 @@
+/*********************************************************************************
+ *
+ * Inviwo - Interactive Visualization Workshop
+ *
+ * Copyright (c) 2013-2024 Inviwo Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************************/
+#ifdef _MSC_VER
+#pragma comment(linker, "/SUBSYSTEM:CONSOLE")
+#endif
+
+#include <inviwo/core/common/defaulttohighperformancegpu.h>
+#include <inviwo/core/common/inviwoapplication.h>
+#include <inviwo/core/network/workspacemanager.h>
+#include <inviwo/core/network/processornetwork.h>
+#include <inviwo/core/util/logcentral.h>
+#include <inviwo/core/util/consolelogger.h>
+#include <inviwo/core/util/utilities.h>
+#include <inviwo/core/util/raiiutils.h>
+#include <inviwo/core/util/commandlineparser.h>
+
+#include <inviwo/sys/moduleregistration.h>
+#include <inviwo/sys/moduleloading.h>
+
+#include <fmt/format.h>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include <memory>
+
+#include <benchmark/benchmark.h>
+
+using namespace inviwo;
+
+struct Log {
+    Log() : logCentral{}, log{std::make_shared<ConsoleLogger>()} {
+        LogCentral::init(&logCentral);
+        LogCentral::getPtr()->registerLogger(log);
+        logCentral.setVerbosity(LogVerbosity::Error);
+    }
+
+    LogCentral logCentral{};
+    std::shared_ptr<ConsoleLogger> log;
+};
+
+struct App {
+    App() : log{}, inviwo{} {
+        inviwo.printApplicationInfo();
+        inviwo.setPostEnqueueFront([]() { glfwPostEmptyEvent(); });
+
+        // Initialize all modules
+        inviwo::util::registerModulesFiltered(
+            inviwo.getModuleManager(),
+            [](ModuleContainer& container) { return container.name().contains("Qt"); });
+    }
+
+    virtual ~App() { }
+
+    Log log;
+    InviwoApplication inviwo;
+};
+
+static std::unique_ptr<App> app{};
+
+struct Fixture : ::benchmark::Fixture {
+    void SetUp(::benchmark::State& state) {
+        if (!app) {
+            app = std::make_unique<App>();
+        }
+    }
+    void TearDown(::benchmark::State& state) {}
+};
+
+BENCHMARK_DEFINE_F(Fixture, Loading)(benchmark::State& st) {
+    auto workspace = app->inviwo.getPath(PathType::Workspaces) / "boron.inv";
+    for (auto _ : st) {
+        app->inviwo.getWorkspaceManager()->load(workspace);
+        app->inviwo.getWorkspaceManager()->clear();
+    }
+}
+
+BENCHMARK_DEFINE_F(Fixture, Saving)(benchmark::State& st) {
+    auto workspace = app->inviwo.getPath(PathType::Workspaces) / "boron.inv";
+    app->inviwo.getWorkspaceManager()->load(workspace);
+    for (auto _ : st) {
+        std::stringstream ss;
+        app->inviwo.getWorkspaceManager()->save(ss, workspace);
+        benchmark::DoNotOptimize(ss);
+    }
+}
+
+BENCHMARK_REGISTER_F(Fixture, Saving)->Unit(benchmark::kMillisecond)->MinTime(10.0);
+//BENCHMARK_REGISTER_F(Fixture, Loading)->Unit(benchmark::kMillisecond)->MinTime(5.0);
+
+BENCHMARK_MAIN();
