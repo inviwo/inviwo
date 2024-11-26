bf129e864fa48ba01dc8a3f6bafd2d63265778ce
Peter Steneteg <peter@steneteg.se>
2024-11-26 11:34:43 +0100

    Various: adding missing &

--- modules/webbrowser/include/modules/webbrowser/processors/webbrowserprocessor.h
+++ modules/webbrowser/include/modules/webbrowser/processors/webbrowserprocessor.h
@@ -87,7 +87,7 @@ public:
 
     virtual void deserialize(Deserializer& d) override;
 
-    virtual const ProcessorInfo getProcessorInfo() const override;
+    virtual const ProcessorInfo& getProcessorInfo() const override;
     static const ProcessorInfo processorInfo_;
 
 private:
