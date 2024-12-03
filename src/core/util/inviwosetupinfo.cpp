6fb0f201df8a4480dfb7dec77ad795b81f5eda74
Peter Steneteg <peter@steneteg.se>
2024-12-03 09:47:58 +0100

    Core: typo fix

--- src/core/util/inviwosetupinfo.cpp
+++ src/core/util/inviwosetupinfo.cpp
@@ -68,7 +68,7 @@ InviwoSetupInfo::InviwoSetupInfo(const InviwoApplication& app, ProcessorNetwork&
             }
         }
         if (!processors.empty()) {
-            modules_.emplace_back(inviwoModule.getIdentifier(), inviwoModule.getVersion(),
+            modules_.emplace_back(inviwoModule.getVersion(), inviwoModule.getIdentifier(),
                                   std::move(processors));
         }
     }
