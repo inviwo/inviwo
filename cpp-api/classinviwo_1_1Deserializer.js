9d505e5e7fd7417362d1a80e051ea2b269943307
Inviwo Team <team@inviwo.org>
2024-12-16 21:56:45 +0000

    Update C++ API Docs

--- cpp-api/classinviwo_1_1Deserializer.js
+++ cpp-api/classinviwo_1_1Deserializer.js
@@ -1,8 +1,8 @@
 var classinviwo_1_1Deserializer =
 [
-    [ "Deserializer", "classinviwo_1_1Deserializer.html#a82f0f553a0463dc442938852d043d433", null ],
-    [ "Deserializer", "classinviwo_1_1Deserializer.html#aadd99ab5a5a5494915e613874f7f2421", null ],
-    [ "Deserializer", "classinviwo_1_1Deserializer.html#ab772bd874826075d68320c3e14de46c1", null ],
+    [ "Deserializer", "classinviwo_1_1Deserializer.html#a9b2828aaaa31e90c4240edd8428ba638", null ],
+    [ "Deserializer", "classinviwo_1_1Deserializer.html#afa8c9a7b085282b9a3da1e6f5c147bdb", null ],
+    [ "Deserializer", "classinviwo_1_1Deserializer.html#ac489c44a45a711d08bc9b86a7d7226b2", null ],
     [ "deserialize", "classinviwo_1_1Deserializer.html#a6d23d60c0e71d30b36f0d49b0316b8e4", null ],
     [ "deserialize", "classinviwo_1_1Deserializer.html#a689235b7b76c295fb6ef7d7a8e731f01", null ],
     [ "deserialize", "classinviwo_1_1Deserializer.html#a480408a47751ac362ba7aa8f783a68b6", null ],
--- cpp-api/classinviwo_1_1Serializer.js
+++ cpp-api/classinviwo_1_1Serializer.js
@@ -1,6 +1,6 @@
 var classinviwo_1_1Serializer =
 [
-    [ "Serializer", "classinviwo_1_1Serializer.html#ac2e513dcb5502e1703736900bcf7ba01", null ],
+    [ "Serializer", "classinviwo_1_1Serializer.html#a5c8e59ec66a0a3d776f5e21a59402edd", null ],
     [ "writeFile", "classinviwo_1_1Serializer.html#a9d4c6a2bb0202db839c85a819b72d051", null ],
     [ "writeFile", "classinviwo_1_1Serializer.html#aaa3f7c110082cff7101918df18800843", null ]
 ];
