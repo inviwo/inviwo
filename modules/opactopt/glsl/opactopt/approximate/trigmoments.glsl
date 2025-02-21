22408c590aa924ed04f1dfbbfac6817d4c777837
arrebarritra <arre2barre@gmail.com>
2025-02-21 15:21:35 +0100

    Add proper licensing to moment code

--- modules/opactopt/glsl/opactopt/approximate/trigmoments.glsl
+++ modules/opactopt/glsl/opactopt/approximate/trigmoments.glsl
@@ -185,16 +185,69 @@ float total(layout(size1x32) image2DArray coeffTex, int N)
 }
 
 /**
- * This file is part of an Vulkan GLSL port of the HLSL code accompanying the paper "Moment-Based
+ * The code below is adapted from LineVis (https://github.com/chrismile/LineVis/),
+ * which in turn adapted their code from HLSL code accompanying the paper "Moment-Based
  * Order-Independent Transparency" by Münstermann, Krumpen, Klein, and Peters
  * (http://momentsingraphics.de/?page_id=210). The original code was released in accordance to CC0
  * (https://creativecommons.org/publicdomain/zero/1.0/).
- *
- * This port is released under the terms of BSD 2-Clause License. For more details please see the
- * LICENSE file in the root directory of this project.
- *
- * Changes for the Vulkan GLSL port: Copyright 2018-2022 Christoph Neuhauser
  */
+
+/*
+* BSD 2-Clause License
+* 
+* Copyright (c) 2020-2022, Christoph Neuhauser, Michael Kern, Felix Brendel
+* All rights reserved.
+* 
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+* 
+* 1. Redistributions of source code must retain the above copyright notice, this
+*    list of conditions and the following disclaimer.
+* 
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+*    this list of conditions and the following disclaimer in the documentation
+*    and/or other materials provided with the distribution.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* 
+* 
+* ----------------------------------------------------------------------------
+* 
+* The following license applies to
+* src/Renderers/Vulkan/Scattering/PathTracer/VolumetricPathTracingPass.{hpp,cpp}
+* and src/Data/Shaders/Vulkan/Scattering/Clouds/Clouds.glsl.
+* 
+* MIT License
+* 
+* Copyright (c) 2021, Christoph Neuhauser, Ludwig Leonard
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
 
 float rsqrt(float x) { return 1.0 / x; }
 
