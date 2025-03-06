7a7abe9979da35e4e715c6f01b893b63a4124a60
Peter Steneteg <peter@steneteg.se>
2025-03-06 09:51:48 +0100

    Plotting: added a OrthographicAxis2D processor WIP

---
+++ modules/plottinggl/include/modules/plottinggl/processors/orthographicaxis2d.h
@@ -0,0 +1,69 @@
+/*********************************************************************************
+ *
+ * Inviwo - Interactive Visualization Workshop
+ *
+ * Copyright (c) 2025 Inviwo Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************************/
+
+#pragma once
+
+#include <modules/plottinggl/plottingglmoduledefine.h>
+#include <inviwo/core/processors/processor.h>
+#include <modules/plotting/properties/axisproperty.h>       // for AxisProperty
+#include <modules/plotting/properties/axisstyleproperty.h>  // for AxisStyleProperty
+#include <modules/plottinggl/utils/axisrenderer.h>          // for AxisRenderer
+#include <inviwo/core/ports/imageport.h>
+#include <inviwo/core/ports/meshport.h>
+#include <inviwo/core/properties/cameraproperty.h>
+#include <inviwo/core/properties/marginproperty.h>
+
+namespace inviwo {
+
+class IVW_MODULE_PLOTTINGGL_API OrthographicAxis2D : public Processor {
+public:
+    OrthographicAxis2D();
+
+    virtual void process() override;
+
+    virtual const ProcessorInfo& getProcessorInfo() const override;
+    static const ProcessorInfo processorInfo_;
+
+private:
+    ImageInport inport_;
+    MeshInport mesh_;
+    ImageOutport outport_;
+
+    plot::AxisStyleProperty style_;
+    plot::AxisProperty axis1_;
+    plot::AxisProperty axis2_;
+    MarginProperty margins_;
+    FloatProperty axisMargin_;
+    BoolProperty antialiasing_;
+    CameraProperty camera_;
+
+    std::array<plot::AxisRenderer, 2> axisRenderers_;
+};
+
+}  // namespace inviwo
