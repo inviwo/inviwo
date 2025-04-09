17c13f45675b5efe70fb52b1188764d02e9a9fd7
Peter Steneteg <peter@steneteg.se>
2025-04-09 21:10:31 +0200

    Core: don't change the identifier when replacing processors

--- src/core/network/networkutils.cpp
+++ src/core/network/networkutils.cpp
@@ -475,8 +475,9 @@ std::shared_ptr<Processor> replaceProcessor(ProcessorNetwork* network,
     util::setPosition(newProcessor.get(), util::getPosition(oldProcessor));
 
     network->addProcessor(newProcessor);
-
-    return replaceProcessor(network, *newProcessor, oldProcessor);
+    auto old =  replaceProcessor(network, *newProcessor, oldProcessor);
+    newProcessor->setIdentifier(old->getIdentifier());
+    return old;
 }
 
 std::shared_ptr<Processor> replaceProcessor(ProcessorNetwork* network, Processor& newProcessor,
