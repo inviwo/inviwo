5199a47037a48cb119abdb1579f7146fe215cc17
Martin Falk <martin.falk@liu.se>
2025-05-06 10:08:21 +0200

    Data: fixed Python script

--- data/scripts/camerarotation.py
+++ data/scripts/camerarotation.py
@@ -12,7 +12,11 @@ network = app.network
 
 cam = network.EntryExitPoints.camera
 
-start = time.clock()
+old_lookto = cam.lookTo
+old_lookup = cam.lookUp
+old_lookfrom = cam.lookFrom
+
+start = time.time()
 
 scale = 1
 d = 15
@@ -26,7 +30,6 @@ for i in range(0, steps):
     cam.lookFrom = vec3(x * scale, 3 * scale, z * scale)
     inviwo_utils.update()  # Needed for canvas to update
 
-
 for i in range(0, steps):
     r = (2 * math.pi * i) / (steps - 1)
     x = 1.0 * math.sin(r)
@@ -34,10 +37,14 @@ for i in range(0, steps):
     cam.lookUp = vec3(x * scale, z * scale, 0)
     inviwo_utils.update()  # Needed for canvas to update
 
-end = time.clock()
+end = time.time()
 fps = 2 * steps / (end - start)
 
 fps = round(fps, 3)
+
+cam.lookTo = old_lookto
+cam.lookUp = old_lookup
+cam.lookFrom = old_lookfrom
 
 print("Frames per second: " + str(fps))
 print("Time per frame: " + str(round(1000 / fps, 1)) + " ms")
