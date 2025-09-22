5b48daba56640722609405528c9011fe0ad0d6d4
Martin Falk <martin.falk@liu.se>
2025-09-22 09:49:24 +0200

    Base: AmiraMesh reader
    * alternative `Data` field

--- modules/base/src/io/amirameshreader.cpp
+++ modules/base/src/io/amirameshreader.cpp
@@ -226,7 +226,10 @@ std::shared_ptr<Mesh> AmiraMeshReader::readData(const std::filesystem::path& pat
     if (meshType == MeshType::Lines) {
         auto indexSpec = getDataSpecForType(header, "LineIdx");
         auto coordinatesSpec = getDataSpecForType(header, "Coordinates");
-        auto vertexDataSpec = getDataSpecForType(header, "Data0");  // optional
+        auto vertexDataSpec = getDataSpecForType(header, "Data");  // optional
+        if (!vertexDataSpec.has_value()) {
+            vertexDataSpec = getDataSpecForType(header, "Data0");
+        }
 
         if (!indexSpec.has_value()) {
             throw DataReaderException(SourceContext{}, "{}: {}", indexSpec.error(), path);
