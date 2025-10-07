arrebarritra/inviwo-opacity-optimisation

Refactoring, expose more options through properties, moment approximation

modules/opactopt/glsl/opactopt/direct/line/projectimportance.frag
@@ -55,12 +55,15 @@ uniform float lineWidth = 2.0; // line width [pixel]
 // without any transformation, i.e. all lines in clip space
 uniform CameraParameters camera = CameraParameters( mat4(1), mat4(1), mat4(1), mat4(1),
                                     mat4(1), mat4(1), vec3(0), 0, 1);
+uniform vec2 reciprocalDimensions;
 
-uniform layout(r32i) iimage2DArray importanceSumCoeffs[2]; // double buffering for gaussian filtering
-uniform layout(r32i) iimage2DArray opticalDepthCoeffs;
+uniform layout(size1x32) iimage2DArray importanceSumCoeffs[2]; // double buffering for gaussian filtering
+uniform layout(size1x32) iimage2DArray opticalDepthCoeffs;
 
+#ifdef USE_IMPORTANCE_VOLUME
 uniform sampler3D importanceVolume;
 uniform VolumeParameters importanceVolumeParameters;
+#endif
 
 // line stippling
 uniform StipplingParameters stippling = StipplingParameters(30.0, 10.0, 0.0, 4.0);
@@ -84,6 +87,12 @@ uniform float lambda;
 #endif
 #ifdef PIECEWISE
     #include "opactopt/approximate/piecewise.glsl"
+#endif
+#ifdef POWER_MOMENTS
+    #include "opactopt/approximate/powermoments.glsl"
+#endif
+#ifdef TRIG_MOMENTS
+    #include "opactopt/approximate/trigmoments.glsl"
 #endif
 
 void main() {
@@ -118,13 +127,14 @@ void main() {
 
     // Calculate g_i^2
 #ifdef USE_IMPORTANCE_VOLUME
+    vec2 texCoord = gl_FragCoord.xy * reciprocalDimensions;
     float viewDepth = depth * (camera.farPlane - camera.nearPlane) + camera.nearPlane;
     float clipDepth = convertDepthViewToClip(camera, viewDepth);
     vec4 clip = vec4(2.0 * texCoord - 1.0, clipDepth, 1.0);
     vec4 worldPos = camera.clipToWorld * clip;
     worldPos /= worldPos.w;
     vec3 texPos = (importanceVolumeParameters.worldToTexture * worldPos).xyz * importanceVolumeParameters.reciprocalDimensions;
-    float gi = texture(importanceVolume, texPos.xyz).x; // sample importance from volume
+    float gi = clamp(texture(importanceVolume, texPos.xyz).x, 0.0, 1.0); // sample importance from volume
 #else
     float gi = color_.a;
 #endif

inviwo/inviwo

modules/opactopt/glsl/opactopt/direct/line/projectimportance.frag
@@ -55,12 +55,20 @@ uniform float lineWidth = 2.0; // line width [pixel]
 // without any transformation, i.e. all lines in clip space
 uniform CameraParameters camera = CameraParameters( mat4(1), mat4(1), mat4(1), mat4(1),
                                     mat4(1), mat4(1), vec3(0), 0, 1);
+uniform vec2 reciprocalDimensions;
 
+#ifdef COEFF_TEX_FIXED_POINT_FACTOR
 uniform layout(r32i) iimage2DArray importanceSumCoeffs[2]; // double buffering for gaussian filtering
 uniform layout(r32i) iimage2DArray opticalDepthCoeffs;
+#else
+uniform layout(size1x32) image2DArray importanceSumCoeffs[2]; // double buffering for gaussian filtering
+uniform layout(size1x32) image2DArray opticalDepthCoeffs;
+#endif
 
+#ifdef USE_IMPORTANCE_VOLUME
 uniform sampler3D importanceVolume;
 uniform VolumeParameters importanceVolumeParameters;
+#endif
 
 // line stippling
 uniform StipplingParameters stippling = StipplingParameters(30.0, 10.0, 0.0, 4.0);
@@ -84,6 +92,12 @@ uniform float lambda;
 #endif
 #ifdef PIECEWISE
     #include "opactopt/approximate/piecewise.glsl"
+#endif
+#ifdef POWER_MOMENTS
+    #include "opactopt/approximate/powermoments.glsl"
+#endif
+#ifdef TRIG_MOMENTS
+    #include "opactopt/approximate/trigmoments.glsl"
 #endif
 
 void main() {
@@ -118,13 +132,14 @@ void main() {
 
     // Calculate g_i^2
 #ifdef USE_IMPORTANCE_VOLUME
+    vec2 texCoord = gl_FragCoord.xy * reciprocalDimensions;
     float viewDepth = depth * (camera.farPlane - camera.nearPlane) + camera.nearPlane;
     float clipDepth = convertDepthViewToClip(camera, viewDepth);
     vec4 clip = vec4(2.0 * texCoord - 1.0, clipDepth, 1.0);
     vec4 worldPos = camera.clipToWorld * clip;
     worldPos /= worldPos.w;
     vec3 texPos = (importanceVolumeParameters.worldToTexture * worldPos).xyz * importanceVolumeParameters.reciprocalDimensions;
-    float gi = texture(importanceVolume, texPos.xyz).x; // sample importance from volume
+    float gi = clamp(texture(importanceVolume, texPos.xyz).x, 0.0, 1.0); // sample importance from volume
 #else
     float gi = color_.a;
 #endif