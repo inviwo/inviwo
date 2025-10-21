ac22dacb9761aa3d165504b52e45548af0d8cc49
Inviwo Team <team@inviwo.org>
2025-10-21 11:36:02 +0000

    Update C++ API Docs

--- cpp-api/classinviwo_1_1VolumeReuseCache.js
+++ cpp-api/classinviwo_1_1VolumeReuseCache.js
@@ -2,5 +2,5 @@ var classinviwo_1_1VolumeReuseCache =
 [
     [ "get", "classinviwo_1_1VolumeReuseCache.html#a1d2d6deebdc0c8b52f647aeebd857144", null ],
     [ "getConfig", "classinviwo_1_1VolumeReuseCache.html#a57f8963be7a083732071fcffb16d4170", null ],
-    [ "setConfig", "classinviwo_1_1VolumeReuseCache.html#a4fafb09b916836fe2c9cbe7c8fcca0e6", null ]
+    [ "setConfig", "classinviwo_1_1VolumeReuseCache.html#ab52d35a6d1c74b28b4799955da21983a", null ]
 ];
