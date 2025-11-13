7e22df868e569c23cf8d011b341179a8236220d2
Peter Steneteg <peter@steneteg.se>
2025-11-13 15:42:30 +0100

    Base/BaseGL: fixes

--- modules/base/include/modules/base/algorithm/pointgeneration.h
+++ modules/base/include/modules/base/algorithm/pointgeneration.h
@@ -100,8 +100,8 @@ void generatePoints(OutIt outIt, std::variant<Grid3DPointGeneration, RandomCubic
                         return glm::scale(dvec3{1.0} /
                                           dvec3{glm::max(opts.nPoints, size3_t{2}) - size3_t{1}});
                     } else if (opts.mode == Grid3DPointGeneration::Mode::Center) {
-                        return glm::scale(dvec3{1.0} / dvec3{opts.nPoints}) *
-                               glm::translate(dvec3{0.5} / dvec3{opts.nPoints});
+                        return glm::translate(dvec3{0.5} / dvec3{opts.nPoints}) *
+                               glm::scale(dvec3{1.0} / dvec3{opts.nPoints});
                     } else {
                         return dmat4{1.0f};
                     }
