92394f58e766178f29922989d69d336157bfd7f4
Peter Steneteg <peter@steneteg.se>
2026-02-11 11:47:24 +0100

    Core: DataSequence fix

--- include/inviwo/core/datastructures/datasequence.h
+++ include/inviwo/core/datastructures/datasequence.h
@@ -94,7 +94,7 @@ public:
 
     template <std::ranges::input_range R>
         requires std::same_as<std::ranges::range_value_t<R>, std::shared_ptr<const Data>> ||
-                 std::same_as<std::ranges::range_value_t<R>, std::shared_ptr<const Data>>
+                 std::same_as<std::ranges::range_value_t<R>, std::shared_ptr<Data>>
     explicit DataSequence(std::from_range_t, R data) {
         for (auto item : data) {
             data_.emplace_back(item);
