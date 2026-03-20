inviwo/inviwo

commit faa27a9c4c41302b47a680d648cbe6116ef8dd5b

BaseGL,OpenGL,OIT: AMD shader fix

modules/basegl/glsl/linerenderer.frag
@@ -86,7 +86,7 @@ void main() {
 
     // line stippling
 #if defined(ENABLE_STIPPLING)
-    float stippleLength = stippling.length + stippling.spacing;
+    float stippleLength = stippling.len + stippling.spacing;
 #if STIPPLE_MODE == 2
     // in world space
     float v = (fragment.distanceWorld * stippling.worldScale);

modules/oit/glsl/oit-linerenderer.frag
@@ -106,7 +106,7 @@ void main() {
 
     // line stippling
 #if defined(ENABLE_STIPPLING)
-    float stippleLength = stippling.length + stippling.spacing;
+    float stippleLength = stippling.len + stippling.spacing;
 #if STIPPLE_MODE == 2
     // in world space
     float v = (distanceWorld_ * stippling.worldScale);

modules/opengl/glsl/utils/structs.glsl
@@ -141,7 +141,7 @@ struct VolumeIndicatorParameters {
 };
 
 struct StipplingParameters {
-    float length;
+    float len;
     float spacing;
     float offset;
     float worldScale;

commit cac43034bf335885cdccbdc418fe39a530ea79d1

OpenGL,BaseGL,OIT,OpactOpt: stippling fix, broken in faa27a9
# Conflicts:
#	modules/basegl/src/rendering/linerenderer.cpp
#	modules/oit/src/processors/linerasterizer.cpp
#	modules/opactopt/src/processors/directopacityoptimization.cpp

modules/basegl/glsl/linerenderer.frag
@@ -86,7 +86,7 @@ void main() {
 
     // line stippling
 #if defined(ENABLE_STIPPLING)
-    float stippleLength = stippling.len + stippling.spacing;
+    float stippleLength = stippling.stippleLength + stippling.spacing;
 #if STIPPLE_MODE == 2
     // in world space
     float v = (fragment.distanceWorld * stippling.worldScale);

modules/basegl/src/rendering/linerenderer.cpp
@@ -177,7 +177,7 @@ void LineRenderer::setUniforms(Shader& lineShader, const Mesh& mesh, const Camer
     lineShader.setUniform("config.color", settings_.overrideColorValue);
     lineShader.setUniform("config.alpha", settings_.overrideAlphaValue);
     // Stippling settings
-    lineShader.setUniform("stippling.length", settings_.stippling.length);
+    lineShader.setUniform("stippling.stippleLength", settings_.stippling.length);
     lineShader.setUniform("stippling.spacing", settings_.stippling.spacing);
     lineShader.setUniform("stippling.offset", settings_.stippling.offset);
     lineShader.setUniform("stippling.worldScale", settings_.stippling.worldScale);

modules/oit/glsl/oit-linerenderer.frag
@@ -106,7 +106,7 @@ void main() {
 
     // line stippling
 #if defined(ENABLE_STIPPLING)
-    float stippleLength = stippling.len + stippling.spacing;
+    float stippleLength = stippling.stippleLength + stippling.spacing;
 #if STIPPLE_MODE == 2
     // in world space
     float v = (distanceWorld_ * stippling.worldScale);

modules/oit/src/processors/linerasterizer.cpp
@@ -181,7 +181,7 @@ void LineRasterizer::setUniforms(Shader& shader) {
     shader.setUniform("miterLimit", lineSettings_.miterLimit);
     shader.setUniform("roundCaps", lineSettings_.roundCaps);
     // Stippling settings
-    shader.setUniform("stippling.length", lineSettings_.stippling.length);
+    shader.setUniform("stippling.stippleLength", lineSettings_.stippling.length);
     shader.setUniform("stippling.spacing", lineSettings_.stippling.spacing);
     shader.setUniform("stippling.offset", lineSettings_.stippling.offset);
     shader.setUniform("stippling.worldScale", lineSettings_.stippling.worldScale);

modules/opactopt/src/processors/directopacityoptimization.cpp
@@ -591,7 +591,7 @@ void OpacityOptimization::renderGeometry(const Pass pass, Units& units) {
                             lineSettings_.defaultColor);
 
         // Stippling settings
-        shader.setUniform("stippling.length", lineSettings_.stippling.length);
+        shader.setUniform("stippling.stippleLength", lineSettings_.stippling.length);
         shader.setUniform("stippling.spacing", lineSettings_.stippling.spacing);
         shader.setUniform("stippling.offset", lineSettings_.stippling.offset);
         shader.setUniform("stippling.worldScale", lineSettings_.stippling.worldScale);

modules/opengl/glsl/utils/structs.glsl
@@ -162,7 +162,7 @@ struct VolumeIndicatorParameters {
 };
 
 struct StipplingParameters {
-    float len;
+    float stippleLength;
     float spacing;
     float offset;
     float worldScale;