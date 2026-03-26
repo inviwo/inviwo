e62b611f58d8b10e4da0c42f272b9cdc668fae14
Martin Falk <martin.falk@liu.se>
2026-03-26 11:10:50 +0100

    Base: MeshInformationProperty fix

--- modules/base/src/properties/meshinformationproperty.cpp
+++ modules/base/src/properties/meshinformationproperty.cpp
@@ -100,12 +100,12 @@ MeshInformationProperty::MeshInformationProperty(std::string_view identifier,
 
     , transformations_("transformations", "Transformations")
     , modelTransform_("modelTransform_", "Model Transform", mat4(1.0f),
-                      util::filled<mat3>(std::numeric_limits<float>::lowest()),
-                      util::filled<mat3>(std::numeric_limits<float>::max()),
-                      util::filled<mat3>(0.001f), InvalidationLevel::Valid)
+                      util::filled<mat4>(std::numeric_limits<float>::lowest()),
+                      util::filled<mat4>(std::numeric_limits<float>::max()),
+                      util::filled<mat4>(0.001f), InvalidationLevel::Valid)
     , worldTransform_("worldTransform_", "World Transform", mat4(1.0f),
-                      util::filled<mat3>(std::numeric_limits<float>::lowest()),
-                      util::filled<mat3>(std::numeric_limits<float>::max()),
+                      util::filled<mat4>(std::numeric_limits<float>::lowest()),
+                      util::filled<mat4>(std::numeric_limits<float>::max()),
                       util::filled<mat3>(0.001f), InvalidationLevel::Valid)
     , basis_("basis", "Basis", mat3(1.0f), util::filled<mat3>(std::numeric_limits<float>::lowest()),
              util::filled<mat3>(std::numeric_limits<float>::max()), util::filled<mat3>(0.001f),
