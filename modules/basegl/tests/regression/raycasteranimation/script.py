eec5cc53cfc200417997ea693173fddcba35fc98
Peter Steneteg <peter@steneteg.se>
2026-04-02 15:12:27 +0200

    Various: regression update

--- modules/basegl/tests/regression/raycasteranimation/script.py
+++ modules/basegl/tests/regression/raycasteranimation/script.py
@@ -1,7 +1,7 @@
 # Inviwo Python script
 import inviwopy
 from inviwopy import qt as inviwoqt
-from inviwopy.glm import size2_t, vec3
+from inviwopy.glm import size2_t, dvec3
 
 import math
 import time
@@ -19,19 +19,19 @@ canvas = network.CanvasGL
 orgsize = canvas.size
 
 with ivw.camera.Camera(network.EntryExitPoints.camera,
-                       lookfrom=vec3(0, 4, 0),
-                       lookto=vec3(0, 0, 0),
-                       lookup=vec3(0, 0, 1)) as c:
+                       lookFrom=dvec3(0, 4, 0),
+                       lookTo=dvec3(0, 0, 0),
+                       lookUp=dvec3(0, 0, 1)) as c:
     for size in [256, 512, 768, 1024]:
         canvas.size = size2_t(size, size)
         ivw.regression.saveCanvas(canvas, "CanvasGL-" + str(size) + "x" + str(size))
 
         start = time.perf_counter()
-        for step in c.rotate(math.pi / steps, steps, vec3(0, 1, 0)):
+        for step in c.rotate(math.pi / steps, steps, dvec3(0, 1, 0)):
             inviwoqt.update()
         end = time.perf_counter()
-        frametime = (end - start) / steps
-        fps = 1.0 / frametime
+        frameTime = (end - start) / steps
+        fps = 1.0 / frameTime
 
         m.addFrequency('FPS-' + str(size) + 'x' + str(size), fps)
 
