24515e1507fa66bc702656aea03a1091f8b88682
Peter Steneteg <peter@steneteg.se>
2026-04-21 10:21:09 +0200

    Core: fixes

--- include/inviwo/core/algorithm/rangeutils.h
+++ include/inviwo/core/algorithm/rangeutils.h
@@ -53,57 +53,60 @@ inline constexpr decltype(std::ranges::starts_with) starts_with{};
 // From cpp_reference until we have this in all compilers we support
 struct ends_with_fn {
     template <std::input_iterator I1, std::sentinel_for<I1> S1, std::input_iterator I2,
-              std::sentinel_for<I2> S2, class Pred = ranges::equal_to, class Proj1 = std::identity,
-              class Proj2 = std::identity>
+              std::sentinel_for<I2> S2, class Pred = std::ranges::equal_to,
+              class Proj1 = std::identity, class Proj2 = std::identity>
         requires(std::forward_iterator<I1> || std::sized_sentinel_for<S1, I1>) &&
                 (std::forward_iterator<I2> || std::sized_sentinel_for<S2, I2>) &&
                 std::indirectly_comparable<I1, I2, Pred, Proj1, Proj2>
     constexpr bool operator()(I1 first1, S1 last1, I2 first2, S2 last2, Pred pred = {},
                               Proj1 proj1 = {}, Proj2 proj2 = {}) const {
-        const auto n1 = ranges::distance(first1, last1);
-        const auto n2 = ranges::distance(first2, last2);
+        const auto n1 = std::ranges::distance(first1, last1);
+        const auto n2 = std::ranges::distance(first2, last2);
         if (n1 < n2) return false;
-        ranges::advance(first1, n1 - n2);
-        return ranges::equal(std::move(first1), last1, std::move(first2), last2, pred, proj1,
-                             proj2);
+        std::ranges::advance(first1, n1 - n2);
+        return std::ranges::equal(std::move(first1), last1, std::move(first2), last2, pred, proj1,
+                                  proj2);
     }
 
-    template <ranges::input_range R1, ranges::input_range R2, class Pred = ranges::equal_to,
-              class Proj1 = std::identity, class Proj2 = std::identity>
-        requires(ranges::forward_range<R1> || ranges::sized_range<R1>) &&
-                (ranges::forward_range<R2> || ranges::sized_range<R2>) &&
-                std::indirectly_comparable<ranges::iterator_t<R1>, ranges::iterator_t<R2>, Pred,
-                                           Proj1, Proj2>
+    template <std::ranges::input_range R1, std::ranges::input_range R2,
+              class Pred = std::ranges::equal_to, class Proj1 = std::identity,
+              class Proj2 = std::identity>
+        requires(std::ranges::forward_range<R1> || std::ranges::sized_range<R1>) &&
+                (std::ranges::forward_range<R2> || std::ranges::sized_range<R2>) &&
+                std::indirectly_comparable<std::ranges::iterator_t<R1>, std::ranges::iterator_t<R2>,
+                                           Pred, Proj1, Proj2>
     constexpr bool operator()(R1&& r1, R2&& r2, Pred pred = {}, Proj1 proj1 = {},
                               Proj2 proj2 = {}) const {
-        const auto n1 = ranges::distance(r1);
-        const auto n2 = ranges::distance(r2);
+        const auto n1 = std::ranges::distance(r1);
+        const auto n2 = std::ranges::distance(r2);
         if (n1 < n2) return false;
-        return ranges::equal(views::drop(ranges::ref_view(r1), n1 - static_cast<decltype(n1)>(n2)),
-                             r2, pred, proj1, proj2);
+        return std::ranges::equal(
+            std::views::drop(std::ranges::ref_view(r1), n1 - static_cast<decltype(n1)>(n2)), r2,
+            pred, proj1, proj2);
     }
 };
 
 struct starts_with_fn {
     template <std::input_iterator I1, std::sentinel_for<I1> S1, std::input_iterator I2,
-              std::sentinel_for<I2> S2, class Pred = ranges::equal_to, class Proj1 = std::identity,
-              class Proj2 = std::identity>
+              std::sentinel_for<I2> S2, class Pred = std::ranges::equal_to,
+              class Proj1 = std::identity, class Proj2 = std::identity>
         requires std::indirectly_comparable<I1, I2, Pred, Proj1, Proj2>
     constexpr bool operator()(I1 first1, S1 last1, I2 first2, S2 last2, Pred pred = {},
                               Proj1 proj1 = {}, Proj2 proj2 = {}) const {
-        return ranges::mismatch(std::move(first1), last1, std::move(first2), last2, std::move(pred),
-                                std::move(proj1), std::move(proj2))
+        return std::ranges::mismatch(std::move(first1), last1, std::move(first2), last2,
+                                     std::move(pred), std::move(proj1), std::move(proj2))
                    .in2 == last2;
     }
 
-    template <ranges::input_range R1, ranges::input_range R2, class Pred = ranges::equal_to,
-              class Proj1 = std::identity, class Proj2 = std::identity>
-        requires std::indirectly_comparable<ranges::iterator_t<R1>, ranges::iterator_t<R2>, Pred,
-                                            Proj1, Proj2>
+    template <std::ranges::input_range R1, std::ranges::input_range R2,
+              class Pred = std::ranges::equal_to, class Proj1 = std::identity,
+              class Proj2 = std::identity>
+        requires std::indirectly_comparable<std::ranges::iterator_t<R1>,
+                                            std::ranges::iterator_t<R2>, Pred, Proj1, Proj2>
     constexpr bool operator()(R1&& r1, R2&& r2, Pred pred = {}, Proj1 proj1 = {},
                               Proj2 proj2 = {}) const {
-        return (*this)(ranges::begin(r1), ranges::end(r1), ranges::begin(r2), ranges::end(r2),
-                       std::move(pred), std::move(proj1), std::move(proj2));
+        return (*this)(std::ranges::begin(r1), std::ranges::end(r1), std::ranges::begin(r2),
+                       std::ranges::end(r2), std::move(pred), std::move(proj1), std::move(proj2));
     }
 };
 
