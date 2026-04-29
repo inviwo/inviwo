inviwo/inviwo

commit 264c2219c6eaf09edacb70e11e065623e53ed628

QtWidgets: added NumberWidget
* new widget based on QLineEdit supporting value dragging

modules/qtwidgets/include/modules/qtwidgets/properties/ordinalpropertywidgetqt.h
@@ -41,6 +41,7 @@
 #include <modules/qtwidgets/properties/propertysettingswidgetqt.h>  // for OrdinalLikePropertySe...
 #include <modules/qtwidgets/properties/propertywidgetqt.h>          // for PropertyWidgetQt
 #include <modules/qtwidgets/sliderwidgetqt.h>                       // for SliderWidgetQt
+#include <modules/qtwidgets/numberwidget.h>
 
 #include <array>        // for array
 #include <cmath>        // for sin, atan2, cos, M_PI
@@ -159,13 +160,13 @@ OrdinalLikePropertyWidgetQt<Prop, Sem>::OrdinalLikePropertyWidgetQt(Prop* proper
                 if (col > 0) w->setWrapping(true);
                 return w;
             } else if constexpr (Sem == OrdinalPropertyWidgetQtSemantics::Text) {
-                return new OrdinalEditorWidget<BT>();
+                return new NumberWidget<BT>();
             } else if constexpr (Sem == OrdinalPropertyWidgetQtSemantics::Spherical) {
-                auto w = new SliderWidgetQt<BT>();
+                auto w = new NumberWidget<BT>();
                 if (col > 0) w->setWrapping(true);
                 return w;
             } else {
-                return new SliderWidgetQt<BT>();
+                return new NumberWidget<BT>();
             }
         }();
 
@@ -180,8 +181,17 @@ OrdinalLikePropertyWidgetQt<Prop, Sem>::OrdinalLikePropertyWidgetQt(Prop* proper
         sp.setHorizontalPolicy(QSizePolicy::Expanding);
         editor->setSizePolicy(sp);
 
-        if constexpr (Sem == OrdinalPropertyWidgetQtSemantics::SphericalSpinBox ||
-                      Sem == OrdinalPropertyWidgetQtSemantics::Spherical) {
+        if constexpr ((Sem == OrdinalPropertyWidgetQtSemantics::Default ||
+                       Sem == OrdinalPropertyWidgetQtSemantics::Text) &&
+                      util::extent<T, 0>::value > 1 && util::extent<T, 1>::value == 1) {
+            constexpr std::array<const char*, 4> labels{"x", "y", "z", "w"};
+            editor->setPrefix(labels[col]);
+            return editor;
+        } else if constexpr (Sem == OrdinalPropertyWidgetQtSemantics::Spherical) {
+            constexpr std::array<const char*, 3> sphericalLabels{"r", R"(θ)", R"(φ)"};
+            editor->setPrefix(sphericalLabels[col]);
+            return editor;
+        } else if constexpr (Sem == OrdinalPropertyWidgetQtSemantics::SphericalSpinBox) {
             constexpr std::array<const char*, 3> sphericalLabels{"r", "<html>&theta;</html>",
                                                                  "<html>&phi;</html>"};
             auto* widget = new QWidget(this);
@@ -211,10 +221,6 @@ OrdinalLikePropertyWidgetQt<Prop, Sem>::OrdinalLikePropertyWidgetQt(Prop* proper
             if constexpr (util::extent<T, 1>::value > 1 &&
                           Sem != OrdinalPropertyWidgetQtSemantics::Default) {
                 std::swap(layoutCol, layoutRow);
-            } else if constexpr (Sem == OrdinalPropertyWidgetQtSemantics::Default ||
-                                 Sem == OrdinalPropertyWidgetQtSemantics::Spherical) {
-                layoutCol = 1;
-                layoutRow = col + util::extent<T, 1>::value * row;
             }
             gridLayout->addWidget(editor, static_cast<int>(layoutRow), static_cast<int>(layoutCol));
         }

modules/qtwidgets/CMakeLists.txt
@@ -16,6 +16,7 @@ set(MOC_FILES
     include/modules/qtwidgets/inviwowidgetsqt.h
     include/modules/qtwidgets/lightpositionwidgetqt.h
     include/modules/qtwidgets/lineeditqt.h
+    include/modules/qtwidgets/numberwidget.h
     include/modules/qtwidgets/ordinaleditorwidget.h
     include/modules/qtwidgets/processors/processordockwidgetqt.h    
     include/modules/qtwidgets/properties/colorlineedit.h
@@ -117,6 +118,7 @@ set(SOURCE_FILES
     src/lineeditqt.cpp
     src/mousecursorutils.cpp
     src/numberlineedit.cpp
+    src/numberwidget.cpp
     src/ordinalbasewidget.cpp
     src/ordinaleditorwidget.cpp
     src/processors/processordockwidgetqt.cpp

modules/qtwidgets/include/modules/qtwidgets/numberwidget.h
@@ -1,0 +1,333 @@
+/*********************************************************************************
+ *
+ * Inviwo - Interactive Visualization Workshop
+ *
+ * Copyright (c) 2025 Inviwo Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************************/
+#pragma once
+
+#include <modules/qtwidgets/qtwidgetsmoduledefine.h>
+#include <modules/qtwidgets/ordinalbasewidget.h>
+#include <modules/qtwidgets/inviwoqtutils.h>
+
+#include <optional>
+#include <limits>
+
+#include <QLineEdit>
+#include <QByteArray>
+
+class QEvent;
+class QMouseEvent;
+class QPaintEvent;
+
+namespace inviwo {
+
+class IVW_MODULE_QTWIDGETS_API BaseNumberWidget : public QLineEdit {
+    Q_OBJECT
+public:
+    explicit BaseNumberWidget(QWidget* parent = nullptr);
+    BaseNumberWidget(std::string_view prefix, std::string_view postfix, QWidget* parent = nullptr);
+    virtual ~BaseNumberWidget() = default;
+
+    void setPrefix(std::string_view prefix);
+    const QString& getPrefix() const;
+    void setPostfix(std::string_view postfix);
+    const QString& getPostfix() const;
+
+    void setWrapping(bool wrapping);
+    bool getWrapping() const;
+
+    void updateText();
+
+    virtual bool event(QEvent* event) override;
+
+signals:
+    void valueChanged();
+
+protected:
+    virtual void mousePressEvent(QMouseEvent* event) override;
+    virtual void mouseReleaseEvent(QMouseEvent* event) override;
+    virtual void mouseMoveEvent(QMouseEvent* event) override;
+    virtual void paintEvent(QPaintEvent* event) override;
+    virtual void changeEvent(QEvent* event) override;
+
+    virtual bool incrementValue() = 0;
+    virtual bool decrementValue() = 0;
+    virtual bool applyDelta(double deltaSteps) = 0;
+    virtual void initDragValue() = 0;
+    virtual bool valueFromTextValid(const QString& str) = 0;
+    virtual bool updateValueFromText(const QString& str) = 0;
+    virtual QString getTextFromValue(bool precise) const = 0;
+    virtual std::optional<double> getPercentage() const = 0;
+
+private:
+    enum class FocusAction : std::uint8_t { SetFocus, ClearFocus };
+    enum class HoverState : std::uint8_t { Invalid, Center, NegativeInc, PositiveInc };
+
+    QString getPrefixedText() const;
+    void updateState(FocusAction action);
+    HoverState getHoverState(QPoint mousepos) const;
+    void updateHoverState(QPoint mousepos);
+
+    struct InteractionState {
+        QPoint previousPos{};
+        bool dragging = false;
+        HoverState hover = HoverState::Invalid;
+    };
+
+    bool wrapping_;
+    QString prefix_;
+    QString postfix_;
+    InteractionState state_;
+
+    static constexpr Qt::KeyboardModifier increasedStepModifier = Qt::ControlModifier;
+    static constexpr Qt::KeyboardModifier decreasedStepModifier = Qt::ShiftModifier;
+    static constexpr double increasedStepSize = 10.0;
+    static constexpr double decreasedStepSize = 0.1;
+};
+
+template <typename T>
+class IVW_MODULE_QTWIDGETS_API NumberWidget final : public BaseNumberWidget,
+                                                    public OrdinalBaseWidget<T> {
+public:
+    NumberWidget();
+    virtual ~NumberWidget() = default;
+
+    // Implements OrdinalBaseWidget
+    virtual T getValue() const override;
+    virtual void setValue(T value) override;
+    virtual void initValue(T value) override;
+    virtual void setMinValue(T minValue, ConstraintBehavior cb) override;
+    virtual void setMaxValue(T maxValue, ConstraintBehavior cb) override;
+    virtual void setIncrement(T increment) override;
+
+protected:
+    virtual bool incrementValue() override;
+    virtual bool decrementValue() override;
+    virtual bool applyDelta(double deltaSteps) override;
+    virtual void initDragValue() override;
+    virtual bool valueFromTextValid(const QString& str) override;
+    virtual bool updateValueFromText(const QString& str) override;
+    virtual QString getTextFromValue(bool precise) const override;
+    virtual std::optional<double> getPercentage() const override;
+
+    bool updateValue(T value);
+    double getUIIncrement() const;
+
+private:
+    T value_;
+    T minValue_;
+    T maxValue_;
+    T increment_;
+    T initialDragValue_;
+    ConstraintBehavior minCB_;
+    ConstraintBehavior maxCB_;
+};
+
+template <typename T>
+inline NumberWidget<T>::NumberWidget()
+    : OrdinalBaseWidget<T>()
+    , value_{1}
+    , minValue_{0}
+    , maxValue_{2}
+    , increment_{1}
+    , initialDragValue_{0}
+    , minCB_{ConstraintBehavior::Editable}
+    , maxCB_{ConstraintBehavior::Editable} {}
+
+template <typename T>
+T NumberWidget<T>::getValue() const {
+    return value_;
+}
+template <typename T>
+void NumberWidget<T>::setValue(T value) {
+    if (value != value_) {
+        value_ = value;
+        updateText();
+        emit valueChanged();
+    }
+}
+template <typename T>
+void NumberWidget<T>::initValue(T value) {
+    value_ = value;
+    updateText();
+}
+template <typename T>
+void NumberWidget<T>::setMinValue(T minValue, ConstraintBehavior cb) {
+    minValue_ = minValue;
+    minCB_ = cb;
+}
+template <typename T>
+void NumberWidget<T>::setMaxValue(T maxValue, ConstraintBehavior cb) {
+    maxValue_ = maxValue;
+    maxCB_ = cb;
+}
+template <typename T>
+void NumberWidget<T>::setIncrement(T increment) {
+    increment_ = increment;
+}
+
+template <typename T>
+bool NumberWidget<T>::incrementValue() {
+    const T uiIncrement = getUIIncrement();
+    if (maxValue_ - uiIncrement < value_) {
+        if (getWrapping()) {
+            const T delta = maxValue_ - value_;
+            return updateValue(minValue_ + uiIncrement - delta);
+        } else {
+            return updateValue(maxValue_);
+        }
+    } else {
+        return updateValue(value_ + uiIncrement);
+    }
+}
+
+template <typename T>
+bool NumberWidget<T>::decrementValue() {
+    const T uiIncrement = getUIIncrement();
+    if (minValue_ + uiIncrement > value_) {
+        if (getWrapping()) {
+            const T delta = value_ - minValue_;
+            return updateValue(maxValue_ - uiIncrement + delta);
+        } else {
+            return updateValue(minValue_);
+        }
+    } else {
+        return updateValue(value_ - uiIncrement);
+    }
+}
+
+template <typename T>
+bool NumberWidget<T>::applyDelta(double deltaSteps) {
+    if (deltaSteps == 0.0) {
+        return false;
+    }
+
+    T delta = static_cast<T>(deltaSteps * getUIIncrement());
+    if (getWrapping()) {
+        if constexpr (std::is_floating_point_v<T>) {
+            delta = std::fmod(delta, maxValue_ - minValue_);
+        } else {
+            delta = delta % (maxValue_ - minValue_);
+        }
+        if (maxValue_ - delta < initialDragValue_) {
+            delta -= maxValue_ - minValue_;
+        } else if (minValue_ - delta > initialDragValue_) {
+            delta += maxValue_ - minValue_;
+        }
+        return updateValue(initialDragValue_ + delta);
+    }
+
+    if (delta > 0) {
+        if (maxCB_ == ConstraintBehavior::Ignore) {
+            if (std::numeric_limits<T>::max() - delta < initialDragValue_) {
+                return updateValue(std::numeric_limits<T>::max());
+            } else {
+                return updateValue(initialDragValue_ + delta);
+            }
+        } else if (maxValue_ - delta < initialDragValue_) {
+            return updateValue(maxValue_);
+        } else {
+            return updateValue(initialDragValue_ + delta);
+        }
+    } else {
+        if (minCB_ == ConstraintBehavior::Ignore) {
+            if (std::numeric_limits<T>::lowest() - delta > initialDragValue_) {
+                return updateValue(std::numeric_limits<T>::lowest());
+            } else {
+                return updateValue(initialDragValue_ + delta);
+            }
+        } else if (minValue_ - delta > initialDragValue_) {
+            return updateValue(minValue_);
+        } else {
+            return updateValue(initialDragValue_ + delta);
+        }
+    }
+}
+
+template <typename T>
+void NumberWidget<T>::initDragValue() {
+    // Need to keep track of the current value when mouse dragging starts to have an absolute delta.
+    // Otherwise the incremental delta between two events might be too small for updating integer
+    // values.
+    initialDragValue_ = value_;
+}
+
+template <typename T>
+bool NumberWidget<T>::valueFromTextValid(const QString& str) {
+    return utilqt::numericValueFromString<T>(str).has_value();
+}
+
+template <typename T>
+bool NumberWidget<T>::updateValueFromText(const QString& str) {
+    if (auto newValue = utilqt::numericValueFromString<T>(str); newValue && *newValue != value_) {
+        value_ = *newValue;
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+QString NumberWidget<T>::getTextFromValue(bool precise) const {
+    if constexpr (std::is_floating_point_v<T>) {
+        if (precise) {
+            return utilqt::formatAsNonscientific(value_);
+        } else {
+            return QString::number(value_, 'f', utilqt::decimals(increment_));
+        }
+    } else {
+        return QString::number(value_);
+    }
+}
+
+template <typename T>
+std::optional<double> NumberWidget<T>::getPercentage() const {
+    if (minValue_ == std::numeric_limits<T>::lowest() ||
+        maxValue_ == std::numeric_limits<T>::max()) {
+        return std::nullopt;
+    }
+    return static_cast<double>(value_ - minValue_) / static_cast<double>(maxValue_ - minValue_);
+}
+
+template <typename T>
+bool NumberWidget<T>::updateValue(T v) {
+    if (value_ != v) {
+        value_ = v;
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+double NumberWidget<T>::getUIIncrement() const {
+    if (minValue_ == std::numeric_limits<T>::lowest() ||
+        maxValue_ == std::numeric_limits<T>::max()) {
+        return static_cast<double>(increment_);
+    }
+
+    return static_cast<double>(maxValue_ - minValue_) / static_cast<double>(width());
+}
+
+}  // namespace inviwo

modules/qtwidgets/src/numberwidget.cpp
@@ -1,0 +1,330 @@
+/*********************************************************************************
+ *
+ * Inviwo - Interactive Visualization Workshop
+ *
+ * Copyright (c) 2025 Inviwo Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************************/
+
+#include <modules/qtwidgets/numberwidget.h>
+#include <modules/qtwidgets/inviwoqtutils.h>
+
+#include <QGuiApplication>
+#include <QPaintEvent>
+#include <QKeyEvent>
+#include <QMouseEvent>
+#include <QStyleOption>
+#include <QStyleOptionFrame>
+#include <QPainter>
+#include <QSvgRenderer>
+#include <QFontMetrics>
+
+namespace inviwo {
+
+BaseNumberWidget::BaseNumberWidget(QWidget* parent) : BaseNumberWidget{{}, {}, parent} {}
+
+BaseNumberWidget::BaseNumberWidget(std::string_view prefix, std::string_view postfix,
+                                   QWidget* parent)
+    : QLineEdit{parent}
+    , wrapping_{false}
+    , prefix_{utilqt::toQString(prefix)}
+    , postfix_{utilqt::toQString(postfix)} {
+
+    QSizePolicy sp = sizePolicy();
+    sp.setHorizontalPolicy(QSizePolicy::Minimum);
+    setSizePolicy(sp);
+
+    updateState(FocusAction::ClearFocus);
+    setFocusPolicy(Qt::TabFocus);
+    setMouseTracking(true);
+
+    QObject::connect(this, &QLineEdit::textEdited, this, [this](const QString& text) {
+        if (valueFromTextValid(text)) {
+            setProperty("invalid", QVariant{});
+        } else {
+            setProperty("invalid", true);
+        }
+        style()->unpolish(this);
+        style()->polish(this);
+    });
+}
+
+void BaseNumberWidget::setPrefix(std::string_view prefix) {
+    prefix_ = utilqt::toQString(prefix);
+    updateText();
+}
+
+const QString& BaseNumberWidget::getPrefix() const { return prefix_; }
+
+void BaseNumberWidget::setPostfix(std::string_view postfix) {
+    postfix_ = utilqt::toQString(postfix);
+    updateText();
+}
+
+const QString& BaseNumberWidget::getPostfix() const { return postfix_; }
+
+void BaseNumberWidget::setWrapping(bool wrapping) { wrapping_ = wrapping; }
+
+bool BaseNumberWidget::getWrapping() const { return wrapping_; }
+
+bool BaseNumberWidget::event(QEvent* event) {
+    if (event->type() == QEvent::ReadOnlyChange) {
+        updateHoverState(mapFromGlobal(QCursor::pos()));
+        QSignalBlocker blocker{this};
+        updateText();
+    } else if (!isReadOnly() && isEnabled()) {
+        if (event->type() == QEvent::FocusIn) {
+            updateState(FocusAction::SetFocus);
+        } else if (event->type() == QEvent::FocusOut) {
+            if (updateValueFromText(text())) {
+                emit valueChanged();
+            }
+            updateState(FocusAction::ClearFocus);
+        } else if (event->type() == QEvent::KeyPress) {
+            const auto* keyEvent = static_cast<QKeyEvent*>(event);
+
+            if (keyEvent->key() == Qt::Key_Escape) {
+                // cancel editing
+                QSignalBlocker blocker{this};
+                updateText();
+                clearFocus();
+                return true;
+            } else if (keyEvent->key() == Qt::Key_Return || keyEvent->key() == Qt::Key_Enter) {
+                // commit changes
+                clearFocus();
+                return true;
+            }
+        }
+    }
+    return QLineEdit::event(event);
+}
+
+void BaseNumberWidget::mousePressEvent(QMouseEvent* event) {
+    if (!hasFocus() && !isReadOnly() && isEnabled() && event->button() == Qt::LeftButton) {
+        state_.previousPos = event->pos();
+        state_.dragging = false;
+        event->accept();
+    } else {
+        QLineEdit::mousePressEvent(event);
+    }
+}
+
+void BaseNumberWidget::mouseReleaseEvent(QMouseEvent* event) {
+    if (!hasFocus() && !isReadOnly() && isEnabled() && event->button() == Qt::LeftButton) {
+        event->accept();
+        if (!state_.dragging) {
+            switch (state_.hover) {
+                case HoverState::PositiveInc:
+                    incrementValue();
+                    emit valueChanged();
+                    update();
+                    break;
+                case HoverState::NegativeInc:
+                    decrementValue();
+                    emit valueChanged();
+                    update();
+                    break;
+                case HoverState::Center:
+                default:
+                    // mouse left click on center and no movement, start editing
+                    setFocus(Qt::TabFocusReason);
+                    break;
+            }
+        }
+        state_.dragging = false;
+    } else {
+        QLineEdit::mouseReleaseEvent(event);
+    }
+}
+
+void BaseNumberWidget::mouseMoveEvent(QMouseEvent* event) {
+    updateHoverState(event->pos());
+
+    // ignore dragging as long as movement delta is too small
+    if (!hasFocus() && !isReadOnly() && isEnabled() && event->buttons().testFlag(Qt::LeftButton)) {
+        event->accept();
+        const int delta = (event->pos() - state_.previousPos).x();
+        if (!state_.dragging) {
+            const int minimalMovement = 3;
+            state_.dragging = std::abs(delta) > minimalMovement;
+            if (state_.dragging) {
+                setCursor(Qt::SizeHorCursor);
+                state_.hover = HoverState::Center;
+                initDragValue();
+            }
+        } else {
+            double deltaStep = static_cast<double>(delta);
+            if (QGuiApplication::queryKeyboardModifiers().testFlag(increasedStepModifier)) {
+                deltaStep *= increasedStepSize;
+            } else if (QGuiApplication::queryKeyboardModifiers().testFlag(decreasedStepModifier)) {
+                deltaStep *= decreasedStepSize;
+            }
+            applyDelta(deltaStep);
+            emit valueChanged();
+            update();
+        }
+    } else {
+        QLineEdit::mouseMoveEvent(event);
+    }
+}
+
+void BaseNumberWidget::paintEvent(QPaintEvent* event) {
+    const int borderWidth = 1;
+    const int incrementWidth = height() * 2 / 3;
+    QStyleOptionFrame panel;
+    initStyleOption(&panel);
+    const bool hover = panel.state.testFlag(QStyle::State_MouseOver);
+
+    if (!hasFocus() && !isReadOnly() && isEnabled()) {
+        constexpr std::string_view arrowLeft{":/svgicons/arrow-left-enabled.svg"};
+        constexpr std::string_view arrowRight{":/svgicons/arrow-right-enabled.svg"};
+
+        QPainter painter{this};
+        style()->drawPrimitive(QStyle::PE_PanelLineEdit, &panel, &painter, this);
+        QRect r = style()->subElementRect(QStyle::SE_LineEditContents, &panel, this);
+        painter.setPen(Qt::NoPen);
+        painter.setClipRect(r);
+        painter.setRenderHints(QPainter::Antialiasing | QPainter::TextAntialiasing);
+
+        QRect interiorRect{rect().adjusted(borderWidth, borderWidth, -borderWidth, -borderWidth)};
+
+        // draw percentage bar in the center part
+        if (auto percentage = getPercentage(); percentage.has_value()) {
+            const QRect centerRect{QPoint{borderWidth, borderWidth},
+                                   QPoint{static_cast<int>(interiorRect.width() * *percentage),
+                                          height() - 2 * borderWidth}};
+            const QBrush percentageBrush{hover ? "#1e70a8" : "#103a57"};
+            painter.setBrush(percentageBrush);
+            painter.drawRect(centerRect);
+        }
+
+        if (hover) {
+            QRect arrowRect{interiorRect.topLeft(), QSize{incrementWidth, interiorRect.height()}};
+
+            const QBrush bgActive{"#185987"};
+
+            if (state_.hover == HoverState::NegativeInc) {
+                painter.setBrush(bgActive);
+                painter.drawRect(arrowRect);
+            }
+
+            QSvgRenderer renderer(utilqt::toQString(arrowLeft));
+            renderer.setAspectRatioMode(Qt::KeepAspectRatio);
+            renderer.render(&painter, arrowRect);
+
+            arrowRect.moveRight(width() - borderWidth);
+            if (state_.hover == HoverState::PositiveInc) {
+                painter.setBrush(bgActive);
+                painter.drawRect(arrowRect);
+            }
+
+            renderer.load(utilqt::toQString(arrowRight));
+            renderer.setAspectRatioMode(Qt::KeepAspectRatio);
+            renderer.render(&painter, arrowRect);
+        }
+
+        painter.setFont(font());
+        painter.setPen(palette().color(QPalette::Text));
+        painter.drawText(event->rect(), Qt::AlignCenter, getPrefixedText());
+    } else {
+        QLineEdit::paintEvent(event);
+    }
+}
+
+void BaseNumberWidget::changeEvent(QEvent* event) {
+    if (event->type() == QEvent::EnabledChange) {
+        updateText();
+    }
+    QLineEdit::changeEvent(event);
+}
+
+void BaseNumberWidget::updateText() {
+    const QString str = (isReadOnly() || !isEnabled()) ? getPrefixedText() : getTextFromValue(true);
+    if (str != text()) {
+        setText(str);
+    }
+}
+
+QString BaseNumberWidget::getPrefixedText() const {
+    return QString("%1%2%3%4")
+        .arg(prefix_)
+        .arg(prefix_.length() > 0 ? ": " : "")
+        .arg(getTextFromValue(false))
+        .arg(postfix_);
+}
+
+void BaseNumberWidget::updateState(FocusAction action) {
+    if (action == FocusAction::SetFocus) {
+        // update textual representation
+        updateText();
+    }
+    setProperty("invalid", QVariant{});
+    style()->unpolish(this);
+    style()->polish(this);
+    setAlignment(action == FocusAction::SetFocus ? Qt::AlignLeft : Qt::AlignCenter);
+
+    updateHoverState(mapFromGlobal(QCursor::pos()));
+}
+
+auto BaseNumberWidget::getHoverState(QPoint mousepos) const -> HoverState {
+    const int incrementWidth = height() * 2 / 3;
+
+    if (hasFocus() || isReadOnly() || !isEnabled()) {
+        return HoverState::Invalid;
+    }
+    if (mousepos.x() < incrementWidth) {
+        return HoverState::NegativeInc;
+    } else if (mousepos.x() > width() - incrementWidth) {
+        return HoverState::PositiveInc;
+    } else {
+        return HoverState::Center;
+    }
+}
+
+void BaseNumberWidget::updateHoverState(QPoint mousepos) {
+    if (state_.dragging) {
+        return;
+    }
+
+    if (auto hoverState = getHoverState(mousepos); hoverState != state_.hover) {
+        state_.hover = hoverState;
+        update();
+
+        switch (state_.hover) {
+            case HoverState::Invalid:
+                setCursor(Qt::IBeamCursor);
+                break;
+            case HoverState::Center:
+                setCursor(Qt::SizeHorCursor);
+                break;
+            case HoverState::NegativeInc:
+            case HoverState::PositiveInc:
+            default:
+                unsetCursor();
+                break;
+        }
+    }
+}
+
+}  // namespace inviwo

resources/stylesheets/inviwo.qss
@@ -376,6 +376,7 @@ QPushButton:focus, QToolButton:focus {
 QLineEdit#ColorLineEdit[input="valid"] {
     border: 0.0625em solid #1a800d;
 }
+QLineEdit[invalid=true],
 QLineEdit#ColorLineEdit[input="invalid"] {
     border: 0.0625em solid #801717;
 }

commit 40288a1ea93703ad937305bcf13d292c9eb03c53

QtWidgets: OrdinalPropertyWidget matrix order fix
* regression in 264c2219

modules/qtwidgets/include/modules/qtwidgets/properties/ordinalpropertywidgetqt.h
@@ -210,23 +210,22 @@ OrdinalLikePropertyWidgetQt<Prop, Sem>::OrdinalLikePropertyWidgetQt(Prop* proper
         }
     };
 
-    for (size_t row = 0; row < util::extent<T, 1>::value; row++) {
-        for (size_t col = 0; col < util::extent<T, 0>::value; col++) {
+    for (size_t row = 0; row < util::extent_v<T, 1>; row++) {
+        for (size_t col = 0; col < util::extent_v<T, 0>; col++) {
             auto editor = factory(row, col);
 
             auto layoutCol = col;
             auto layoutRow = row;
 
             // vectors should be drawn in row major while matrices are column major
-            if constexpr (util::extent<T, 1>::value > 1 &&
-                          Sem != OrdinalPropertyWidgetQtSemantics::Default) {
+            if constexpr (util::rank_v<T> > 1) {
                 std::swap(layoutCol, layoutRow);
             }
             gridLayout->addWidget(editor, static_cast<int>(layoutRow), static_cast<int>(layoutCol));
         }
     }
 
-    if ((gridLayout->count() > 0) && gridLayout->itemAt(0)->widget()) {
+    if (gridLayout->isEmpty() && gridLayout->itemAt(0)->widget()) {
         setFocusPolicy(gridLayout->itemAt(0)->widget()->focusPolicy());
         setFocusProxy(gridLayout->itemAt(0)->widget());
     }