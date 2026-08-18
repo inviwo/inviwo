0cb577800f3599f03ffdba215c53f8fbdb6e960e
Peter Steneteg <peter@steneteg.se>
2026-08-18 13:56:48 +0200

    Core/Base sequence fixes

--- include/inviwo/core/processors/sequencecompositesource.h
+++ include/inviwo/core/processors/sequencecompositesource.h
@@ -122,8 +122,8 @@ public:
     virtual void setSequenceIndex(size_t index) override {
         if (index != sequenceIndex_) {
             sequenceIndex_ = index;
-            invalidate(InvalidationLevel::InvalidOutput);
         }
+        invalidate(InvalidationLevel::InvalidOutput);
     }
 
 private:
