b30ad0440ca1b378949e281b396e4a3122a3cb9d
Peter Steneteg <peter@steneteg.se>
2026-09-01 10:43:26 +0200

    Core : OrdinalOptProperty added builder functions util::ordinalOpt*

--- src/core/properties/ordinaloptproperty.cpp
+++ src/core/properties/ordinaloptproperty.cpp
@@ -67,4 +67,56 @@ template class IVW_CORE_TMPL_INST OrdinalOptProperty<dmat4>;
 template class IVW_CORE_TMPL_INST OrdinalOptProperty<glm::dquat>;
 template class IVW_CORE_TMPL_INST OrdinalOptProperty<glm::fquat>;
 
+OrdinalOptPropertyState<vec4> util::ordinalOptColor(float r, float g, float b, float a,
+                                                    InvalidationLevel invalidationLevel) {
+    return ordinalOptColor(vec4(r, g, b, a), invalidationLevel);
+}
+
+OrdinalOptPropertyState<vec4> util::ordinalOptColor(const vec4& value,
+                                                    InvalidationLevel invalidationLevel) {
+    return ordinalOptColor(std::optional<vec4>{value}, invalidationLevel);
+}
+
+OrdinalOptPropertyState<vec3> util::ordinalOptColor(const vec3& value,
+                                                    InvalidationLevel invalidationLevel) {
+    return ordinalOptColor(std::optional<vec3>{value}, invalidationLevel);
+}
+
+OrdinalOptPropertyState<vec4> util::ordinalOptColor(const std::optional<vec4>& value,
+                                                    InvalidationLevel invalidationLevel) {
+    return {value,
+            vec4{0.0f},
+            ConstraintBehavior::Immutable,
+            vec4{1.0f},
+            ConstraintBehavior::Immutable,
+            vec4{0.01f},
+            invalidationLevel,
+            PropertySemantics::Color};
+}
+
+OrdinalOptPropertyState<vec3> util::ordinalOptColor(const std::optional<vec3>& value,
+                                                    InvalidationLevel invalidationLevel) {
+    return {value,
+            vec3{0.0f},
+            ConstraintBehavior::Immutable,
+            vec3{1.0f},
+            ConstraintBehavior::Immutable,
+            vec3{0.01f},
+            invalidationLevel,
+            PropertySemantics::Color};
+}
+
+OrdinalOptPropertyState<vec3> util::ordinalOptLight(const std::optional<vec3>& pos, float min,
+                                                    float max,
+                                                    InvalidationLevel invalidationLevel) {
+    return {pos,
+            vec3{min},
+            ConstraintBehavior::Ignore,
+            vec3{max},
+            ConstraintBehavior::Ignore,
+            vec3{0.1f},
+            invalidationLevel,
+            PropertySemantics::LightPosition};
+}
+
 }  // namespace inviwo
