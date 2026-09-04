05a98b1a1344ad1f9a5de14a7403bbe94d006851
Martin Falk <martin.falk@liu.se>
2026-09-04 10:50:26 +0200

    PlottingGL: updated default format of PCP labels

--- modules/plottinggl/include/modules/plottinggl/processors/parallelcoordinates/parallelcoordinates.h
+++ modules/plottinggl/include/modules/plottinggl/processors/parallelcoordinates/parallelcoordinates.h
@@ -156,7 +156,7 @@ public:
     virtual void serialize(Serializer& s) const override;
     virtual void deserialize(Deserializer& d) override;
 
-    static constexpr std::string_view defaultLabelFormat = "{:.4f}";
+    static constexpr std::string_view defaultLabelFormat = "{:.6g}";
 
 protected:
     void linePicked(PickingEvent* p);
